WindOctober/sp1

feat: sp1-sdk, remote prover (#370)
Co-authored-by: mattstam <mattstam@live.com>

cli/src/assets/script/main.rs
@@ -1,6 +1,6 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

cli/src/commands/prove.rs
@@ -1,10 +1,7 @@
 use anstyle::*;
 use anyhow::Result;
 use clap::Parser;
-use sp1_core::{
-    utils::{self},
-    SP1Prover, SP1Stdin,
-};
+use sp1_sdk::{utils, SP1Prover, SP1Stdin};
 use std::time::Instant;
 use std::{env, fs::File, io::Read, path::PathBuf, str::FromStr};
 

examples/chess/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

examples/io/script/src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");

examples/json/script/src/main.rs
@@ -1,7 +1,7 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use lib::{Account, Transaction};
-use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 const JSON_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

recursion/program/src/constraints.rs
@@ -161,9 +161,9 @@ mod tests {
             ShardMainData, ShardProof, StarkGenericConfig, Verifier,
         },
         utils::BabyBearPoseidon2,
-        SP1Prover, SP1Stdin,
     };
     use sp1_recursion_core::runtime::Runtime;
+    use sp1_sdk::{SP1Prover, SP1Stdin};
 
     use p3_challenger::{CanObserve, FieldChallenger};
     use p3_field::PrimeField32;

sdk/src/client.rs
@@ -1,0 +1,113 @@
+use std::{env, time::Duration};
+
+use anyhow::{Ok, Result};
+use futures::future::join_all;
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Client as HttpClient, Url,
+};
+use reqwest_middleware::ClientWithMiddleware as HttpClientWithMiddleware;
+use serde::{de::DeserializeOwned, Serialize};
+use sp1_core::stark::StarkGenericConfig;
+use twirp::Client as TwirpClient;
+
+use crate::{
+    proto::prover::{
+        CreateProofRequest, GetProofStatusRequest, GetProofStatusResponse, ProofStatus,
+        Sp1ProverServiceClient, SubmitProofRequest,
+    },
+    SP1ProofWithIO,
+};
+
+pub struct SP1ProverServiceClient {
+    pub rpc: TwirpClient,
+    pub http: HttpClientWithMiddleware,
+}
+
+const DEFAULT_PROVER_NETWORK_RPC: &str = "https://rpc.succinct.xyz/";
+
+impl SP1ProverServiceClient {
+    pub fn with_token(access_token: String) -> Self {
+        let rpc_url = env::var("PROVER_NETWORK_RPC")
+            .unwrap_or_else(|_| DEFAULT_PROVER_NETWORK_RPC.to_string());
+        Self::with_url(access_token, rpc_url)
+    }
+
+    pub fn with_url(access_token: String, rpc_url: String) -> Self {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "Authorization",
+            HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
+        );
+        let twirp_http_client = HttpClient::builder()
+            .pool_max_idle_per_host(0)
+            .pool_idle_timeout(Duration::from_secs(240))
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let rpc =
+            TwirpClient::new(Url::parse(&rpc_url).unwrap(), twirp_http_client, vec![]).unwrap();
+
+        let http_client = HttpClient::builder()
+            .pool_max_idle_per_host(0)
+            .pool_idle_timeout(Duration::from_secs(240))
+            .build()
+            .unwrap();
+        Self {
+            rpc,
+            http: http_client.into(),
+        }
+    }
+
+    async fn upload_file(&self, url: &str, data: Vec<u8>) -> Result<()> {
+        self.http.put(url).body(data).send().await?;
+        Ok(())
+    }
+
+    pub async fn create_proof(&self, elf: &[u8], stdin: &[u8]) -> Result<String> {
+        let res = self.rpc.create_proof(CreateProofRequest {}).await?;
+
+        let program_bytes = bincode::serialize(elf)?;
+        let stdin_bytes = bincode::serialize(stdin)?;
+        let program_promise = self.upload_file(&res.program_put_url, program_bytes);
+        let stdin_promise = self.upload_file(&res.stdin_put_url, stdin_bytes);
+        let v = vec![program_promise, stdin_promise];
+        let mut results = join_all(v).await;
+        results.pop().expect("Failed to upload stdin")?;
+        results.pop().expect("Failed to upload program")?;
+
+        self.rpc
+            .submit_proof(SubmitProofRequest { id: res.id.clone() })
+            .await?;
+
+        Ok(res.id)
+    }
+
+    pub async fn get_proof_status<SC: StarkGenericConfig + Serialize + DeserializeOwned>(
+        &self,
+        proof_id: &str,
+    ) -> Result<(GetProofStatusResponse, Option<SP1ProofWithIO<SC>>)> {
+        let res = self
+            .rpc
+            .get_proof_status(GetProofStatusRequest {
+                id: proof_id.to_string(),
+            })
+            .await?;
+
+        let result = if res.status() == ProofStatus::Succeeded {
+            let proof = self
+                .http
+                .get(res.result_get_url.clone())
+                .send()
+                .await?
+                .bytes()
+                .await?;
+            Some(bincode::deserialize(&proof).expect("Failed to deserialize proof"))
+        } else {
+            None
+        };
+
+        Ok((res, result))
+    }
+}

sdk/src/io.rs
@@ -1,0 +1,166 @@
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+use sp1_core::utils::Buffer;
+
+/// Standard input for the prover.
+#[derive(Serialize, Deserialize)]
+pub struct SP1Stdin {
+    pub buffer: Buffer,
+}
+
+/// Standard output for the prover.
+#[derive(Serialize, Deserialize)]
+pub struct SP1Stdout {
+    pub buffer: Buffer,
+}
+
+impl Default for SP1Stdin {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SP1Stdin {
+    /// Create a new `SP1Stdin`.
+    pub fn new() -> Self {
+        Self {
+            buffer: Buffer::new(),
+        }
+    }
+
+    /// Create a `SP1Stdin` from a slice of bytes.
+    pub fn from(data: &[u8]) -> Self {
+        Self {
+            buffer: Buffer::from(data),
+        }
+    }
+
+    /// Read a value from the buffer.
+    pub fn read<T: Serialize + DeserializeOwned>(&mut self) -> T {
+        self.buffer.read()
+    }
+
+    /// Read a slice of bytes from the buffer.
+    pub fn read_slice(&mut self, slice: &mut [u8]) {
+        self.buffer.read_slice(slice);
+    }
+
+    /// Write a value to the buffer.
+    pub fn write<T: Serialize>(&mut self, data: &T) {
+        self.buffer.write(data);
+    }
+
+    /// Write a slice of bytes to the buffer.
+    pub fn write_slice(&mut self, slice: &[u8]) {
+        self.buffer.write_slice(slice);
+    }
+}
+
+impl Default for SP1Stdout {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SP1Stdout {
+    /// Create a new `SP1Stdout`.
+    pub fn new() -> Self {
+        Self {
+            buffer: Buffer::new(),
+        }
+    }
+
+    /// Create a `SP1Stdout` from a slice of bytes.
+    pub fn from(data: &[u8]) -> Self {
+        Self {
+            buffer: Buffer::from(data),
+        }
+    }
+
+    /// Read a value from the buffer.    
+    pub fn read<T: Serialize + DeserializeOwned>(&mut self) -> T {
+        self.buffer.read()
+    }
+
+    /// Read a slice of bytes from the buffer.
+    pub fn read_slice(&mut self, slice: &mut [u8]) {
+        self.buffer.read_slice(slice);
+    }
+
+    /// Write a value to the buffer.
+    pub fn write<T: Serialize + DeserializeOwned>(&mut self, data: &T) {
+        self.buffer.write(data);
+    }
+
+    /// Write a slice of bytes to the buffer.
+    pub fn write_slice(&mut self, slice: &[u8]) {
+        self.buffer.write_slice(slice);
+    }
+}
+
+pub mod proof_serde {
+    use serde::{de::DeserializeOwned, Deserialize, Deserializer, Serialize};
+    use sp1_core::stark::{Proof, StarkGenericConfig};
+
+    pub fn serialize<S, SC: StarkGenericConfig + Serialize>(
+        proof: &Proof<SC>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        if serializer.is_human_readable() {
+            let bytes = bincode::serialize(proof).unwrap();
+            let hex_bytes = hex::encode(bytes);
+            serializer.serialize_str(&hex_bytes)
+        } else {
+            proof.serialize(serializer)
+        }
+    }
+
+    pub fn deserialize<'de, D, SC: StarkGenericConfig + DeserializeOwned>(
+        deserializer: D,
+    ) -> Result<Proof<SC>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        if deserializer.is_human_readable() {
+            let hex_bytes = String::deserialize(deserializer).unwrap();
+            let bytes = hex::decode(hex_bytes).unwrap();
+            let proof = bincode::deserialize(&bytes).map_err(serde::de::Error::custom)?;
+            Ok(proof)
+        } else {
+            Proof::<SC>::deserialize(deserializer)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::{SP1ProofWithIO, SP1Prover, SP1Stdin, SP1Verifier};
+
+        pub const FIBONACCI_IO_ELF: &[u8] =
+            include_bytes!("../../examples/fibonacci-io/program/elf/riscv32im-succinct-zkvm-elf");
+
+        /// Tests serialization with a human-readable encoding
+        #[test]
+        fn test_json_roundtrip() {
+            let mut stdin = SP1Stdin::new();
+            stdin.write(&3u32);
+            let proof = SP1Prover::prove(FIBONACCI_IO_ELF, stdin).unwrap();
+            let json = serde_json::to_string(&proof).unwrap();
+            let output = serde_json::from_str::<SP1ProofWithIO<_>>(&json).unwrap();
+            SP1Verifier::verify(FIBONACCI_IO_ELF, &output).unwrap();
+        }
+
+        /// Tests serialization with a binary encoding
+        #[test]
+        fn test_bincode_roundtrip() {
+            let mut stdin = SP1Stdin::new();
+            stdin.write(&3u32);
+            let proof = SP1Prover::prove(FIBONACCI_IO_ELF, stdin).unwrap();
+            let serialized = bincode::serialize(&proof).unwrap();
+            let output = bincode::deserialize::<SP1ProofWithIO<_>>(&serialized).unwrap();
+            SP1Verifier::verify(FIBONACCI_IO_ELF, &output).unwrap();
+        }
+    }
+}

argumentcomputer/sphinx

cli/src/assets/script/main.rs
@@ -1,6 +1,6 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use wp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

cli/src/commands/prove.rs
@@ -3,10 +3,7 @@ use anyhow::Result;
 use clap::Parser;
 use std::time::Instant;
 use std::{env, fs::File, io::Read, path::PathBuf, str::FromStr};
-use wp1_core::{
-    utils::{self},
-    SP1Prover, SP1Stdin,
-};
+use wp1_sdk::{utils, SP1Prover, SP1Stdin};
 
 use crate::{
     build::{build_program, BuildArgs},

examples/chess/script/src/main.rs
@@ -1,4 +1,4 @@
-use wp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

examples/io/script/src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use wp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");

examples/json/script/src/main.rs
@@ -1,7 +1,7 @@
 //! A simple script to generate and verify the proof of a given program.
 
 use lib::{Account, Transaction};
-use wp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 const JSON_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 

recursion/program/src/constraints.rs
@@ -161,9 +161,9 @@ mod tests {
             ShardMainData, ShardProof, StarkGenericConfig, Verifier,
         },
         utils::BabyBearPoseidon2,
-        SP1Prover, SP1Stdin,
     };
     use wp1_recursion_core::runtime::Runtime;
+    use wp1_sdk::{SP1Prover, SP1Stdin};
 
     use p3_challenger::{CanObserve, FieldChallenger};
     use p3_field::PrimeField32;

sdk/src/client.rs
@@ -1,0 +1,113 @@
+use std::{env, time::Duration};
+
+use anyhow::{Ok, Result};
+use futures::future::join_all;
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Client as HttpClient, Url,
+};
+use reqwest_middleware::ClientWithMiddleware as HttpClientWithMiddleware;
+use serde::{de::DeserializeOwned, Serialize};
+use twirp::Client as TwirpClient;
+use wp1_core::stark::StarkGenericConfig;
+
+use crate::{
+    proto::prover::{
+        CreateProofRequest, GetProofStatusRequest, GetProofStatusResponse, ProofStatus,
+        Sp1ProverServiceClient, SubmitProofRequest,
+    },
+    SP1ProofWithIO,
+};
+
+pub struct SP1ProverServiceClient {
+    pub rpc: TwirpClient,
+    pub http: HttpClientWithMiddleware,
+}
+
+const DEFAULT_PROVER_NETWORK_RPC: &str = "https://rpc.succinct.xyz/";
+
+impl SP1ProverServiceClient {
+    pub fn with_token(access_token: String) -> Self {
+        let rpc_url = env::var("PROVER_NETWORK_RPC")
+            .unwrap_or_else(|_| DEFAULT_PROVER_NETWORK_RPC.to_string());
+        Self::with_url(access_token, rpc_url)
+    }
+
+    pub fn with_url(access_token: String, rpc_url: String) -> Self {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "Authorization",
+            HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
+        );
+        let twirp_http_client = HttpClient::builder()
+            .pool_max_idle_per_host(0)
+            .pool_idle_timeout(Duration::from_secs(240))
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let rpc =
+            TwirpClient::new(Url::parse(&rpc_url).unwrap(), twirp_http_client, vec![]).unwrap();
+
+        let http_client = HttpClient::builder()
+            .pool_max_idle_per_host(0)
+            .pool_idle_timeout(Duration::from_secs(240))
+            .build()
+            .unwrap();
+        Self {
+            rpc,
+            http: http_client.into(),
+        }
+    }
+
+    async fn upload_file(&self, url: &str, data: Vec<u8>) -> Result<()> {
+        self.http.put(url).body(data).send().await?;
+        Ok(())
+    }
+
+    pub async fn create_proof(&self, elf: &[u8], stdin: &[u8]) -> Result<String> {
+        let res = self.rpc.create_proof(CreateProofRequest {}).await?;
+
+        let program_bytes = bincode::serialize(elf)?;
+        let stdin_bytes = bincode::serialize(stdin)?;
+        let program_promise = self.upload_file(&res.program_put_url, program_bytes);
+        let stdin_promise = self.upload_file(&res.stdin_put_url, stdin_bytes);
+        let v = vec![program_promise, stdin_promise];
+        let mut results = join_all(v).await;
+        results.pop().expect("Failed to upload stdin")?;
+        results.pop().expect("Failed to upload program")?;
+
+        self.rpc
+            .submit_proof(SubmitProofRequest { id: res.id.clone() })
+            .await?;
+
+        Ok(res.id)
+    }
+
+    pub async fn get_proof_status<SC: StarkGenericConfig + Serialize + DeserializeOwned>(
+        &self,
+        proof_id: &str,
+    ) -> Result<(GetProofStatusResponse, Option<SP1ProofWithIO<SC>>)> {
+        let res = self
+            .rpc
+            .get_proof_status(GetProofStatusRequest {
+                id: proof_id.to_string(),
+            })
+            .await?;
+
+        let result = if res.status() == ProofStatus::Succeeded {
+            let proof = self
+                .http
+                .get(res.result_get_url.clone())
+                .send()
+                .await?
+                .bytes()
+                .await?;
+            Some(bincode::deserialize(&proof).expect("Failed to deserialize proof"))
+        } else {
+            None
+        };
+
+        Ok((res, result))
+    }
+}

sdk/src/io.rs
@@ -1,0 +1,166 @@
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+use wp1_core::utils::Buffer;
+
+/// Standard input for the prover.
+#[derive(Serialize, Deserialize)]
+pub struct SP1Stdin {
+    pub buffer: Buffer,
+}
+
+/// Standard output for the prover.
+#[derive(Serialize, Deserialize)]
+pub struct SP1Stdout {
+    pub buffer: Buffer,
+}
+
+impl Default for SP1Stdin {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SP1Stdin {
+    /// Create a new `SP1Stdin`.
+    pub fn new() -> Self {
+        Self {
+            buffer: Buffer::new(),
+        }
+    }
+
+    /// Create a `SP1Stdin` from a slice of bytes.
+    pub fn from(data: &[u8]) -> Self {
+        Self {
+            buffer: Buffer::from(data),
+        }
+    }
+
+    /// Read a value from the buffer.
+    pub fn read<T: Serialize + DeserializeOwned>(&mut self) -> T {
+        self.buffer.read()
+    }
+
+    /// Read a slice of bytes from the buffer.
+    pub fn read_slice(&mut self, slice: &mut [u8]) {
+        self.buffer.read_slice(slice);
+    }
+
+    /// Write a value to the buffer.
+    pub fn write<T: Serialize>(&mut self, data: &T) {
+        self.buffer.write(data);
+    }
+
+    /// Write a slice of bytes to the buffer.
+    pub fn write_slice(&mut self, slice: &[u8]) {
+        self.buffer.write_slice(slice);
+    }
+}
+
+impl Default for SP1Stdout {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SP1Stdout {
+    /// Create a new `SP1Stdout`.
+    pub fn new() -> Self {
+        Self {
+            buffer: Buffer::new(),
+        }
+    }
+
+    /// Create a `SP1Stdout` from a slice of bytes.
+    pub fn from(data: &[u8]) -> Self {
+        Self {
+            buffer: Buffer::from(data),
+        }
+    }
+
+    /// Read a value from the buffer.    
+    pub fn read<T: Serialize + DeserializeOwned>(&mut self) -> T {
+        self.buffer.read()
+    }
+
+    /// Read a slice of bytes from the buffer.
+    pub fn read_slice(&mut self, slice: &mut [u8]) {
+        self.buffer.read_slice(slice);
+    }
+
+    /// Write a value to the buffer.
+    pub fn write<T: Serialize + DeserializeOwned>(&mut self, data: &T) {
+        self.buffer.write(data);
+    }
+
+    /// Write a slice of bytes to the buffer.
+    pub fn write_slice(&mut self, slice: &[u8]) {
+        self.buffer.write_slice(slice);
+    }
+}
+
+pub mod proof_serde {
+    use serde::{de::DeserializeOwned, Deserialize, Deserializer, Serialize};
+    use wp1_core::stark::{Proof, StarkGenericConfig};
+
+    pub fn serialize<S, SC: StarkGenericConfig + Serialize>(
+        proof: &Proof<SC>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        if serializer.is_human_readable() {
+            let bytes = bincode::serialize(proof).unwrap();
+            let hex_bytes = hex::encode(bytes);
+            serializer.serialize_str(&hex_bytes)
+        } else {
+            proof.serialize(serializer)
+        }
+    }
+
+    pub fn deserialize<'de, D, SC: StarkGenericConfig + DeserializeOwned>(
+        deserializer: D,
+    ) -> Result<Proof<SC>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        if deserializer.is_human_readable() {
+            let hex_bytes = String::deserialize(deserializer).unwrap();
+            let bytes = hex::decode(hex_bytes).unwrap();
+            let proof = bincode::deserialize(&bytes).map_err(serde::de::Error::custom)?;
+            Ok(proof)
+        } else {
+            Proof::<SC>::deserialize(deserializer)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::{SP1ProofWithIO, SP1Prover, SP1Stdin, SP1Verifier};
+
+        pub const FIBONACCI_IO_ELF: &[u8] =
+            include_bytes!("../../examples/fibonacci-io/program/elf/riscv32im-succinct-zkvm-elf");
+
+        /// Tests serialization with a human-readable encoding
+        #[test]
+        fn test_json_roundtrip() {
+            let mut stdin = SP1Stdin::new();
+            stdin.write(&3u32);
+            let proof = SP1Prover::prove(FIBONACCI_IO_ELF, stdin).unwrap();
+            let json = serde_json::to_string(&proof).unwrap();
+            let output = serde_json::from_str::<SP1ProofWithIO<_>>(&json).unwrap();
+            SP1Verifier::verify(FIBONACCI_IO_ELF, &output).unwrap();
+        }
+
+        /// Tests serialization with a binary encoding
+        #[test]
+        fn test_bincode_roundtrip() {
+            let mut stdin = SP1Stdin::new();
+            stdin.write(&3u32);
+            let proof = SP1Prover::prove(FIBONACCI_IO_ELF, stdin).unwrap();
+            let serialized = bincode::serialize(&proof).unwrap();
+            let output = bincode::deserialize::<SP1ProofWithIO<_>>(&serialized).unwrap();
+            SP1Verifier::verify(FIBONACCI_IO_ELF, &output).unwrap();
+        }
+    }
+}