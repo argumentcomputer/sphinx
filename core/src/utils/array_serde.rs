bfb0791cfcdd232f43e549deff775505ec475ac1
François Garillot <francois@garillot.net>
2024-03-29 10:22:39 -0400

    chore: test serde for hybrid_array

--- core/src/utils/array_serde.rs
+++ core/src/utils/array_serde.rs
@@ -26,3 +26,28 @@ impl ArraySerde {
         Array::<T, U>::try_from(&v[..]).map_err(serde::de::Error::custom)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use hybrid_array::{sizes::U4, Array, ArraySize};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+    struct Foo<U: ArraySize>(
+        #[serde(with = "crate::utils::array_serde::ArraySerde")] Array<u32, U>,
+    );
+
+    #[test]
+    fn test_roundtrip_array() {
+        // create an array, serialize with bincode, deserialize, and compare
+        let mut arr = Array::<u32, U4>::default();
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        let foo = Foo::<U4>(arr);
+        let serialized = bincode::serialize(&foo).unwrap();
+        let deserialized: Foo<U4> = bincode::deserialize(&serialized).unwrap();
+        assert_eq!(foo, deserialized);
+    }
+}
