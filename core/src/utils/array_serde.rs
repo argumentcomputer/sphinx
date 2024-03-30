4223dd88931745532594566b84adb89fb1f60bc7
François Garillot <francois@garillot.net>
2024-03-30 15:08:12 -0400

    refactor: Fix serialization with custom methods
    - Introduces custom serialization/deserialization methods for the Array type in `array_serde.rs`.
    - Incorporates `PartiallyInitialized` struct to optimize array deserialization.
    - Enhances efficiency by removing double copying during array deserialization.
    - Discards usage of previously used generic serialize method.

--- core/src/utils/array_serde.rs
+++ core/src/utils/array_serde.rs
@@ -1,7 +1,41 @@
-use hybrid_array::{Array, ArraySize};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt;
+use std::marker::PhantomData;
+use std::mem::MaybeUninit;
 
-// TODO(FG): improve this with serde_with::TryFomIntoRef
+use hybrid_array::{Array, ArraySize};
+use serde::de::{Deserialize, Deserializer, Error, SeqAccess, Visitor};
+use serde::ser::{Serialize, SerializeTuple, Serializer};
+
+pub(crate) struct PartiallyInitialized<T, U: ArraySize>(
+    pub(crate) Option<MaybeUninit<Array<T, U>>>,
+    pub(crate) usize,
+);
+
+impl<T, U: ArraySize> PartiallyInitialized<T, U> {
+    #[inline]
+    pub(crate) fn new() -> Self {
+        PartiallyInitialized(Some(MaybeUninit::uninit()), 0)
+    }
+}
+
+impl<T, U: ArraySize> Drop for PartiallyInitialized<T, U> {
+    fn drop(&mut self) {
+        if !core::mem::needs_drop::<T>() {
+            return;
+        }
+        if let Some(arr) = &mut self.0 {
+            while self.1 > 0 {
+                self.1 -= 1;
+                let offs = self.1;
+                let p = (arr.as_mut_ptr() as *mut T).wrapping_add(offs);
+                unsafe {
+                    core::ptr::drop_in_place::<T>(p);
+                }
+            }
+        }
+    }
+}
+
 pub struct ArraySerde;
 
 impl ArraySerde {
@@ -12,7 +46,11 @@ impl ArraySerde {
     where
         S: Serializer,
     {
-        value.serialize(serializer)
+        let mut seq = serializer.serialize_tuple(value.len())?;
+        for elem in &value[..] {
+            seq.serialize_element(elem)?;
+        }
+        seq.end()
     }
 
     pub fn deserialize<'de, D, T: Clone + Deserialize<'de>, U: ArraySize>(
@@ -21,9 +59,47 @@ impl ArraySerde {
     where
         D: Deserializer<'de>,
     {
-        // TODO: remove the double copy
-        let v = Vec::<T>::deserialize(deserializer)?;
-        Array::<T, U>::try_from(&v[..]).map_err(serde::de::Error::custom)
+        struct ArrayVisitor<T> {
+            element: PhantomData<T>,
+        }
+
+        impl<'de, T, U: ArraySize> Visitor<'de> for ArrayVisitor<Array<T, U>>
+        where
+            T: Deserialize<'de>,
+        {
+            type Value = Array<T, U>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(formatter, "an array of length {}", U::USIZE)
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Array<T, U>, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                unsafe {
+                    let mut arr = PartiallyInitialized::<T, U>::new();
+                    {
+                        let p = arr.0.as_mut().unwrap();
+                        for i in 0..U::USIZE {
+                            let p = (p.as_mut_ptr() as *mut T).wrapping_add(i);
+                            let val = seq
+                                .next_element()?
+                                .ok_or_else(|| Error::invalid_length(i, &self))?;
+                            core::ptr::write(p, val);
+                            arr.1 += 1;
+                        }
+                    }
+                    let initialized = arr.0.take().unwrap().assume_init();
+                    Ok(initialized)
+                }
+            }
+        }
+
+        let visitor = ArrayVisitor {
+            element: PhantomData,
+        };
+        deserializer.deserialize_tuple(U::USIZE, visitor)
     }
 }
 
