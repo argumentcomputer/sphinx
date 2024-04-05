WindOctober/sp1

feat: public values (#455)

core/src/air/public_values.rs
@@ -1,0 +1,103 @@
+use core::fmt::Debug;
+
+use std::{
+    array::IntoIter,
+    ops::{Index, IndexMut},
+};
+
+use p3_field::Field;
+use serde::{Deserialize, Serialize};
+
+use crate::air::WORD_SIZE;
+
+use super::Word;
+
+// TODO:  Create a config struct that will store the num_words setting and the hash function
+//        and initial entropy used.
+const PV_DIGEST_NUM_WORDS: usize = 8;
+
+/// The PublicValuesDigest struct is used to represent the public values digest.  This is the hash of all the
+/// bytes that the guest program has written to public values.
+#[derive(Serialize, Deserialize, Clone, Copy, Default, Debug)]
+pub struct PublicValuesDigest<T>([T; PV_DIGEST_NUM_WORDS]);
+
+/// Conversion from a byte array into a PublicValuesDigest<u32>.
+impl From<&[u8]> for PublicValuesDigest<u32> {
+    fn from(bytes: &[u8]) -> Self {
+        assert!(bytes.len() == PV_DIGEST_NUM_WORDS * WORD_SIZE);
+
+        let mut words = [0u32; PV_DIGEST_NUM_WORDS];
+        for i in 0..PV_DIGEST_NUM_WORDS {
+            words[i] = u32::from_le_bytes(
+                bytes[i * WORD_SIZE..(i + 1) * WORD_SIZE]
+                    .try_into()
+                    .unwrap(),
+            );
+        }
+        Self(words)
+    }
+}
+
+/// Create a PublicValuesDigest with u32 words to one with Word<T> words.
+impl<T: Field> PublicValuesDigest<Word<T>> {
+    pub fn new(orig: PublicValuesDigest<u32>) -> Self {
+        PublicValuesDigest(orig.0.map(|x| x.into()))
+    }
+}
+
+/// Implement the Index trait for PublicValuesDigest to index specific words.
+impl<T> Index<usize> for PublicValuesDigest<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+/// Implement the IntoIterator trait for PublicValuesDigest to iterate over the words.
+impl<T> IntoIterator for PublicValuesDigest<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T, PV_DIGEST_NUM_WORDS>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+/// Conversion into a byte vec.
+impl From<PublicValuesDigest<u32>> for Vec<u8> {
+    fn from(val: PublicValuesDigest<u32>) -> Self {
+        val.0
+            .iter()
+            .flat_map(|word| word.to_le_bytes().to_vec())
+            .collect::<Vec<u8>>()
+    }
+}
+
+/// Conversion into a field vec.
+impl<T: Debug + Copy> From<PublicValuesDigest<Word<T>>> for Vec<T> {
+    fn from(val: PublicValuesDigest<Word<T>>) -> Self {
+        val.0.iter().flat_map(|word| word.0).collect::<Vec<T>>()
+    }
+}
+
+/// Implement the IndexMut trait for PublicValuesDigest to index specific words.
+impl<T> IndexMut<usize> for PublicValuesDigest<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::air::public_values;
+
+    /// Check that the PI_DIGEST_NUM_WORDS number match the zkVM crate's.
+    #[test]
+    fn test_public_values_digest_num_words_consistency_zkvm() {
+        assert_eq!(
+            public_values::PV_DIGEST_NUM_WORDS,
+            sp1_zkvm::PV_DIGEST_NUM_WORDS
+        );
+    }
+}

examples/chess/program/src/main.rs
@@ -16,5 +16,5 @@ pub fn main() {
     let is_valid_move = ChessMove::from_san(&b, &san).is_ok();
 
     // Write whether or not the move is legal
-    sp1_zkvm::io::write(&is_valid_move);
+    sp1_zkvm::io::commit(&is_valid_move);
 }

examples/fibonacci-io/program/src/main.rs
@@ -15,9 +15,12 @@ pub fn main() {
     // from the prover.
     let n = sp1_zkvm::io::read::<u32>();
 
+    // Write n to public input
+    sp1_zkvm::io::commit(&n);
+
     // Compute the n'th fibonacci number, using normal Rust code.
-    let mut a = 0;
-    let mut b = 1;
+    let mut a: u32 = 0;
+    let mut b: u32 = 1;
     let mut sum;
     for _ in 1..n {
         sum = a + b;
@@ -29,6 +32,6 @@ pub fn main() {
     //
     // Behind the scenes, this also compiles down to a custom system call which handles writing
     // outputs to the prover.
-    sp1_zkvm::io::write(&a);
-    sp1_zkvm::io::write(&b);
+    sp1_zkvm::io::commit(&a);
+    sp1_zkvm::io::commit(&b);
 }

examples/io/program/src/main.rs
@@ -23,5 +23,5 @@ pub fn main() {
         b: p1.b && p2.b,
     };
     println!("Addition of 2 points: {:?}", p3);
-    sp1_zkvm::io::write(&p3);
+    sp1_zkvm::io::commit(&p3);
 }

examples/regex/program/src/main.rs
@@ -27,5 +27,5 @@ pub fn main() {
     let result = regex.is_match(&target_string);
 
     // Write the result (true or false) to the output.
-    sp1_zkvm::io::write(&result);
+    sp1_zkvm::io::commit(&result);
 }

argumentcomputer/sphinx

core/src/air/public_values.rs
@@ -1,0 +1,103 @@
+use core::fmt::Debug;
+
+use std::{
+    array::IntoIter,
+    ops::{Index, IndexMut},
+};
+
+use p3_field::Field;
+use serde::{Deserialize, Serialize};
+
+use crate::air::WORD_SIZE;
+
+use super::Word;
+
+// TODO:  Create a config struct that will store the num_words setting and the hash function
+//        and initial entropy used.
+const PV_DIGEST_NUM_WORDS: usize = 8;
+
+/// The PublicValuesDigest struct is used to represent the public values digest.  This is the hash of all the
+/// bytes that the guest program has written to public values.
+#[derive(Serialize, Deserialize, Clone, Copy, Default, Debug)]
+pub struct PublicValuesDigest<T>([T; PV_DIGEST_NUM_WORDS]);
+
+/// Conversion from a byte array into a PublicValuesDigest<u32>.
+impl From<&[u8]> for PublicValuesDigest<u32> {
+    fn from(bytes: &[u8]) -> Self {
+        assert!(bytes.len() == PV_DIGEST_NUM_WORDS * WORD_SIZE);
+
+        let mut words = [0u32; PV_DIGEST_NUM_WORDS];
+        for i in 0..PV_DIGEST_NUM_WORDS {
+            words[i] = u32::from_le_bytes(
+                bytes[i * WORD_SIZE..(i + 1) * WORD_SIZE]
+                    .try_into()
+                    .unwrap(),
+            );
+        }
+        Self(words)
+    }
+}
+
+/// Create a PublicValuesDigest with u32 words to one with Word<T> words.
+impl<T: Field> PublicValuesDigest<Word<T>> {
+    pub fn new(orig: PublicValuesDigest<u32>) -> Self {
+        PublicValuesDigest(orig.0.map(|x| x.into()))
+    }
+}
+
+/// Implement the Index trait for PublicValuesDigest to index specific words.
+impl<T> Index<usize> for PublicValuesDigest<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+/// Implement the IntoIterator trait for PublicValuesDigest to iterate over the words.
+impl<T> IntoIterator for PublicValuesDigest<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T, PV_DIGEST_NUM_WORDS>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+/// Conversion into a byte vec.
+impl From<PublicValuesDigest<u32>> for Vec<u8> {
+    fn from(val: PublicValuesDigest<u32>) -> Self {
+        val.0
+            .iter()
+            .flat_map(|word| word.to_le_bytes().to_vec())
+            .collect::<Vec<u8>>()
+    }
+}
+
+/// Conversion into a field vec.
+impl<T: Debug + Copy> From<PublicValuesDigest<Word<T>>> for Vec<T> {
+    fn from(val: PublicValuesDigest<Word<T>>) -> Self {
+        val.0.iter().flat_map(|word| word.0).collect::<Vec<T>>()
+    }
+}
+
+/// Implement the IndexMut trait for PublicValuesDigest to index specific words.
+impl<T> IndexMut<usize> for PublicValuesDigest<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::air::public_values;
+
+    /// Check that the PI_DIGEST_NUM_WORDS number match the zkVM crate's.
+    #[test]
+    fn test_public_values_digest_num_words_consistency_zkvm() {
+        assert_eq!(
+            public_values::PV_DIGEST_NUM_WORDS,
+            wp1_zkvm::PV_DIGEST_NUM_WORDS
+        );
+    }
+}

examples/chess/program/src/main.rs
@@ -16,5 +16,5 @@ pub fn main() {
     let is_valid_move = ChessMove::from_san(&b, &san).is_ok();
 
     // Write whether or not the move is legal
-    wp1_zkvm::io::write(&is_valid_move);
+    wp1_zkvm::io::commit(&is_valid_move);
 }

examples/fibonacci-io/program/src/main.rs
@@ -15,9 +15,12 @@ pub fn main() {
     // from the prover.
     let n = wp1_zkvm::io::read::<u32>();
 
+    // Write n to public input
+    wp1_zkvm::io::commit(&n);
+
     // Compute the n'th fibonacci number, using normal Rust code.
-    let mut a = 0;
-    let mut b = 1;
+    let mut a: u32 = 0;
+    let mut b: u32 = 1;
     let mut sum;
     for _ in 1..n {
         sum = a + b;
@@ -29,6 +32,6 @@ pub fn main() {
     //
     // Behind the scenes, this also compiles down to a custom system call which handles writing
     // outputs to the prover.
-    wp1_zkvm::io::write(&a);
-    wp1_zkvm::io::write(&b);
+    wp1_zkvm::io::commit(&a);
+    wp1_zkvm::io::commit(&b);
 }

examples/io/program/src/main.rs
@@ -23,5 +23,5 @@ pub fn main() {
         b: p1.b && p2.b,
     };
     println!("Addition of 2 points: {:?}", p3);
-    wp1_zkvm::io::write(&p3);
+    wp1_zkvm::io::commit(&p3);
 }

examples/regex/program/src/main.rs
@@ -27,5 +27,5 @@ pub fn main() {
     let result = regex.is_match(&target_string);
 
     // Write the result (true or false) to the output.
-    wp1_zkvm::io::write(&result);
+    wp1_zkvm::io::commit(&result);
 }