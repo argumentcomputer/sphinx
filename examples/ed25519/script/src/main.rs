WindOctober/sp1

feat: sdk using secp256k1 auth (#483)

examples/ed25519/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
 const ED25519_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -6,10 +6,13 @@ fn main() {
     // Generate proof.
     utils::setup_logger();
     let stdin = SP1Stdin::new();
-    let proof = SP1Prover::prove(ED25519_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(ED25519_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ED25519_ELF, &proof).expect("verification failed");
+    client
+        .verify(ED25519_ELF, &proof)
+        .expect("verification failed");
 
     // Save proof.
     proof

examples/fibonacci-benchmark/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -11,10 +11,11 @@ fn main() {
     let stdin = SP1Stdin::new();
 
     // Generate the proof for the given program.
-    let proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    client.verify(ELF, &proof).expect("verification failed");
 
     // Save the proof.
     proof

examples/fibonacci-io/script/src/main.rs
@@ -1,5 +1,5 @@
 use sha2::{Digest, Sha256};
-use sp1_sdk::{utils, PublicValues, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, ProverClient, PublicValues, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -19,7 +19,8 @@ fn main() {
     stdin.write(&n);
 
     // Generate the proof for the given program and input.
-    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let mut proof = client.prove(ELF, stdin).unwrap();
 
     println!("generated proof");
 
@@ -34,7 +35,7 @@ fn main() {
     println!("b: {}", b);
 
     // Verify proof and public values
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    client.verify(ELF, &proof).expect("verification failed");
 
     let mut pv_hasher = Sha256::new();
     pv_hasher.update(n.to_le_bytes());

examples/rsa/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const REGEX_IO_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -11,10 +11,13 @@ fn main() {
     let stdin = SP1Stdin::new();
 
     // Generate the proof for the given program and input.
-    let proof = SP1Prover::prove(REGEX_IO_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(REGEX_IO_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(REGEX_IO_ELF, &proof).expect("verification failed");
+    client
+        .verify(REGEX_IO_ELF, &proof)
+        .expect("verification failed");
 
     // Save the proof.
     proof

examples/tendermint-benchmark/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
 const ED25519_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -6,10 +6,13 @@ fn main() {
     // Generate proof.
     utils::setup_logger();
     let stdin = SP1Stdin::new();
-    let proof = SP1Prover::prove(ED25519_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client::prove(ED25519_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ED25519_ELF, &proof).expect("verification failed");
+    client
+        .verify(ED25519_ELF, &proof)
+        .expect("verification failed");
 
     // Save proof.
     proof

examples/tendermint/script/src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use reqwest::Client;
-use sp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier, PublicValues};
+use sp1_sdk::{utils, ProverClient, PublicValues, SP1Stdin};
 
 use sha2::{Digest, Sha256};
 use tendermint_light_client_verifier::options::Options;
@@ -55,10 +55,13 @@ async fn main() {
     // let encoded: Vec<u8> = bincode::serialize(&light_block_1).unwrap();
     // let decoded: LightBlock = bincode::deserialize(&encoded[..]).unwrap();
 
-    let proof = SP1Prover::prove(TENDERMINT_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(TENDERMINT_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(TENDERMINT_ELF, &proof).expect("verification failed");
+    client
+        .verify(TENDERMINT_ELF, &proof)
+        .expect("verification failed");
 
     // Verify the public values
     let mut pv_hasher = Sha256::new();

argumentcomputer/sphinx

examples/ed25519/script/src/main.rs
@@ -1,4 +1,4 @@
-use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, ProverClient, SP1Stdin};
 
 const ED25519_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -6,10 +6,13 @@ fn main() {
     // Generate proof.
     utils::setup_logger();
     let stdin = SP1Stdin::new();
-    let proof = SP1Prover::prove(ED25519_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(ED25519_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ED25519_ELF, &proof).expect("verification failed");
+    client
+        .verify(ED25519_ELF, &proof)
+        .expect("verification failed");
 
     // Save proof.
     proof

examples/fibonacci-benchmark/script/src/main.rs
@@ -1,4 +1,4 @@
-use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, ProverClient, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -11,10 +11,11 @@ fn main() {
     let stdin = SP1Stdin::new();
 
     // Generate the proof for the given program.
-    let proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    client.verify(ELF, &proof).expect("verification failed");
 
     // Save the proof.
     proof

examples/fibonacci-io/script/src/main.rs
@@ -1,5 +1,5 @@
 use sha2::{Digest, Sha256};
-use wp1_sdk::{utils, PublicValues, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, ProverClient, PublicValues, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -19,7 +19,8 @@ fn main() {
     stdin.write(&n);
 
     // Generate the proof for the given program and input.
-    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let mut proof = client.prove(ELF, stdin).unwrap();
 
     println!("generated proof");
 
@@ -34,7 +35,7 @@ fn main() {
     println!("b: {}", b);
 
     // Verify proof and public values
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    client.verify(ELF, &proof).expect("verification failed");
 
     let mut pv_hasher = Sha256::new();
     pv_hasher.update(n.to_le_bytes());

examples/rsa/script/src/main.rs
@@ -1,4 +1,4 @@
-use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, ProverClient, SP1Stdin};
 
 /// The ELF we want to execute inside the zkVM.
 const REGEX_IO_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
@@ -11,10 +11,13 @@ fn main() {
     let stdin = SP1Stdin::new();
 
     // Generate the proof for the given program and input.
-    let proof = SP1Prover::prove(REGEX_IO_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(REGEX_IO_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(REGEX_IO_ELF, &proof).expect("verification failed");
+    client
+        .verify(REGEX_IO_ELF, &proof)
+        .expect("verification failed");
 
     // Save the proof.
     proof

examples/tendermint-benchmark/script/src/main.rs
@@ -1,4 +1,4 @@
-use wp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
+use wp1_sdk::{utils, ProverClient, SP1Stdin};
 
 const ED25519_ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -6,10 +6,13 @@ fn main() {
     // Generate proof.
     utils::setup_logger();
     let stdin = SP1Stdin::new();
-    let proof = SP1Prover::prove(ED25519_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client::prove(ED25519_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ED25519_ELF, &proof).expect("verification failed");
+    client
+        .verify(ED25519_ELF, &proof)
+        .expect("verification failed");
 
     // Save proof.
     proof

examples/tendermint/script/src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use reqwest::Client;
-use wp1_sdk::{utils, SP1Prover, SP1Stdin, SP1Verifier, PublicValues};
+use wp1_sdk::{utils, ProverClient, PublicValues, SP1Stdin};
 
 use sha2::{Digest, Sha256};
 use tendermint_light_client_verifier::options::Options;
@@ -55,10 +55,13 @@ async fn main() {
     // let encoded: Vec<u8> = bincode::serialize(&light_block_1).unwrap();
     // let decoded: LightBlock = bincode::deserialize(&encoded[..]).unwrap();
 
-    let proof = SP1Prover::prove(TENDERMINT_ELF, stdin).expect("proving failed");
+    let client = ProverClient::new();
+    let proof = client.prove(TENDERMINT_ELF, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(TENDERMINT_ELF, &proof).expect("verification failed");
+    client
+        .verify(TENDERMINT_ELF, &proof)
+        .expect("verification failed");
 
     // Verify the public values
     let mut pv_hasher = Sha256::new();