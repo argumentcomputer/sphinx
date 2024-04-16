succinctlabs/sp1

feat: recursion optimizations + compiler cleanup (#499)

recursion/circuit/src/poseidon2.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 use p3_field::AbstractField;
 use p3_field::Field;
 use sp1_recursion_compiler::ir::Felt;
-use sp1_recursion_compiler::ir::{Builder, Config, DslIR, Var};
+use sp1_recursion_compiler::ir::{Builder, Config, DslIr, Var};
 
 use crate::challenger::reduce_32;
 use crate::types::OuterDigest;
@@ -20,7 +20,7 @@ pub trait Poseidon2CircuitBuilder<C: Config> {
 
 impl<C: Config> Poseidon2CircuitBuilder<C> for Builder<C> {
     fn p2_permute_mut(&mut self, state: [Var<C::N>; SPONGE_SIZE]) {
-        self.push(DslIR::CircuitPoseidon2Permute(state))
+        self.push(DslIr::CircuitPoseidon2Permute(state))
     }
 
     fn p2_hash(&mut self, input: &[Felt<C::F>]) -> OuterDigest<C> {
@@ -59,9 +59,9 @@ pub mod tests {
     use p3_field::AbstractField;
     use p3_symmetric::{CryptographicHasher, Permutation, PseudoCompressionFunction};
     use serial_test::serial;
-    use sp1_recursion_compiler::constraints::{gnark_ffi, ConstraintBackend};
+    use sp1_recursion_compiler::config::OuterConfig;
+    use sp1_recursion_compiler::constraints::{gnark_ffi, ConstraintCompiler};
     use sp1_recursion_compiler::ir::{Builder, Felt, Var};
-    use sp1_recursion_compiler::OuterConfig;
     use sp1_recursion_core::stark::config::{outer_perm, OuterCompress, OuterHash};
 
     use crate::poseidon2::Poseidon2CircuitBuilder;
@@ -89,9 +89,9 @@ pub mod tests {
         builder.assert_var_eq(b, output[1]);
         builder.assert_var_eq(c, output[2]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(constraints);
+        gnark_ffi::execute(constraints);
     }
 
     #[test]
@@ -123,9 +123,9 @@ pub mod tests {
 
         builder.assert_var_eq(result[0], output[0]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(constraints);
+        gnark_ffi::execute(constraints);
     }
 
     #[test]
@@ -144,8 +144,8 @@ pub mod tests {
         let result = builder.p2_compress([a, b]);
         builder.assert_var_eq(result[0], gt[0]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(constraints);
+        gnark_ffi::execute(constraints);
     }
 }

recursion/compiler/tests/conditionals.rs
@@ -2,8 +2,8 @@ use p3_baby_bear::BabyBear;
 use p3_field::extension::BinomialExtensionField;
 use p3_field::AbstractField;
 use sp1_core::utils::BabyBearPoseidon2;
-use sp1_recursion_compiler::asm::VmBuilder;
-use sp1_recursion_compiler::prelude::*;
+use sp1_recursion_compiler::asm::AsmBuilder;
+use sp1_recursion_compiler::ir::Var;
 use sp1_recursion_core::runtime::Runtime;
 
 #[test]
@@ -11,7 +11,7 @@ fn test_compiler_conditionals() {
     type SC = BabyBearPoseidon2;
     type F = BabyBear;
     type EF = BinomialExtensionField<BabyBear, 4>;
-    let mut builder = VmBuilder::<F, EF>::default();
+    let mut builder = AsmBuilder::<F, EF>::default();
 
     let zero: Var<_> = builder.eval(F::zero());
     let one: Var<_> = builder.eval(F::one());
@@ -50,7 +50,43 @@ fn test_compiler_conditionals() {
     );
     builder.assert_var_eq(c, F::zero());
 
-    let code = builder.compile_to_asm();
+    let code = builder.compile_asm();
+    println!("{}", code);
+    // let program = builder.compile();
+    let program = code.machine_code();
+
+    let config = SC::default();
+    let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());
+    runtime.run();
+}
+
+#[test]
+fn test_compiler_conditionals_v2() {
+    type SC = BabyBearPoseidon2;
+    type F = BabyBear;
+    type EF = BinomialExtensionField<BabyBear, 4>;
+    let mut builder = AsmBuilder::<F, EF>::default();
+
+    let zero: Var<_> = builder.eval(F::zero());
+    let one: Var<_> = builder.eval(F::one());
+    let two: Var<_> = builder.eval(F::two());
+    let three: Var<_> = builder.eval(F::from_canonical_u32(3));
+    let four: Var<_> = builder.eval(F::from_canonical_u32(4));
+
+    let c: Var<_> = builder.eval(F::zero());
+    builder.if_eq(zero, zero).then(|builder| {
+        builder.if_eq(one, one).then(|builder| {
+            builder.if_eq(two, two).then(|builder| {
+                builder.if_eq(three, three).then(|builder| {
+                    builder
+                        .if_eq(four, four)
+                        .then(|builder| builder.assign(c, F::one()))
+                })
+            })
+        })
+    });
+
+    let code = builder.compile_asm();
     println!("{}", code);
     // let program = builder.compile();
     let program = code.machine_code();

recursion/compiler/tests/public_values.rs
@@ -1,7 +1,7 @@
 use p3_field::AbstractField;
 use sp1_core::stark::StarkGenericConfig;
 use sp1_core::utils::BabyBearPoseidon2;
-use sp1_recursion_compiler::asm::VmBuilder;
+use sp1_recursion_compiler::asm::AsmBuilder;
 use sp1_recursion_compiler::prelude::*;
 use sp1_recursion_core::runtime::Runtime;
 
@@ -10,7 +10,7 @@ fn test_compiler_public_values() {
     type SC = BabyBearPoseidon2;
     type F = <SC as StarkGenericConfig>::Val;
     type EF = <SC as StarkGenericConfig>::Challenge;
-    let mut builder = VmBuilder::<F, EF>::default();
+    let mut builder = AsmBuilder::<F, EF>::default();
 
     let a: Felt<_> = builder.constant(F::from_canonical_u32(10));
     let b: Felt<_> = builder.constant(F::from_canonical_u32(20));
@@ -23,7 +23,7 @@ fn test_compiler_public_values() {
     builder.write_public_values(&var_array);
     builder.commit_public_values();
 
-    let program = builder.compile();
+    let program = builder.compile_program();
 
     let config = SC::default();
     let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());

recursion/program/src/challenger.rs
@@ -242,8 +242,8 @@ mod tests {
     use p3_field::PrimeField32;
     use sp1_core::stark::StarkGenericConfig;
     use sp1_core::utils::BabyBearPoseidon2;
+    use sp1_recursion_compiler::asm::AsmBuilder;
     use sp1_recursion_compiler::asm::AsmConfig;
-    use sp1_recursion_compiler::asm::VmBuilder;
     use sp1_recursion_compiler::ir::Felt;
     use sp1_recursion_compiler::ir::Usize;
     use sp1_recursion_compiler::ir::Var;
@@ -267,7 +267,7 @@ mod tests {
         let result: F = challenger.sample();
         println!("expected result: {}", result);
 
-        let mut builder = VmBuilder::<F, EF>::default();
+        let mut builder = AsmBuilder::<F, EF>::default();
 
         let width: Var<_> = builder.eval(F::from_canonical_usize(PERMUTATION_WIDTH));
         let mut challenger = DuplexChallengerVariable::<AsmConfig<F, EF>> {
@@ -288,7 +288,7 @@ mod tests {
         let expected_result: Felt<_> = builder.eval(result);
         builder.assert_felt_eq(expected_result, element);
 
-        let program = builder.compile();
+        let program = builder.compile_program();
 
         let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());
         runtime.run();

argumentcomputer/sphinx

recursion/circuit/src/poseidon2.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 use p3_field::AbstractField;
 use p3_field::Field;
 use wp1_recursion_compiler::ir::Felt;
-use wp1_recursion_compiler::ir::{Builder, Config, DslIR, Var};
+use wp1_recursion_compiler::ir::{Builder, Config, DslIr, Var};
 
 use crate::challenger::reduce_32;
 use crate::types::OuterDigest;
@@ -20,7 +20,7 @@ pub trait Poseidon2CircuitBuilder<C: Config> {
 
 impl<C: Config> Poseidon2CircuitBuilder<C> for Builder<C> {
     fn p2_permute_mut(&mut self, state: [Var<C::N>; SPONGE_SIZE]) {
-        self.push(DslIR::CircuitPoseidon2Permute(state))
+        self.push(DslIr::CircuitPoseidon2Permute(state))
     }
 
     fn p2_hash(&mut self, input: &[Felt<C::F>]) -> OuterDigest<C> {
@@ -59,9 +59,9 @@ pub mod tests {
     use p3_field::AbstractField;
     use p3_symmetric::{CryptographicHasher, Permutation, PseudoCompressionFunction};
     use serial_test::serial;
-    use wp1_recursion_compiler::constraints::{gnark_ffi, ConstraintBackend};
+    use wp1_recursion_compiler::config::OuterConfig;
+    use wp1_recursion_compiler::constraints::{gnark_ffi, ConstraintCompiler};
     use wp1_recursion_compiler::ir::{Builder, Felt, Var};
-    use wp1_recursion_compiler::OuterConfig;
     use wp1_recursion_core::stark::config::{outer_perm, OuterCompress, OuterHash};
 
     use crate::poseidon2::Poseidon2CircuitBuilder;
@@ -89,9 +89,9 @@ pub mod tests {
         builder.assert_var_eq(b, output[1]);
         builder.assert_var_eq(c, output[2]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(&constraints);
+        gnark_ffi::execute(&constraints);
     }
 
     #[test]
@@ -123,9 +123,9 @@ pub mod tests {
 
         builder.assert_var_eq(result[0], output[0]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(&constraints);
+        gnark_ffi::execute(&constraints);
     }
 
     #[test]
@@ -144,8 +144,8 @@ pub mod tests {
         let result = builder.p2_compress([a, b]);
         builder.assert_var_eq(result[0], gt[0]);
 
-        let mut backend = ConstraintBackend::<OuterConfig>::default();
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
         let constraints = backend.emit(builder.operations);
-        gnark_ffi::test_circuit(&constraints);
+        gnark_ffi::execute(&constraints);
     }
 }

recursion/compiler/tests/conditionals.rs
@@ -2,8 +2,8 @@ use p3_baby_bear::BabyBear;
 use p3_field::extension::BinomialExtensionField;
 use p3_field::AbstractField;
 use wp1_core::utils::BabyBearPoseidon2;
-use wp1_recursion_compiler::asm::VmBuilder;
-use wp1_recursion_compiler::prelude::*;
+use wp1_recursion_compiler::asm::AsmBuilder;
+use wp1_recursion_compiler::ir::Var;
 use wp1_recursion_core::runtime::Runtime;
 
 #[test]
@@ -11,7 +11,7 @@ fn test_compiler_conditionals() {
     type SC = BabyBearPoseidon2;
     type F = BabyBear;
     type EF = BinomialExtensionField<BabyBear, 4>;
-    let mut builder = VmBuilder::<F, EF>::default();
+    let mut builder = AsmBuilder::<F, EF>::default();
 
     let zero: Var<_> = builder.eval(F::zero());
     let one: Var<_> = builder.eval(F::one());
@@ -50,7 +50,43 @@ fn test_compiler_conditionals() {
     );
     builder.assert_var_eq(c, F::zero());
 
-    let code = builder.compile_to_asm();
+    let code = builder.compile_asm();
+    println!("{}", code);
+    // let program = builder.compile();
+    let program = code.machine_code();
+
+    let config = SC::default();
+    let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());
+    runtime.run();
+}
+
+#[test]
+fn test_compiler_conditionals_v2() {
+    type SC = BabyBearPoseidon2;
+    type F = BabyBear;
+    type EF = BinomialExtensionField<BabyBear, 4>;
+    let mut builder = AsmBuilder::<F, EF>::default();
+
+    let zero: Var<_> = builder.eval(F::zero());
+    let one: Var<_> = builder.eval(F::one());
+    let two: Var<_> = builder.eval(F::two());
+    let three: Var<_> = builder.eval(F::from_canonical_u32(3));
+    let four: Var<_> = builder.eval(F::from_canonical_u32(4));
+
+    let c: Var<_> = builder.eval(F::zero());
+    builder.if_eq(zero, zero).then(|builder| {
+        builder.if_eq(one, one).then(|builder| {
+            builder.if_eq(two, two).then(|builder| {
+                builder.if_eq(three, three).then(|builder| {
+                    builder
+                        .if_eq(four, four)
+                        .then(|builder| builder.assign(&c, F::one()))
+                })
+            })
+        })
+    });
+
+    let code = builder.compile_asm();
     println!("{}", code);
     // let program = builder.compile();
     let program = code.machine_code();

recursion/compiler/tests/public_values.rs
@@ -1,7 +1,7 @@
 use p3_field::AbstractField;
 use wp1_core::stark::StarkGenericConfig;
 use wp1_core::utils::BabyBearPoseidon2;
-use wp1_recursion_compiler::asm::VmBuilder;
+use wp1_recursion_compiler::asm::AsmBuilder;
 use wp1_recursion_compiler::prelude::*;
 use wp1_recursion_core::runtime::Runtime;
 
@@ -10,7 +10,7 @@ fn test_compiler_public_values() {
     type SC = BabyBearPoseidon2;
     type F = <SC as StarkGenericConfig>::Val;
     type EF = <SC as StarkGenericConfig>::Challenge;
-    let mut builder = VmBuilder::<F, EF>::default();
+    let mut builder = AsmBuilder::<F, EF>::default();
 
     let a: Felt<_> = builder.constant(F::from_canonical_u32(10));
     let b: Felt<_> = builder.constant(F::from_canonical_u32(20));
@@ -23,7 +23,7 @@ fn test_compiler_public_values() {
     builder.write_public_values(&var_array);
     builder.commit_public_values();
 
-    let program = builder.compile();
+    let program = builder.compile_program();
 
     let config = SC::default();
     let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());

recursion/program/src/challenger.rs
@@ -242,8 +242,8 @@ mod tests {
     use p3_field::PrimeField32;
     use wp1_core::stark::StarkGenericConfig;
     use wp1_core::utils::BabyBearPoseidon2;
+    use wp1_recursion_compiler::asm::AsmBuilder;
     use wp1_recursion_compiler::asm::AsmConfig;
-    use wp1_recursion_compiler::asm::VmBuilder;
     use wp1_recursion_compiler::ir::Felt;
     use wp1_recursion_compiler::ir::Usize;
     use wp1_recursion_compiler::ir::Var;
@@ -267,7 +267,7 @@ mod tests {
         let result: F = challenger.sample();
         println!("expected result: {}", result);
 
-        let mut builder = VmBuilder::<F, EF>::default();
+        let mut builder = AsmBuilder::<F, EF>::default();
 
         let width: Var<_> = builder.eval(F::from_canonical_usize(PERMUTATION_WIDTH));
         let mut challenger = DuplexChallengerVariable::<AsmConfig<F, EF>> {
@@ -288,7 +288,7 @@ mod tests {
         let expected_result: Felt<_> = builder.eval(result);
         builder.assert_felt_eq(expected_result, element);
 
-        let program = builder.compile();
+        let program = builder.compile_program();
 
         let mut runtime = Runtime::<F, EF, _>::new(&program, config.perm.clone());
         runtime.run();