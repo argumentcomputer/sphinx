WindOctober/sp1

wip(recursion): reduce program (#497)

recursion/program/src/challenger.rs
@@ -1,6 +1,10 @@
 use p3_field::AbstractField;
 
-use sp1_recursion_compiler::prelude::{Array, Builder, Config, Ext, Felt, Usize, Var};
+use sp1_recursion_compiler::prelude::MemIndex;
+use sp1_recursion_compiler::prelude::MemVariable;
+use sp1_recursion_compiler::prelude::Ptr;
+use sp1_recursion_compiler::prelude::Variable;
+use sp1_recursion_compiler::prelude::{Array, Builder, Config, DslVariable, Ext, Felt, Usize, Var};
 use sp1_recursion_core::runtime::{DIGEST_SIZE, PERMUTATION_WIDTH};
 
 use crate::fri::types::DigestVariable;
@@ -37,7 +41,7 @@ pub trait CanSampleBitsVariable<C: Config> {
 }
 
 /// Reference: https://github.com/Plonky3/Plonky3/blob/4809fa7bedd9ba8f6f5d3267b1592618e3776c57/challenger/src/duplex_challenger.rs#L10
-#[derive(Clone)]
+#[derive(Clone, DslVariable)]
 pub struct DuplexChallengerVariable<C: Config> {
     pub sponge_state: Array<C, Felt<C::F>>,
     pub nb_inputs: Var<C::N>,
@@ -54,6 +58,34 @@ impl<C: Config> DuplexChallengerVariable<C> {
             input_buffer: builder.dyn_array(PERMUTATION_WIDTH),
             nb_outputs: builder.eval(C::N::zero()),
             output_buffer: builder.dyn_array(PERMUTATION_WIDTH),
+        }
+    }
+
+    /// Creates a new challenger with the same state as an existing challenger.
+    pub fn as_clone(&self, builder: &mut Builder<C>) -> Self {
+        let mut sponge_state = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.sponge_state, i);
+            builder.set(&mut sponge_state, i, element);
+        });
+        let nb_inputs = builder.eval(self.nb_inputs);
+        let mut input_buffer = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.input_buffer, i);
+            builder.set(&mut input_buffer, i, element);
+        });
+        let nb_outputs = builder.eval(self.nb_outputs);
+        let mut output_buffer = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.output_buffer, i);
+            builder.set(&mut output_buffer, i, element);
+        });
+        DuplexChallengerVariable::<C> {
+            sponge_state,
+            nb_inputs,
+            input_buffer,
+            nb_outputs,
+            output_buffer,
         }
     }
 

recursion/program/src/hints.rs
@@ -1,4 +1,14 @@
+use crate::challenger::DuplexChallengerVariable;
+use crate::fri::TwoAdicMultiplicativeCosetVariable;
+use crate::types::{
+    AirOpenedValuesVariable, ChipOpenedValuesVariable, PublicValuesVariable,
+    ShardCommitmentVariable, ShardOpenedValuesVariable, ShardProofVariable, VerifyingKeyVariable,
+};
+use p3_challenger::DuplexChallenger;
+use p3_commit::TwoAdicMultiplicativeCoset;
+use p3_field::TwoAdicField;
 use p3_field::{AbstractExtensionField, AbstractField};
+use sp1_core::stark::VerifyingKey;
 use sp1_core::{
     air::{PublicValues, Word},
     stark::{AirOpenedValues, ChipOpenedValues, ShardCommitment, ShardOpenedValues, ShardProof},
@@ -9,14 +19,11 @@ use sp1_recursion_compiler::{
 };
 use sp1_recursion_core::{
     air::Block,
-    stark::config::{InnerChallenge, InnerDigest, InnerDigestHash, InnerPcsProof, InnerVal},
+    stark::config::{
+        InnerChallenge, InnerDigest, InnerDigestHash, InnerPcsProof, InnerPerm, InnerVal,
+    },
 };
 use sp1_sdk::utils::BabyBearPoseidon2;
-
-use crate::types::{
-    AirOpenedValuesVariable, ChipOpenedValuesVariable, PublicValuesVariable,
-    ShardCommitmentVariable, ShardOpenedValuesVariable, ShardProofVariable,
-};
 
 pub trait Hintable<C: Config> {
     type HintVariable: MemVariable<C>;
@@ -61,6 +68,68 @@ impl Hintable<C> for InnerChallenge {
 
     fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
         vec![vec![Block::from((*self).as_base_slice())]]
+    }
+}
+
+impl Hintable<C> for TwoAdicMultiplicativeCoset<InnerVal> {
+    type HintVariable = TwoAdicMultiplicativeCosetVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let log_n = usize::read(builder);
+        let shift = InnerVal::read(builder);
+        let g_val = InnerVal::read(builder);
+        let size = usize::read(builder);
+
+        // Initialize a domain.
+        TwoAdicMultiplicativeCosetVariable::<C> {
+            log_n,
+            size,
+            shift,
+            g: g_val,
+        }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut vec = Vec::new();
+        vec.extend(usize::write(&self.log_n));
+        vec.extend(InnerVal::write(&self.shift));
+        vec.extend(InnerVal::write(&InnerVal::two_adic_generator(self.log_n)));
+        vec.extend(usize::write(&(1usize << (self.log_n))));
+        vec
+    }
+}
+
+trait VecAutoHintable<C: Config>: Hintable<C> {}
+
+impl VecAutoHintable<C> for ShardProof<BabyBearPoseidon2> {}
+impl VecAutoHintable<C> for TwoAdicMultiplicativeCoset<InnerVal> {}
+impl VecAutoHintable<C> for Vec<usize> {}
+
+impl<I: VecAutoHintable<C>> Hintable<C> for Vec<I> {
+    type HintVariable = Array<C, I::HintVariable>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let len = builder.hint_var();
+        let mut arr = builder.dyn_array(len);
+        builder.range(0, len).for_each(|i, builder| {
+            let hint = I::read(builder);
+            builder.set(&mut arr, i, hint);
+        });
+        arr
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+
+        let len = InnerVal::from_canonical_usize(self.len());
+        stream.push(vec![len.into()]);
+
+        self.iter().for_each(|i| {
+            let comm = I::write(i);
+            stream.extend(comm);
+        });
+
+        stream
     }
 }
 
@@ -285,6 +354,51 @@ impl Hintable<C> for PublicValues<u32, u32> {
         stream.extend(F::from_canonical_u32(self.start_pc).write());
         stream.extend(F::from_canonical_u32(self.next_pc).write());
         stream.extend(F::from_canonical_u32(self.exit_code).write());
+        stream
+    }
+}
+
+impl Hintable<C> for DuplexChallenger<InnerVal, InnerPerm, 16> {
+    type HintVariable = DuplexChallengerVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let sponge_state = builder.hint_felts();
+        let nb_inputs = builder.hint_var();
+        let input_buffer = builder.hint_felts();
+        let nb_outputs = builder.hint_var();
+        let output_buffer = builder.hint_felts();
+        DuplexChallengerVariable {
+            sponge_state,
+            nb_inputs,
+            input_buffer,
+            nb_outputs,
+            output_buffer,
+        }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+        stream.extend(self.sponge_state.to_vec().write());
+        stream.extend(self.input_buffer.len().write());
+        stream.extend(self.input_buffer.write());
+        stream.extend(self.output_buffer.len().write());
+        stream.extend(self.output_buffer.write());
+        stream
+    }
+}
+
+impl Hintable<C> for VerifyingKey<BabyBearPoseidon2> {
+    type HintVariable = VerifyingKeyVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let commitment = InnerDigest::read(builder);
+        VerifyingKeyVariable { commitment }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+        let h: InnerDigest = self.commit.into();
+        stream.extend(h.write());
         stream
     }
 }

argumentcomputer/sphinx

recursion/program/src/challenger.rs
@@ -1,6 +1,10 @@
 use p3_field::AbstractField;
 
-use wp1_recursion_compiler::prelude::{Array, Builder, Config, Ext, Felt, Usize, Var};
+use wp1_recursion_compiler::prelude::MemIndex;
+use wp1_recursion_compiler::prelude::MemVariable;
+use wp1_recursion_compiler::prelude::Ptr;
+use wp1_recursion_compiler::prelude::Variable;
+use wp1_recursion_compiler::prelude::{Array, Builder, Config, DslVariable, Ext, Felt, Usize, Var};
 use wp1_recursion_core::runtime::{DIGEST_SIZE, PERMUTATION_WIDTH};
 
 use crate::fri::types::DigestVariable;
@@ -37,7 +41,7 @@ pub trait CanSampleBitsVariable<C: Config> {
 }
 
 /// Reference: https://github.com/Plonky3/Plonky3/blob/4809fa7bedd9ba8f6f5d3267b1592618e3776c57/challenger/src/duplex_challenger.rs#L10
-#[derive(Clone)]
+#[derive(Clone, DslVariable)]
 pub struct DuplexChallengerVariable<C: Config> {
     pub sponge_state: Array<C, Felt<C::F>>,
     pub nb_inputs: Var<C::N>,
@@ -54,6 +58,34 @@ impl<C: Config> DuplexChallengerVariable<C> {
             input_buffer: builder.dyn_array(PERMUTATION_WIDTH),
             nb_outputs: builder.eval(C::N::zero()),
             output_buffer: builder.dyn_array(PERMUTATION_WIDTH),
+        }
+    }
+
+    /// Creates a new challenger with the same state as an existing challenger.
+    pub fn as_clone(&self, builder: &mut Builder<C>) -> Self {
+        let mut sponge_state = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.sponge_state, i);
+            builder.set(&mut sponge_state, i, element);
+        });
+        let nb_inputs = builder.eval(self.nb_inputs);
+        let mut input_buffer = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.input_buffer, i);
+            builder.set(&mut input_buffer, i, element);
+        });
+        let nb_outputs = builder.eval(self.nb_outputs);
+        let mut output_buffer = builder.dyn_array(PERMUTATION_WIDTH);
+        builder.range(0, PERMUTATION_WIDTH).for_each(|i, builder| {
+            let element = builder.get(&self.output_buffer, i);
+            builder.set(&mut output_buffer, i, element);
+        });
+        DuplexChallengerVariable::<C> {
+            sponge_state,
+            nb_inputs,
+            input_buffer,
+            nb_outputs,
+            output_buffer,
         }
     }
 

recursion/program/src/hints.rs
@@ -1,4 +1,14 @@
+use crate::challenger::DuplexChallengerVariable;
+use crate::fri::TwoAdicMultiplicativeCosetVariable;
+use crate::types::{
+    AirOpenedValuesVariable, ChipOpenedValuesVariable, PublicValuesVariable,
+    ShardCommitmentVariable, ShardOpenedValuesVariable, ShardProofVariable, VerifyingKeyVariable,
+};
+use p3_challenger::DuplexChallenger;
+use p3_commit::TwoAdicMultiplicativeCoset;
+use p3_field::TwoAdicField;
 use p3_field::{AbstractExtensionField, AbstractField};
+use wp1_core::stark::VerifyingKey;
 use wp1_core::{
     air::{PublicValues, Word},
     stark::{AirOpenedValues, ChipOpenedValues, ShardCommitment, ShardOpenedValues, ShardProof},
@@ -9,14 +19,11 @@ use wp1_recursion_compiler::{
 };
 use wp1_recursion_core::{
     air::Block,
-    stark::config::{InnerChallenge, InnerDigest, InnerDigestHash, InnerPcsProof, InnerVal},
+    stark::config::{
+        InnerChallenge, InnerDigest, InnerDigestHash, InnerPcsProof, InnerPerm, InnerVal,
+    },
 };
 use wp1_sdk::utils::BabyBearPoseidon2;
-
-use crate::types::{
-    AirOpenedValuesVariable, ChipOpenedValuesVariable, PublicValuesVariable,
-    ShardCommitmentVariable, ShardOpenedValuesVariable, ShardProofVariable,
-};
 
 pub trait Hintable<C: Config> {
     type HintVariable: MemVariable<C>;
@@ -61,6 +68,68 @@ impl Hintable<C> for InnerChallenge {
 
     fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
         vec![vec![Block::try_from((*self).as_base_slice()).unwrap()]]
+    }
+}
+
+impl Hintable<C> for TwoAdicMultiplicativeCoset<InnerVal> {
+    type HintVariable = TwoAdicMultiplicativeCosetVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let log_n = usize::read(builder);
+        let shift = InnerVal::read(builder);
+        let g_val = InnerVal::read(builder);
+        let size = usize::read(builder);
+
+        // Initialize a domain.
+        TwoAdicMultiplicativeCosetVariable::<C> {
+            log_n,
+            size,
+            shift,
+            g: g_val,
+        }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut vec = Vec::new();
+        vec.extend(usize::write(&self.log_n));
+        vec.extend(InnerVal::write(&self.shift));
+        vec.extend(InnerVal::write(&InnerVal::two_adic_generator(self.log_n)));
+        vec.extend(usize::write(&(1usize << (self.log_n))));
+        vec
+    }
+}
+
+trait VecAutoHintable<C: Config>: Hintable<C> {}
+
+impl VecAutoHintable<C> for ShardProof<BabyBearPoseidon2> {}
+impl VecAutoHintable<C> for TwoAdicMultiplicativeCoset<InnerVal> {}
+impl VecAutoHintable<C> for Vec<usize> {}
+
+impl<I: VecAutoHintable<C>> Hintable<C> for Vec<I> {
+    type HintVariable = Array<C, I::HintVariable>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let len = builder.hint_var();
+        let mut arr = builder.dyn_array(len);
+        builder.range(0, len).for_each(|i, builder| {
+            let hint = I::read(builder);
+            builder.set(&mut arr, i, hint);
+        });
+        arr
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+
+        let len = InnerVal::from_canonical_usize(self.len());
+        stream.push(vec![len.into()]);
+
+        for i in self.iter() {
+            let comm = I::write(i);
+            stream.extend(comm);
+        }
+
+        stream
     }
 }
 
@@ -285,6 +354,51 @@ impl Hintable<C> for PublicValues<u32, u32> {
         stream.extend(F::from_canonical_u32(self.start_pc).write());
         stream.extend(F::from_canonical_u32(self.next_pc).write());
         stream.extend(F::from_canonical_u32(self.exit_code).write());
+        stream
+    }
+}
+
+impl Hintable<C> for DuplexChallenger<InnerVal, InnerPerm, 16> {
+    type HintVariable = DuplexChallengerVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let sponge_state = builder.hint_felts();
+        let nb_inputs = builder.hint_var();
+        let input_buffer = builder.hint_felts();
+        let nb_outputs = builder.hint_var();
+        let output_buffer = builder.hint_felts();
+        DuplexChallengerVariable {
+            sponge_state,
+            nb_inputs,
+            input_buffer,
+            nb_outputs,
+            output_buffer,
+        }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+        stream.extend(self.sponge_state.to_vec().write());
+        stream.extend(self.input_buffer.len().write());
+        stream.extend(self.input_buffer.write());
+        stream.extend(self.output_buffer.len().write());
+        stream.extend(self.output_buffer.write());
+        stream
+    }
+}
+
+impl Hintable<C> for VerifyingKey<BabyBearPoseidon2> {
+    type HintVariable = VerifyingKeyVariable<C>;
+
+    fn read(builder: &mut Builder<C>) -> Self::HintVariable {
+        let commitment = InnerDigest::read(builder);
+        VerifyingKeyVariable { commitment }
+    }
+
+    fn write(&self) -> Vec<Vec<Block<<C as Config>::F>>> {
+        let mut stream = Vec::new();
+        let h: InnerDigest = self.commit.into();
+        stream.extend(h.write());
         stream
     }
 }