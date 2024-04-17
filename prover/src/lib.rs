succinctlabs/sp1

feat: 0 cycle input for recursion program (#510)

prover/src/lib.rs
@@ -5,7 +5,7 @@
 use p3_baby_bear::BabyBear;
 use p3_challenger::CanObserve;
 use p3_commit::TwoAdicMultiplicativeCoset;
-use p3_field::PrimeField32;
+use p3_field::{AbstractField, PrimeField32};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use sp1_core::{
     air::MachineAir,
@@ -17,6 +17,7 @@ use sp1_core::{
     utils::{run_and_prove, BabyBearPoseidon2},
 };
 use sp1_recursion_core::{
+    cpu::Instruction,
     runtime::{RecursionProgram, Runtime},
     stark::{config::BabyBearPoseidon2Inner, RecursionAir},
 };
@@ -30,6 +31,7 @@ type InnerEF = <InnerSC as StarkGenericConfig>::Challenge;
 type OuterSC = BabyBearPoseidon2Inner;
 
 pub struct SP1ProverImpl {
+    reduce_setup_program: RecursionProgram<BabyBear>,
     reduce_program: RecursionProgram<BabyBear>,
     reduce_vk: VerifyingKey<InnerSC>,
 }
@@ -81,9 +83,21 @@ fn get_preprocessed_data<SC: StarkGenericConfig, A: MachineAir<Val<SC>>>(
 impl SP1ProverImpl {
     pub fn new() -> Self {
         // TODO: load from serde
-        let reduce_program = build_reduce();
+        let reduce_setup_program = build_reduce(true);
+        let mut reduce_program = build_reduce(false);
+        reduce_program.instructions[0] = Instruction::new(
+            sp1_recursion_core::runtime::Opcode::ADD,
+            BabyBear::zero(),
+            [BabyBear::zero(); 4],
+            [BabyBear::zero(); 4],
+            BabyBear::zero(),
+            BabyBear::zero(),
+            false,
+            false,
+        );
         let (_, reduce_vk) = RecursionAir::machine(InnerSC::default()).setup(&reduce_program);
         Self {
+            reduce_setup_program,
             reduce_program,
             reduce_vk,
         }
@@ -198,11 +212,27 @@ impl SP1ProverImpl {
         }
         println!("witness_stream.len() = {}", witness_stream.len());
 
+        // Execute runtime to get the memory setup.
+        println!("setting up memory for recursion");
+        let machine = RecursionAir::machine(recursion_config.clone());
+        let mut runtime = Runtime::<InnerF, InnerEF, _>::new(
+            &self.reduce_setup_program,
+            machine.config().perm.clone(),
+        );
+        runtime.witness_stream = witness_stream;
+        runtime.run();
+        let mut checkpoint = runtime.memory.clone();
+        runtime.print_stats();
+
         // Execute runtime.
+        println!("executing recursion");
         let machine = RecursionAir::machine(recursion_config);
         let mut runtime =
             Runtime::<InnerF, InnerEF, _>::new(&self.reduce_program, machine.config().perm.clone());
-        runtime.witness_stream = witness_stream;
+        checkpoint.iter_mut().for_each(|e| {
+            e.timestamp = BabyBear::zero();
+        });
+        runtime.memory = checkpoint;
         runtime.run();
         runtime.print_stats();
 
@@ -232,8 +262,8 @@ mod tests {
     use super::*;
     use sp1_core::utils::setup_logger;
 
-    #[ignore]
     #[test]
+    #[ignore]
     fn test_prove_sp1() {
         setup_logger();
         std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
@@ -255,9 +285,13 @@ mod tests {
 
         let elf =
             include_bytes!("../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
-        let stdin = [bincode::serialize::<u32>(&6).unwrap()];
-        let proof: Proof<SP1SC> = SP1ProverImpl::prove(elf, &stdin);
+        // let stdin = [bincode::serialize::<u32>(&6).unwrap()];
+        // let proof: Proof<SP1SC> = SP1ProverImpl::prove(elf, &stdin);
+        // let serialized = bincode::serialize(&proof).unwrap();
+        // std::fs::write("xxx.bin", serialized).unwrap();
+        // exit(0);
 
+        let proof: Proof<SP1SC> = bincode::deserialize(&std::fs::read("xxx.bin").unwrap()).unwrap();
         let sp1_machine = RiscvAir::machine(SP1SC::default());
         let (_, vk) = sp1_machine.setup(&Program::from(elf));
 

recursion/program/src/reduce.rs
@@ -7,6 +7,8 @@ use crate::fri::TwoAdicFriPcsVariable;
 use crate::fri::TwoAdicMultiplicativeCosetVariable;
 use crate::hints::Hintable;
 use crate::stark::StarkVerifier;
+use crate::types::ShardProofVariable;
+use crate::types::VerifyingKeyVariable;
 use p3_baby_bear::BabyBear;
 use p3_baby_bear::DiffusionMatrixBabybear;
 use p3_challenger::DuplexChallenger;
@@ -27,6 +29,7 @@ use sp1_core::stark::VerifyingKey;
 use sp1_core::stark::{RiscvAir, StarkGenericConfig};
 use sp1_recursion_compiler::asm::AsmBuilder;
 use sp1_recursion_compiler::asm::AsmConfig;
+use sp1_recursion_compiler::ir::Array;
 use sp1_recursion_compiler::ir::Builder;
 use sp1_recursion_compiler::ir::Felt;
 use sp1_recursion_compiler::ir::MemVariable;
@@ -93,7 +96,7 @@ fn felt_to_var(builder: &mut RecursionBuilder, felt: Felt<BabyBear>) -> Var<Baby
     builder.bits2num_v(&bits)
 }
 
-pub fn build_reduce() -> RecursionProgram<Val> {
+pub fn build_reduce(setup: bool) -> RecursionProgram<Val> {
     let sp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
     let recursion_machine = RecursionAir::machine(BabyBearPoseidon2Inner::default());
 
@@ -107,19 +110,45 @@ pub fn build_reduce() -> RecursionProgram<Val> {
         config: recursion_config,
     };
 
-    // Read witness inputs
-    let is_recursive_flags = Vec::<usize>::read(&mut builder);
-    let sorted_indices = Vec::<Vec<usize>>::read(&mut builder);
-    let sp1_challenger = DuplexChallenger::read(&mut builder);
-    let mut reconstruct_challenger = DuplexChallenger::read(&mut builder);
-    let prep_sorted_indices = Vec::<usize>::read(&mut builder);
-    let prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
-    let recursion_prep_sorted_indices = Vec::<usize>::read(&mut builder);
-    let recursion_prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
-    let sp1_vk = VerifyingKey::<BabyBearPoseidon2>::read(&mut builder);
-    let recursion_vk = VerifyingKey::<BabyBearPoseidon2Inner>::read(&mut builder);
-    let num_proofs = is_recursive_flags.len();
+    // 1) Allocate inputs to the stack.
+    let is_recursive_flags: Array<_, Var<_>> = builder.uninit();
+    let sorted_indices: Array<_, Array<_, Var<_>>> = builder.uninit();
+    let sp1_challenger: DuplexChallengerVariable<_> = builder.uninit();
+    let mut reconstruct_challenger: DuplexChallengerVariable<_> = builder.uninit();
+    let prep_sorted_indices: Array<_, Var<_>> = builder.uninit();
+    let prep_domains: Array<_, TwoAdicMultiplicativeCosetVariable<_>> = builder.uninit();
+    let recursion_prep_sorted_indices: Array<_, Var<_>> = builder.uninit();
+    let recursion_prep_domains: Array<_, TwoAdicMultiplicativeCosetVariable<_>> = builder.uninit();
+    let sp1_vk: VerifyingKeyVariable<_> = builder.uninit();
+    let recursion_vk: VerifyingKeyVariable<_> = builder.uninit();
+    let proofs: Array<_, ShardProofVariable<_>> = builder.uninit();
 
+    // 2) Witness the inputs.
+    if setup {
+        Vec::<usize>::witness(&is_recursive_flags, &mut builder);
+        Vec::<Vec<usize>>::witness(&sorted_indices, &mut builder);
+        DuplexChallenger::witness(&sp1_challenger, &mut builder);
+        DuplexChallenger::witness(&reconstruct_challenger, &mut builder);
+        Vec::<usize>::witness(&prep_sorted_indices, &mut builder);
+        Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::witness(&prep_domains, &mut builder);
+        Vec::<usize>::witness(&recursion_prep_sorted_indices, &mut builder);
+        Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::witness(&recursion_prep_domains, &mut builder);
+        VerifyingKey::<SC>::witness(&sp1_vk, &mut builder);
+        VerifyingKey::<SC>::witness(&recursion_vk, &mut builder);
+        let num_proofs = is_recursive_flags.len();
+        let mut proofs_target = builder.dyn_array(num_proofs);
+        builder.range(0, num_proofs).for_each(|i, builder| {
+            let proof = ShardProof::<SC>::read(builder);
+            builder.set(&mut proofs_target, i, proof);
+        });
+        builder.assign(proofs.clone(), proofs_target);
+
+        // Compile the program up to this point.
+        return builder.compile_program();
+    }
+
+    builder.print_debug(99999);
+    let num_proofs = is_recursive_flags.len();
     let _pre_reconstruct_challenger = clone(&mut builder, &reconstruct_challenger);
     let zero: Var<_> = builder.constant(F::zero());
     let one: Var<_> = builder.constant(F::one());
@@ -133,7 +162,7 @@ pub fn build_reduce() -> RecursionProgram<Val> {
     }
 
     builder.range(0, num_proofs).for_each(|i, builder| {
-        let proof = ShardProof::<BabyBearPoseidon2>::read(builder);
+        let proof = builder.get(&proofs, i);
         let sorted_indices = builder.get(&sorted_indices, i);
         let is_recursive = builder.get(&is_recursive_flags, i);
         builder.if_eq(is_recursive, zero).then_or_else(

argumentcomputer/sphinx

prover/src/lib.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 use p3_baby_bear::BabyBear;
 use p3_challenger::CanObserve;
 use p3_commit::TwoAdicMultiplicativeCoset;
-use p3_field::PrimeField32;
+use p3_field::{AbstractField, PrimeField32};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use wp1_core::{
     air::MachineAir,
@@ -17,6 +17,7 @@ use wp1_core::{
     utils::{run_and_prove, BabyBearPoseidon2},
 };
 use wp1_recursion_core::{
+    cpu::Instruction,
     runtime::{RecursionProgram, Runtime},
     stark::{config::BabyBearPoseidon2Inner, RecursionAir},
 };
@@ -29,6 +30,7 @@ type InnerEF = <InnerSC as StarkGenericConfig>::Challenge;
 type OuterSC = BabyBearPoseidon2Inner;
 
 pub struct SP1ProverImpl {
+    reduce_setup_program: RecursionProgram<BabyBear>,
     reduce_program: RecursionProgram<BabyBear>,
     reduce_vk: VerifyingKey<InnerSC>,
 }
@@ -80,9 +82,21 @@ fn get_preprocessed_data<SC: StarkGenericConfig, A: MachineAir<Val<SC>>>(
 impl SP1ProverImpl {
     pub fn new() -> Self {
         // TODO: load from serde
-        let reduce_program = build_reduce();
+        let reduce_setup_program = build_reduce(true);
+        let mut reduce_program = build_reduce(false);
+        reduce_program.instructions[0] = Instruction::new(
+            wp1_recursion_core::runtime::Opcode::ADD,
+            BabyBear::zero(),
+            [BabyBear::zero(); 4],
+            [BabyBear::zero(); 4],
+            BabyBear::zero(),
+            BabyBear::zero(),
+            false,
+            false,
+        );
         let (_, reduce_vk) = RecursionAir::machine(InnerSC::default()).setup(&reduce_program);
         Self {
+            reduce_setup_program,
             reduce_program,
             reduce_vk,
         }
@@ -197,11 +211,27 @@ impl SP1ProverImpl {
         }
         println!("witness_stream.len() = {}", witness_stream.len());
 
+        // Execute runtime to get the memory setup.
+        println!("setting up memory for recursion");
+        let machine = RecursionAir::machine(recursion_config.clone());
+        let mut runtime = Runtime::<InnerF, InnerEF, _>::new(
+            &self.reduce_setup_program,
+            machine.config().perm.clone(),
+        );
+        runtime.witness_stream = witness_stream;
+        runtime.run();
+        let mut checkpoint = runtime.memory.clone();
+        runtime.print_stats();
+
         // Execute runtime.
+        println!("executing recursion");
         let machine = RecursionAir::machine(recursion_config);
         let mut runtime =
             Runtime::<InnerF, InnerEF, _>::new(&self.reduce_program, machine.config().perm.clone());
-        runtime.witness_stream = witness_stream;
+        for e in checkpoint.iter_mut() {
+            e.timestamp = BabyBear::zero();
+        }
+        runtime.memory = checkpoint;
         runtime.run();
         runtime.print_stats();
 
@@ -231,8 +261,8 @@ mod tests {
     use super::*;
     use wp1_core::utils::setup_logger;
 
-    #[ignore]
     #[test]
+    #[ignore]
     fn test_prove_sp1() {
         setup_logger();
         std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
@@ -254,9 +284,13 @@ mod tests {
 
         let elf =
             include_bytes!("../../examples/fibonacci/program/elf/riscv32im-succinct-zkvm-elf");
-        let stdin = [bincode::serialize::<u32>(&6).unwrap()];
-        let proof: Proof<SP1SC> = SP1ProverImpl::prove(elf, &stdin);
+        // let stdin = [bincode::serialize::<u32>(&6).unwrap()];
+        // let proof: Proof<SP1SC> = SP1ProverImpl::prove(elf, &stdin);
+        // let serialized = bincode::serialize(&proof).unwrap();
+        // std::fs::write("xxx.bin", serialized).unwrap();
+        // exit(0);
 
+        let proof: Proof<SP1SC> = bincode::deserialize(&std::fs::read("xxx.bin").unwrap()).unwrap();
         let wp1_machine = RiscvAir::machine(SP1SC::default());
         let (_, vk) = wp1_machine.setup(&Program::from(elf));
 

recursion/program/src/reduce.rs
@@ -7,6 +7,8 @@ use crate::fri::TwoAdicFriPcsVariable;
 use crate::fri::TwoAdicMultiplicativeCosetVariable;
 use crate::hints::Hintable;
 use crate::stark::StarkVerifier;
+use crate::types::ShardProofVariable;
+use crate::types::VerifyingKeyVariable;
 use p3_baby_bear::BabyBear;
 use p3_baby_bear::DiffusionMatrixBabybear;
 use p3_challenger::DuplexChallenger;
@@ -27,6 +29,7 @@ use wp1_core::stark::VerifyingKey;
 use wp1_core::stark::{RiscvAir, StarkGenericConfig};
 use wp1_recursion_compiler::asm::AsmBuilder;
 use wp1_recursion_compiler::asm::AsmConfig;
+use wp1_recursion_compiler::ir::Array;
 use wp1_recursion_compiler::ir::Builder;
 use wp1_recursion_compiler::ir::Felt;
 use wp1_recursion_compiler::ir::MemVariable;
@@ -93,7 +96,7 @@ fn felt_to_var(builder: &mut RecursionBuilder, felt: Felt<BabyBear>) -> Var<Baby
     builder.bits2num_v(&bits)
 }
 
-pub fn build_reduce() -> RecursionProgram<Val> {
+pub fn build_reduce(setup: bool) -> RecursionProgram<Val> {
     let wp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
     let recursion_machine = RecursionAir::machine(BabyBearPoseidon2Inner::default());
 
@@ -107,19 +110,45 @@ pub fn build_reduce() -> RecursionProgram<Val> {
         config: recursion_config,
     };
 
-    // Read witness inputs
-    let is_recursive_flags = Vec::<usize>::read(&mut builder);
-    let sorted_indices = Vec::<Vec<usize>>::read(&mut builder);
-    let wp1_challenger = DuplexChallenger::read(&mut builder);
-    let mut reconstruct_challenger = DuplexChallenger::read(&mut builder);
-    let prep_sorted_indices = Vec::<usize>::read(&mut builder);
-    let prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
-    let recursion_prep_sorted_indices = Vec::<usize>::read(&mut builder);
-    let recursion_prep_domains = Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::read(&mut builder);
-    let wp1_vk = VerifyingKey::<BabyBearPoseidon2>::read(&mut builder);
-    let recursion_vk = VerifyingKey::<BabyBearPoseidon2Inner>::read(&mut builder);
-    let num_proofs = is_recursive_flags.len();
+    // 1) Allocate inputs to the stack.
+    let is_recursive_flags: Array<_, Var<_>> = builder.uninit();
+    let sorted_indices: Array<_, Array<_, Var<_>>> = builder.uninit();
+    let wp1_challenger: DuplexChallengerVariable<_> = builder.uninit();
+    let mut reconstruct_challenger: DuplexChallengerVariable<_> = builder.uninit();
+    let prep_sorted_indices: Array<_, Var<_>> = builder.uninit();
+    let prep_domains: Array<_, TwoAdicMultiplicativeCosetVariable<_>> = builder.uninit();
+    let recursion_prep_sorted_indices: Array<_, Var<_>> = builder.uninit();
+    let recursion_prep_domains: Array<_, TwoAdicMultiplicativeCosetVariable<_>> = builder.uninit();
+    let wp1_vk: VerifyingKeyVariable<_> = builder.uninit();
+    let recursion_vk: VerifyingKeyVariable<_> = builder.uninit();
+    let proofs: Array<_, ShardProofVariable<_>> = builder.uninit();
 
+    // 2) Witness the inputs.
+    if setup {
+        Vec::<usize>::witness(&is_recursive_flags, &mut builder);
+        Vec::<Vec<usize>>::witness(&sorted_indices, &mut builder);
+        DuplexChallenger::witness(&wp1_challenger, &mut builder);
+        DuplexChallenger::witness(&reconstruct_challenger, &mut builder);
+        Vec::<usize>::witness(&prep_sorted_indices, &mut builder);
+        Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::witness(&prep_domains, &mut builder);
+        Vec::<usize>::witness(&recursion_prep_sorted_indices, &mut builder);
+        Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::witness(&recursion_prep_domains, &mut builder);
+        VerifyingKey::<SC>::witness(&wp1_vk, &mut builder);
+        VerifyingKey::<SC>::witness(&recursion_vk, &mut builder);
+        let num_proofs = is_recursive_flags.len();
+        let mut proofs_target = builder.dyn_array(num_proofs);
+        builder.range(0, num_proofs).for_each(|i, builder| {
+            let proof = ShardProof::<SC>::read(builder);
+            builder.set(&mut proofs_target, i, proof);
+        });
+        builder.assign(&proofs, proofs_target);
+
+        // Compile the program up to this point.
+        return builder.compile_program();
+    }
+
+    builder.print_debug(99999);
+    let num_proofs = is_recursive_flags.len();
     let _pre_reconstruct_challenger = clone(&mut builder, &reconstruct_challenger);
     let zero: Var<_> = builder.constant(F::zero());
     let one: Var<_> = builder.constant(F::one());
@@ -133,7 +162,7 @@ pub fn build_reduce() -> RecursionProgram<Val> {
     }
 
     builder.range(0, num_proofs).for_each(|i, builder| {
-        let proof = ShardProof::<BabyBearPoseidon2>::read(builder);
+        let proof = builder.get(&proofs, i);
         let sorted_indices = builder.get(&sorted_indices, i);
         let is_recursive = builder.get(&is_recursive_flags, i);
         builder.if_eq(is_recursive, zero).then_or_else(