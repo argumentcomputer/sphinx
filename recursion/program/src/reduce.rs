4a98c4dcdde63120a239651a30176903bd47c512
Kevin Jue <kjue235@gmail.com>
2024-04-19 00:52:06 -0400

    feat: verify pc and shard transition in recursive proofs (#514)

--- recursion/program/src/reduce.rs
+++ recursion/program/src/reduce.rs
@@ -6,29 +6,40 @@ use p3_commit::{ExtensionMmcs, TwoAdicMultiplicativeCoset};
 use p3_field::{extension::BinomialExtensionField, AbstractField, Field, TwoAdicField};
 use p3_fri::FriConfig;
 use p3_merkle_tree::FieldMerkleTreeMmcs;
-use p3_poseidon2::{Poseidon2, Poseidon2ExternalMatrixGeneral};
-use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
-use wp1_core::stark::{RiscvAir, ShardProof, StarkGenericConfig, VerifyingKey};
-use wp1_recursion_compiler::{
-    asm::{AsmBuilder, AsmConfig},
-    ir::{Array, Builder, Felt, MemVariable, Var},
-};
-use wp1_recursion_core::{
-    runtime::{RecursionProgram, DIGEST_SIZE},
-    stark::{
-        config::{inner_fri_config, wp1_fri_config, BabyBearPoseidon2Inner},
-        RecursionAir,
-    },
-};
+use p3_poseidon2::Poseidon2;
+use p3_poseidon2::Poseidon2ExternalMatrixGeneral;
+use p3_symmetric::PaddingFreeSponge;
+use p3_symmetric::TruncatedPermutation;
+use wp1_core::air::Word;
+use wp1_core::stark::ShardProof;
+use wp1_core::stark::VerifyingKey;
+use wp1_core::stark::PROOF_MAX_NUM_PVS;
+use wp1_core::stark::{RiscvAir, StarkGenericConfig};
+use wp1_recursion_compiler::asm::AsmBuilder;
+use wp1_recursion_compiler::asm::AsmConfig;
+use wp1_recursion_compiler::ir::Array;
+use wp1_recursion_compiler::ir::Builder;
+use wp1_recursion_compiler::ir::Felt;
+use wp1_recursion_compiler::ir::MemVariable;
+use wp1_recursion_compiler::ir::Var;
+use wp1_recursion_core::air::PublicValues as RecursionPublicValues;
+use wp1_recursion_core::runtime::RecursionProgram;
+use wp1_recursion_core::runtime::DIGEST_SIZE;
+use wp1_recursion_core::stark::config::inner_fri_config;
+use wp1_recursion_core::stark::config::wp1_fri_config;
+use wp1_recursion_core::stark::config::BabyBearPoseidon2Inner;
+use wp1_recursion_core::stark::RecursionAir;
 use wp1_sdk::utils::BabyBearPoseidon2;
+use wp1_sdk::PublicValues;
 
-use crate::{
-    challenger::{CanObserveVariable, DuplexChallengerVariable},
-    fri::{types::FriConfigVariable, TwoAdicFriPcsVariable, TwoAdicMultiplicativeCosetVariable},
-    hints::Hintable,
-    stark::StarkVerifier,
-    types::{ShardProofVariable, VerifyingKeyVariable},
-};
+use crate::challenger::{CanObserveVariable, DuplexChallengerVariable};
+use crate::fri::types::FriConfigVariable;
+use crate::fri::TwoAdicFriPcsVariable;
+use crate::fri::TwoAdicMultiplicativeCosetVariable;
+use crate::hints::Hintable as _;
+use crate::stark::StarkVerifier;
+use crate::types::ShardProofVariable;
+use crate::types::VerifyingKeyVariable;
 
 type SC = BabyBearPoseidon2;
 type F = <SC as StarkGenericConfig>::Val;
@@ -110,6 +121,10 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
     let recursion_prep_domains: Array<_, TwoAdicMultiplicativeCosetVariable<_>> = builder.uninit();
     let wp1_vk: VerifyingKeyVariable<_> = builder.uninit();
     let recursion_vk: VerifyingKeyVariable<_> = builder.uninit();
+    let start_pcs: Array<_, Felt<_>> = builder.uninit();
+    let next_pcs: Array<_, Felt<_>> = builder.uninit();
+    let start_shards: Array<_, Felt<_>> = builder.uninit();
+    let next_shards: Array<_, Felt<_>> = builder.uninit();
     let proofs: Array<_, ShardProofVariable<_>> = builder.uninit();
 
     // 2) Witness the inputs.
@@ -124,6 +139,10 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
         Vec::<TwoAdicMultiplicativeCoset<BabyBear>>::witness(&recursion_prep_domains, &mut builder);
         VerifyingKey::<SC>::witness(&wp1_vk, &mut builder);
         VerifyingKey::<SC>::witness(&recursion_vk, &mut builder);
+        Vec::<Val>::witness(&start_pcs, &mut builder);
+        Vec::<Val>::witness(&next_pcs, &mut builder);
+        Vec::<Val>::witness(&start_shards, &mut builder);
+        Vec::<Val>::witness(&next_shards, &mut builder);
         let num_proofs = is_recursive_flags.len();
         let mut proofs_target = builder.dyn_array(num_proofs);
         builder.range(0, num_proofs).for_each(|i, builder| {
@@ -139,8 +158,9 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
     let num_proofs = is_recursive_flags.len();
     let _pre_reconstruct_challenger = clone(&mut builder, &reconstruct_challenger);
     let zero: Var<_> = builder.constant(F::zero());
+    let zero_felt: Felt<_> = builder.constant(F::zero());
     let one: Var<_> = builder.constant(F::one());
-    let _one_felt: Felt<_> = builder.constant(F::one());
+    let one_felt: Felt<_> = builder.constant(F::one());
     builder.cycle_tracker("stage-a-setup-inputs");
 
     // Setup recursion challenger
@@ -152,15 +172,56 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
     }
     builder.cycle_tracker("stage-b-setup-recursion-challenger");
 
+    let expected_start_pc = builder.get(&start_pcs, zero);
+    let expected_start_shard = builder.get(&start_shards, zero);
+
     builder.range(0, num_proofs).for_each(|i, builder| {
         let proof = builder.get(&proofs, i);
         let sorted_indices = builder.get(&sorted_indices, i);
         let is_recursive = builder.get(&is_recursive_flags, i);
+
+        let shard_start_pc = builder.get(&start_pcs, i);
+        let shard_next_pc = builder.get(&next_pcs, i);
+        let shard_start_shard = builder.get(&start_shards, i);
+        let shard_next_shard = builder.get(&next_shards, i);
+
+        // Verify shard transition
+        builder.assert_felt_eq(expected_start_pc, shard_start_pc);
+        builder.assign(&expected_start_pc, shard_next_pc);
+        builder.assert_felt_eq(expected_start_shard, shard_start_shard);
+        builder.assign(&expected_start_shard, shard_next_shard);
+
         builder.if_eq(is_recursive, zero).then_or_else(
             // Non-recursive proof
             |builder| {
-                let shard_f = builder.get(&proof.public_values, 32);
+                let mut pv_elements = Vec::new();
+                for i in 0..PROOF_MAX_NUM_PVS {
+                    let element = builder.get(&proof.public_values, i);
+                    pv_elements.push(element);
+                }
+
+                let pv = PublicValues::<Word<Felt<_>>, Felt<_>>::from_vec(&pv_elements);
+
+                // Verify witness data
+                builder.assert_felt_eq(shard_start_pc, pv.start_pc);
+                builder.assert_felt_eq(shard_next_pc, pv.next_pc);
+                builder.assert_felt_eq(shard_start_shard, pv.shard);
+                let pv_shard_plus_one: Felt<_> = builder.eval(pv.shard + one_felt);
+
+                let pv_next_pc = felt_to_var(builder, pv.next_pc);
+                builder.if_eq(pv_next_pc, zero).then_or_else(
+                    |builder| {
+                        builder.assert_felt_eq(shard_next_shard, zero_felt);
+                    },
+                    |builder| {
+                        builder.assert_felt_eq(shard_next_shard, pv_shard_plus_one);
+                    },
+                );
+
+                // Need to convert the shard as a felt to a variable, since `if_eq` only handles variables.
+                let shard_f = pv.shard;
                 let shard = felt_to_var(builder, shard_f);
+
                 // First shard logic
                 builder.if_eq(shard, one).then(|builder| {
                     // Initialize the current challenger
@@ -168,8 +229,6 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
                     builder.assign(&reconstruct_challenger, empty_challenger);
                     reconstruct_challenger.observe(builder, wp1_vk.commitment.clone());
                 });
-
-                // TODO: more shard transition constraints here
 
                 // Observe current proof commit and public values into reconstruct challenger
                 for j in 0..DIGEST_SIZE {
@@ -196,7 +255,27 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
             },
             // Recursive proof
             |builder| {
-                // TODO: Verify proof public values
+                let mut pv_elements = Vec::new();
+                for i in 0..PROOF_MAX_NUM_PVS {
+                    let element = builder.get(&proof.public_values, i);
+                    pv_elements.push(element);
+                }
+
+                let proof_pv = RecursionPublicValues::<Felt<_>>::from_vec(&pv_elements);
+
+                let mut pv = builder.array(4);
+                builder.set(&mut pv, 0, shard_start_pc);
+                builder.set(&mut pv, 1, shard_start_shard);
+                builder.set(&mut pv, 2, shard_next_pc);
+                builder.set(&mut pv, 3, shard_next_shard);
+
+                let pv_digest = builder.poseidon2_hash(&pv);
+
+                for j in 0..DIGEST_SIZE {
+                    let expected_digest_element = proof_pv.committed_value_digest[j];
+                    let digest_element = builder.get(&pv_digest, j);
+                    builder.assert_felt_eq(expected_digest_element, digest_element);
+                }
 
                 // Build recursion challenger
                 let mut current_challenger = recursion_challenger.as_clone(builder);
@@ -237,6 +316,19 @@ pub fn build_reduce_program(setup: bool) -> RecursionProgram<Val> {
     // )
     // Note we still need to check that verify_start_challenger matches final reconstruct_challenger
     // after observing pv_digest at the end.
+
+    let start_pc = builder.get(&start_pcs, zero);
+    let start_shard = builder.get(&start_shards, zero);
+    builder.write_public_value(start_pc);
+    builder.write_public_value(start_shard);
+
+    let last_idx: Var<_> = builder.eval(num_proofs - one);
+    let next_pc = builder.get(&next_pcs, last_idx);
+    let next_shard = builder.get(&next_shards, last_idx);
+    builder.write_public_value(next_pc);
+    builder.write_public_value(next_shard);
+
+    builder.commit_public_values();
 
     let program = builder.compile_program();
     let elapsed = time.elapsed();
