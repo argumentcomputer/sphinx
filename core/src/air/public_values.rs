d702266173ecef39d0807f683c2e398aa5659d70
Kevin Jue <kjue235@gmail.com>
2024-04-19 00:52:06 -0400

    feat: verify pc and shard transition in recursive proofs (#514)

--- core/src/air/public_values.rs
+++ core/src/air/public_values.rs
@@ -1,4 +1,6 @@
-use core::{fmt::Debug, mem::size_of};
+use arrayref::array_ref;
+use core::fmt::Debug;
+use core::mem::size_of;
 use std::iter::once;
 
 use itertools::Itertools;
@@ -59,9 +61,9 @@ impl PublicValues<u32, u32> {
     }
 }
 
-impl<F: AbstractField> PublicValues<Word<F>, F> {
+impl<T: Clone> PublicValues<Word<T>, T> {
     /// Convert a vector of field elements into a PublicValues struct.
-    pub fn from_vec(data: &[F]) -> Self {
+    pub fn from_vec(data: &[T]) -> Self {
         let mut iter = data.iter().cloned();
 
         let mut committed_value_digest = Vec::new();
@@ -83,7 +85,8 @@ impl<F: AbstractField> PublicValues<Word<F>, F> {
         };
 
         Self {
-            committed_value_digest: committed_value_digest.try_into().unwrap(),
+            committed_value_digest: array_ref![committed_value_digest, 0, PV_DIGEST_NUM_WORDS]
+                .clone(),
             shard: shard.to_owned(),
             start_pc: start_pc.to_owned(),
             next_pc: next_pc.to_owned(),
