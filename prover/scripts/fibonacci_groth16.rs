WindOctober/sp1

feat: recursion experiments (#522)

prover/scripts/fibonacci_groth16.rs
@@ -1,0 +1,84 @@
+//! Tests end-to-end performance of wrapping a recursion proof to Groth16.
+
+#![feature(generic_const_exprs)]
+#![allow(incomplete_features)]
+
+use std::time::Instant;
+
+use itertools::iproduct;
+use p3_challenger::CanObserve;
+use sp1_core::{
+    runtime::Program,
+    stark::{Proof, RiscvAir, StarkGenericConfig},
+    utils::BabyBearPoseidon2,
+};
+use sp1_prover::SP1ProverImpl;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
+
+fn main() {
+    // Setup tracer.
+    let default_filter = "off";
+    let log_appender = tracing_appender::rolling::never("scripts/results", "fibonacci_groth16.log");
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(default_filter))
+        .add_directive("p3_keccak_air=off".parse().unwrap())
+        .add_directive("p3_fri=off".parse().unwrap())
+        .add_directive("p3_challenger=off".parse().unwrap())
+        .add_directive("p3_dft=off".parse().unwrap())
+        .add_directive("sp1_core=off".parse().unwrap());
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_ansi(false)
+        .with_file(false)
+        .with_target(false)
+        .with_thread_names(false)
+        .with_env_filter(env_filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_writer(log_appender)
+        .finish()
+        .init();
+
+    // Setup enviroment variables.
+    std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
+
+    // Initialize prover.
+    let prover = SP1ProverImpl::new();
+
+    // Setup sweep.
+    let iterations = [480000u32];
+    let shard_sizes = [1 << 22];
+    let batch_sizes = [2];
+    let elf = include_bytes!("../../examples/fibonacci-io/program/elf/riscv32im-succinct-zkvm-elf");
+
+    for (shard_size, iterations, batch_size) in iproduct!(shard_sizes, iterations, batch_sizes) {
+        tracing::info!(
+            "running: shard_size={}, iterations={}, batch_size={}",
+            shard_size,
+            iterations,
+            batch_size
+        );
+        std::env::set_var("SHARD_SIZE", shard_size.to_string());
+
+        tracing::info!("proving leaves");
+        let stdin = [bincode::serialize::<u32>(&iterations).unwrap()];
+        let leaf_proving_start = Instant::now();
+        let proof: Proof<BabyBearPoseidon2> = SP1ProverImpl::prove(elf, &stdin);
+        let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
+        tracing::info!("leaf_proving_duration={}", leaf_proving_duration);
+
+        tracing::info!("proving inner");
+        let sp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
+        let (_, vk) = sp1_machine.setup(&Program::from(elf));
+        let mut sp1_challenger = sp1_machine.config().challenger();
+        sp1_challenger.observe(vk.commit);
+        for shard_proof in proof.shard_proofs.iter() {
+            sp1_challenger.observe(shard_proof.commitment.main_commit);
+            sp1_challenger.observe_slice(&shard_proof.public_values.to_vec());
+        }
+
+        let recursion_proving_start = Instant::now();
+        let _ = prover.reduce_tree(&vk, proof, batch_size);
+        let recursion_proving_duration = recursion_proving_start.elapsed().as_secs_f64();
+        tracing::info!("recursion_proving_duration={}", recursion_proving_duration);
+    }
+}

prover/scripts/tendermint_sweep.rs
@@ -1,0 +1,99 @@
+//! Sweeps end-to-end prover performance across a wide range of parameters for Tendermint.
+
+#![feature(generic_const_exprs)]
+#![allow(incomplete_features)]
+
+use std::{fs::File, io::BufWriter, io::Write, time::Instant};
+
+use itertools::iproduct;
+use p3_challenger::CanObserve;
+use sp1_core::{
+    runtime::Program,
+    stark::{Proof, RiscvAir, StarkGenericConfig},
+    utils::BabyBearPoseidon2,
+};
+use sp1_prover::SP1ProverImpl;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
+
+fn main() {
+    // Setup tracer.
+    let default_filter = "off";
+    let log_appender = tracing_appender::rolling::never("scripts/results", "tendermint_sweep.log");
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(default_filter))
+        .add_directive("p3_keccak_air=off".parse().unwrap())
+        .add_directive("p3_fri=off".parse().unwrap())
+        .add_directive("p3_challenger=off".parse().unwrap())
+        .add_directive("p3_dft=off".parse().unwrap())
+        .add_directive("sp1_core=off".parse().unwrap());
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_ansi(false)
+        .with_file(false)
+        .with_target(false)
+        .with_thread_names(false)
+        .with_env_filter(env_filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_writer(log_appender)
+        .finish()
+        .init();
+
+    // Setup enviroment variables.
+    std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
+
+    // Initialize prover.
+    let prover = SP1ProverImpl::new();
+
+    // Setup sweep.
+    let iterations = [480000u32];
+    let shard_sizes = [1 << 19, 1 << 20, 1 << 21, 1 << 22];
+    let batch_sizes = [2];
+    let elf = include_bytes!(
+        "../../examples/tendermint-benchmark/program/elf/riscv32im-succinct-zkvm-elf"
+    );
+
+    let mut lines = vec![
+        "iterations,shard_size,batch_size,leaf_proving_duration,recursion_proving_duration"
+            .to_string(),
+    ];
+    for (shard_size, iterations, batch_size) in iproduct!(shard_sizes, iterations, batch_sizes) {
+        tracing::info!(
+            "running: shard_size={}, iterations={}, batch_size={}",
+            shard_size,
+            iterations,
+            batch_size
+        );
+        std::env::set_var("SHARD_SIZE", shard_size.to_string());
+
+        let stdin = [bincode::serialize::<u32>(&iterations).unwrap()];
+        let leaf_proving_start = Instant::now();
+        let proof: Proof<BabyBearPoseidon2> = SP1ProverImpl::prove(elf, &stdin);
+        let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
+
+        let sp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
+        let (_, vk) = sp1_machine.setup(&Program::from(elf));
+        let mut sp1_challenger = sp1_machine.config().challenger();
+        sp1_challenger.observe(vk.commit);
+        for shard_proof in proof.shard_proofs.iter() {
+            sp1_challenger.observe(shard_proof.commitment.main_commit);
+            sp1_challenger.observe_slice(&shard_proof.public_values.to_vec());
+        }
+
+        let recursion_proving_start = Instant::now();
+        let _ = prover.reduce_tree(&vk, proof, batch_size);
+        let recursion_proving_duration = recursion_proving_start.elapsed().as_secs_f64();
+
+        lines.push(format!(
+            "{},{},{},{},{}",
+            iterations, shard_size, batch_size, leaf_proving_duration, recursion_proving_duration
+        ));
+    }
+
+    let file = File::create("scripts/results/tendermint_sweep.csv").unwrap();
+    let mut writer = BufWriter::new(file);
+    for line in lines.clone() {
+        writeln!(writer, "{}", line).unwrap();
+    }
+
+    println!("{:#?}", lines);
+}

recursion/program/src/fri/two_adic_pcs.rs
@@ -257,10 +257,10 @@ where
     }
 }
 
-#[cfg(test)]
-pub(crate) mod tests {
+pub mod tests {
 
     use std::cmp::Reverse;
+    use std::collections::VecDeque;
 
     use crate::challenger::CanObserveVariable;
     use crate::challenger::DuplexChallengerVariable;
@@ -270,12 +270,12 @@ pub(crate) mod tests {
     use crate::fri::TwoAdicMultiplicativeCosetVariable;
     use crate::hints::Hintable;
     use itertools::Itertools;
+    use p3_baby_bear::BabyBear;
     use p3_challenger::CanObserve;
     use p3_challenger::FieldChallenger;
     use p3_commit::Pcs;
     use p3_commit::TwoAdicMultiplicativeCoset;
     use p3_field::AbstractField;
-    use p3_field::PrimeField32;
     use p3_field::TwoAdicField;
     use p3_fri::FriConfig;
     use p3_matrix::dense::RowMajorMatrix;
@@ -297,7 +297,8 @@ pub(crate) mod tests {
     use sp1_recursion_compiler::ir::Builder;
     use sp1_recursion_compiler::ir::Usize;
     use sp1_recursion_compiler::ir::Var;
-    use sp1_recursion_core::runtime::Runtime;
+    use sp1_recursion_core::air::Block;
+    use sp1_recursion_core::runtime::RecursionProgram;
     use sp1_recursion_core::runtime::DIGEST_SIZE;
 
     use crate::commit::PcsVariable;
@@ -331,11 +332,12 @@ pub(crate) mod tests {
         }
     }
 
-    #[allow(clippy::type_complexity)]
-    #[test]
-    fn test_two_adic_fri_pcs_single_batch() {
+    pub fn build_test_fri_with_cols_and_log2_rows(
+        nb_cols: usize,
+        nb_log2_rows: usize,
+    ) -> (RecursionProgram<BabyBear>, VecDeque<Vec<Block<BabyBear>>>) {
         let mut rng = &mut OsRng;
-        let log_degrees = &[10, 16];
+        let log_degrees = &[nb_log2_rows];
         let perm = inner_perm();
         let fri_config = inner_fri_config();
         let hash = InnerHash::new(perm.clone());
@@ -358,7 +360,7 @@ pub(crate) mod tests {
                         &pcs_val,
                         1 << d,
                     ),
-                    RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, 10 + d),
+                    RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, nb_cols),
                 )
             })
             .sorted_by_key(|(dom, _)| Reverse(dom.log_n))
@@ -426,130 +428,18 @@ pub(crate) mod tests {
         pcs.verify(&mut builder, rounds, proofvar, &mut challenger);
 
         let program = builder.compile_program();
-        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, perm.clone());
-        runtime.witness_stream.extend(proof.write());
-        runtime.run();
-        println!(
-            "The program executed successfully, number of cycles: {}",
-            runtime.clk.as_canonical_u32() / 4
-        );
+        let mut witness_stream = VecDeque::new();
+        witness_stream.extend(proof.write());
+        (program, witness_stream)
     }
 
-    #[allow(clippy::type_complexity)]
     #[test]
-    fn test_two_adic_fri_pcs_multi_batches() {
-        let mut rng = &mut OsRng;
-        let log_degrees = &[10, 16];
-        let perm = inner_perm();
-        let fri_config = inner_fri_config();
-        let hash = InnerHash::new(perm.clone());
-        let compress = InnerCompress::new(perm.clone());
-        let val_mmcs = InnerValMmcs::new(hash, compress);
-        let dft = InnerDft {};
-        let pcs_val: InnerPcs = InnerPcs::new(
-            log_degrees.iter().copied().max().unwrap(),
-            dft,
-            val_mmcs,
-            fri_config,
-        );
-
-        // Generate proof.
-        let num_of_batches = 3;
-
-        let mut batch_domains_and_polys = vec![];
-        let mut batches_commits = vec![];
-        let mut batches_prover_data = vec![];
-
-        for _ in 0..num_of_batches {
-            let domains_and_polys = log_degrees
-                .iter()
-                .map(|&d| {
-                    (
-                        <InnerPcs as Pcs<InnerChallenge, InnerChallenger>>::natural_domain_for_degree(
-                            &pcs_val,
-                            1 << d,
-                        ),
-                        RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, 10 + d),
-                    )
-                })
-                .sorted_by_key(|(dom, _)| Reverse(dom.log_n))
-                .collect::<Vec<_>>();
-            let (commit, data) = <InnerPcs as Pcs<InnerChallenge, InnerChallenger>>::commit(
-                &pcs_val,
-                domains_and_polys.clone(),
-            );
-
-            batch_domains_and_polys.push(domains_and_polys);
-            batches_commits.push(commit);
-            batches_prover_data.push(data);
-        }
-
-        let mut challenger = InnerChallenger::new(perm.clone());
-        for commit in batches_commits.iter() {
-            challenger.observe(*commit);
-        }
-
-        let zeta = challenger.sample_ext_element::<InnerChallenge>();
-        let points = log_degrees.iter().map(|_| vec![zeta]).collect::<Vec<_>>();
-
-        let data_and_points = batches_prover_data
-            .iter()
-            .map(|data| (data, points.clone()))
-            .collect::<Vec<_>>();
-        let (opening, proof) = pcs_val.open(data_and_points, &mut challenger);
-
-        // Verify proof.
-        let mut challenger = InnerChallenger::new(perm.clone());
-        for commit in batches_commits.iter() {
-            challenger.observe(*commit);
-        }
-        challenger.sample_ext_element::<InnerChallenge>();
-
-        let rounds_val = batches_commits
-            .clone()
-            .into_iter()
-            .zip(batch_domains_and_polys)
-            .zip(opening)
-            .map(|((commit, domains_and_polys), open_vals)| {
-                let os = domains_and_polys
-                    .iter()
-                    .zip(open_vals)
-                    .map(|((domain, _), mat_openings)| {
-                        (*domain, vec![(zeta, mat_openings[0].clone())])
-                    })
-                    .collect();
-                (commit, os)
-            })
-            .collect::<Vec<_>>();
-
-        pcs_val
-            .verify(rounds_val.clone(), &proof, &mut challenger)
-            .unwrap();
-
-        // Test the recursive Pcs.
-        let mut builder = Builder::<InnerConfig>::default();
-        let config = const_fri_config(&mut builder, inner_fri_config());
-        let pcs = TwoAdicFriPcsVariable { config };
-        let rounds = builder.constant::<Array<_, TwoAdicPcsRoundVariable<_>>>(rounds_val);
-
-        // // Test proof verification.
-        let proofvar = InnerPcsProof::read(&mut builder);
-        let mut challenger = DuplexChallengerVariable::new(&mut builder);
-        for commit in batches_commits {
-            let commit: [InnerVal; DIGEST_SIZE] = commit.into();
-            let commit = builder.constant::<Array<_, _>>(commit.to_vec());
-            challenger.observe(&mut builder, commit);
-        }
-        challenger.sample_ext(&mut builder);
-        pcs.verify(&mut builder, rounds, proofvar, &mut challenger);
-
-        let program = builder.compile_program();
-        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, perm.clone());
-        runtime.witness_stream.extend(proof.write());
+    fn test_two_adic_fri_pcs_single_batch() {
+        use sp1_recursion_core::runtime::Runtime;
+        let (program, witness) = build_test_fri_with_cols_and_log2_rows(10, 16);
+        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, inner_perm());
+        runtime.witness_stream = witness;
         runtime.run();
-        println!(
-            "The program executed successfully, number of cycles: {}",
-            runtime.clk.as_canonical_u32() / 4
-        );
+        runtime.print_stats();
     }
 }

argumentcomputer/sphinx

prover/scripts/fibonacci_groth16.rs
@@ -1,0 +1,81 @@
+//! Tests end-to-end performance of wrapping a recursion proof to Groth16.
+
+use std::time::Instant;
+
+use itertools::iproduct;
+use p3_challenger::CanObserve;
+use wp1_core::{
+    runtime::Program,
+    stark::{Proof, RiscvAir, StarkGenericConfig},
+    utils::BabyBearPoseidon2,
+};
+use wp1_prover::SP1ProverImpl;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
+
+fn main() {
+    // Setup tracer.
+    let default_filter = "off";
+    let log_appender = tracing_appender::rolling::never("scripts/results", "fibonacci_groth16.log");
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(default_filter))
+        .add_directive("p3_keccak_air=off".parse().unwrap())
+        .add_directive("p3_fri=off".parse().unwrap())
+        .add_directive("p3_challenger=off".parse().unwrap())
+        .add_directive("p3_dft=off".parse().unwrap())
+        .add_directive("sp1_core=off".parse().unwrap());
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_ansi(false)
+        .with_file(false)
+        .with_target(false)
+        .with_thread_names(false)
+        .with_env_filter(env_filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_writer(log_appender)
+        .finish()
+        .init();
+
+    // Setup enviroment variables.
+    std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
+
+    // Initialize prover.
+    let prover = SP1ProverImpl::new();
+
+    // Setup sweep.
+    let iterations = [480000u32];
+    let shard_sizes = [1 << 22];
+    let batch_sizes = [2];
+    let elf = include_bytes!("../../examples/fibonacci-io/program/elf/riscv32im-succinct-zkvm-elf");
+
+    for (shard_size, iterations, batch_size) in iproduct!(shard_sizes, iterations, batch_sizes) {
+        tracing::info!(
+            "running: shard_size={}, iterations={}, batch_size={}",
+            shard_size,
+            iterations,
+            batch_size
+        );
+        std::env::set_var("SHARD_SIZE", shard_size.to_string());
+
+        tracing::info!("proving leaves");
+        let stdin = [bincode::serialize::<u32>(&iterations).unwrap()];
+        let leaf_proving_start = Instant::now();
+        let proof: Proof<BabyBearPoseidon2> = SP1ProverImpl::prove(elf, &stdin);
+        let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
+        tracing::info!("leaf_proving_duration={}", leaf_proving_duration);
+
+        tracing::info!("proving inner");
+        let wp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
+        let (_, vk) = wp1_machine.setup(&Program::from(elf));
+        let mut wp1_challenger = wp1_machine.config().challenger();
+        wp1_challenger.observe(vk.commit);
+        for shard_proof in proof.shard_proofs.iter() {
+            wp1_challenger.observe(shard_proof.commitment.main_commit);
+            wp1_challenger.observe_slice(&shard_proof.public_values);
+        }
+
+        let recursion_proving_start = Instant::now();
+        let _ = prover.reduce_tree(&vk, proof, batch_size);
+        let recursion_proving_duration = recursion_proving_start.elapsed().as_secs_f64();
+        tracing::info!("recursion_proving_duration={}", recursion_proving_duration);
+    }
+}

prover/scripts/tendermint_sweep.rs
@@ -1,0 +1,96 @@
+//! Sweeps end-to-end prover performance across a wide range of parameters for Tendermint.
+
+use std::{fs::File, io::BufWriter, io::Write, time::Instant};
+
+use itertools::iproduct;
+use p3_challenger::CanObserve;
+use wp1_core::{
+    runtime::Program,
+    stark::{Proof, RiscvAir, StarkGenericConfig},
+    utils::BabyBearPoseidon2,
+};
+use wp1_prover::SP1ProverImpl;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
+
+fn main() {
+    // Setup tracer.
+    let default_filter = "off";
+    let log_appender = tracing_appender::rolling::never("scripts/results", "tendermint_sweep.log");
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new(default_filter))
+        .add_directive("p3_keccak_air=off".parse().unwrap())
+        .add_directive("p3_fri=off".parse().unwrap())
+        .add_directive("p3_challenger=off".parse().unwrap())
+        .add_directive("p3_dft=off".parse().unwrap())
+        .add_directive("sp1_core=off".parse().unwrap());
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_ansi(false)
+        .with_file(false)
+        .with_target(false)
+        .with_thread_names(false)
+        .with_env_filter(env_filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_writer(log_appender)
+        .finish()
+        .init();
+
+    // Setup enviroment variables.
+    std::env::set_var("RECONSTRUCT_COMMITMENTS", "false");
+
+    // Initialize prover.
+    let prover = SP1ProverImpl::new();
+
+    // Setup sweep.
+    let iterations = [480000u32];
+    let shard_sizes = [1 << 19, 1 << 20, 1 << 21, 1 << 22];
+    let batch_sizes = [2];
+    let elf = include_bytes!(
+        "../../examples/tendermint-benchmark/program/elf/riscv32im-succinct-zkvm-elf"
+    );
+
+    let mut lines = vec![
+        "iterations,shard_size,batch_size,leaf_proving_duration,recursion_proving_duration"
+            .to_string(),
+    ];
+    for (shard_size, iterations, batch_size) in iproduct!(shard_sizes, iterations, batch_sizes) {
+        tracing::info!(
+            "running: shard_size={}, iterations={}, batch_size={}",
+            shard_size,
+            iterations,
+            batch_size
+        );
+        std::env::set_var("SHARD_SIZE", shard_size.to_string());
+
+        let stdin = [bincode::serialize::<u32>(&iterations).unwrap()];
+        let leaf_proving_start = Instant::now();
+        let proof: Proof<BabyBearPoseidon2> = SP1ProverImpl::prove(elf, &stdin);
+        let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
+
+        let wp1_machine = RiscvAir::machine(BabyBearPoseidon2::default());
+        let (_, vk) = wp1_machine.setup(&Program::from(elf));
+        let mut wp1_challenger = wp1_machine.config().challenger();
+        wp1_challenger.observe(vk.commit);
+        for shard_proof in proof.shard_proofs.iter() {
+            wp1_challenger.observe(shard_proof.commitment.main_commit);
+            wp1_challenger.observe_slice(&shard_proof.public_values);
+        }
+
+        let recursion_proving_start = Instant::now();
+        let _ = prover.reduce_tree(&vk, proof, batch_size);
+        let recursion_proving_duration = recursion_proving_start.elapsed().as_secs_f64();
+
+        lines.push(format!(
+            "{},{},{},{},{}",
+            iterations, shard_size, batch_size, leaf_proving_duration, recursion_proving_duration
+        ));
+    }
+
+    let file = File::create("scripts/results/tendermint_sweep.csv").unwrap();
+    let mut writer = BufWriter::new(file);
+    for line in lines.clone() {
+        writeln!(writer, "{}", line).unwrap();
+    }
+
+    println!("{:#?}", lines);
+}

recursion/program/src/fri/two_adic_pcs.rs
@@ -255,15 +255,19 @@ where
     }
 }
 
-#[cfg(test)]
-pub(crate) mod tests {
+pub mod tests {
 
     use std::cmp::Reverse;
+    use std::collections::VecDeque;
 
     use itertools::Itertools;
-    use p3_challenger::{CanObserve, FieldChallenger};
-    use p3_commit::{Pcs, TwoAdicMultiplicativeCoset};
-    use p3_field::{AbstractField, PrimeField32, TwoAdicField};
+    use p3_baby_bear::BabyBear;
+    use p3_challenger::CanObserve;
+    use p3_challenger::FieldChallenger;
+    use p3_commit::Pcs;
+    use p3_commit::TwoAdicMultiplicativeCoset;
+    use p3_field::AbstractField;
+    use p3_field::TwoAdicField;
     use p3_fri::FriConfig;
     use p3_matrix::dense::RowMajorMatrix;
     use rand::rngs::OsRng;
@@ -284,7 +288,8 @@ pub(crate) mod tests {
     use wp1_recursion_compiler::ir::Builder;
     use wp1_recursion_compiler::ir::Usize;
     use wp1_recursion_compiler::ir::Var;
-    use wp1_recursion_core::runtime::Runtime;
+    use wp1_recursion_core::air::Block;
+    use wp1_recursion_core::runtime::RecursionProgram;
     use wp1_recursion_core::runtime::DIGEST_SIZE;
 
     use crate::{
@@ -325,11 +330,12 @@ pub(crate) mod tests {
         }
     }
 
-    #[allow(clippy::type_complexity)]
-    #[test]
-    fn test_two_adic_fri_pcs_single_batch() {
+    pub fn build_test_fri_with_cols_and_log2_rows(
+        nb_cols: usize,
+        nb_log2_rows: usize,
+    ) -> (RecursionProgram<BabyBear>, VecDeque<Vec<Block<BabyBear>>>) {
         let mut rng = &mut OsRng;
-        let log_degrees = &[10, 16];
+        let log_degrees = &[nb_log2_rows];
         let perm = inner_perm();
         let fri_config = inner_fri_config();
         let hash = InnerHash::new(perm.clone());
@@ -352,7 +358,7 @@ pub(crate) mod tests {
                         &pcs_val,
                         1 << d,
                     ),
-                    RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, 10 + d),
+                    RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, nb_cols),
                 )
             })
             .sorted_by_key(|(dom, _)| Reverse(dom.log_n))
@@ -420,130 +426,18 @@ pub(crate) mod tests {
         pcs.verify(&mut builder, &rounds, &proofvar, &mut challenger);
 
         let program = builder.compile_program();
-        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, perm.clone());
-        runtime.witness_stream.extend(proof.write());
-        runtime.run();
-        println!(
-            "The program executed successfully, number of cycles: {}",
-            runtime.clk.as_canonical_u32() / 4
-        );
+        let mut witness_stream = VecDeque::new();
+        witness_stream.extend(proof.write());
+        (program, witness_stream)
     }
 
-    #[allow(clippy::type_complexity)]
     #[test]
-    fn test_two_adic_fri_pcs_multi_batches() {
-        let mut rng = &mut OsRng;
-        let log_degrees = &[10, 16];
-        let perm = inner_perm();
-        let fri_config = inner_fri_config();
-        let hash = InnerHash::new(perm.clone());
-        let compress = InnerCompress::new(perm.clone());
-        let val_mmcs = InnerValMmcs::new(hash, compress);
-        let dft = InnerDft {};
-        let pcs_val: InnerPcs = InnerPcs::new(
-            log_degrees.iter().copied().max().unwrap(),
-            dft,
-            val_mmcs,
-            fri_config,
-        );
-
-        // Generate proof.
-        let num_of_batches = 3;
-
-        let mut batch_domains_and_polys = vec![];
-        let mut batches_commits = vec![];
-        let mut batches_prover_data = vec![];
-
-        for _ in 0..num_of_batches {
-            let domains_and_polys = log_degrees
-                .iter()
-                .map(|&d| {
-                    (
-                        <InnerPcs as Pcs<InnerChallenge, InnerChallenger>>::natural_domain_for_degree(
-                            &pcs_val,
-                            1 << d,
-                        ),
-                        RowMajorMatrix::<InnerVal>::rand(&mut rng, 1 << d, 10 + d),
-                    )
-                })
-                .sorted_by_key(|(dom, _)| Reverse(dom.log_n))
-                .collect::<Vec<_>>();
-            let (commit, data) = <InnerPcs as Pcs<InnerChallenge, InnerChallenger>>::commit(
-                &pcs_val,
-                domains_and_polys.clone(),
-            );
-
-            batch_domains_and_polys.push(domains_and_polys);
-            batches_commits.push(commit);
-            batches_prover_data.push(data);
-        }
-
-        let mut challenger = InnerChallenger::new(perm.clone());
-        for commit in batches_commits.iter() {
-            challenger.observe(*commit);
-        }
-
-        let zeta = challenger.sample_ext_element::<InnerChallenge>();
-        let points = log_degrees.iter().map(|_| vec![zeta]).collect::<Vec<_>>();
-
-        let data_and_points = batches_prover_data
-            .iter()
-            .map(|data| (data, points.clone()))
-            .collect::<Vec<_>>();
-        let (opening, proof) = pcs_val.open(data_and_points, &mut challenger);
-
-        // Verify proof.
-        let mut challenger = InnerChallenger::new(perm.clone());
-        for commit in batches_commits.iter() {
-            challenger.observe(*commit);
-        }
-        challenger.sample_ext_element::<InnerChallenge>();
-
-        let rounds_val = batches_commits
-            .clone()
-            .into_iter()
-            .zip(batch_domains_and_polys)
-            .zip(opening)
-            .map(|((commit, domains_and_polys), open_vals)| {
-                let os = domains_and_polys
-                    .iter()
-                    .zip(open_vals)
-                    .map(|((domain, _), mat_openings)| {
-                        (*domain, vec![(zeta, mat_openings[0].clone())])
-                    })
-                    .collect();
-                (commit, os)
-            })
-            .collect::<Vec<_>>();
-
-        pcs_val
-            .verify(rounds_val.clone(), &proof, &mut challenger)
-            .unwrap();
-
-        // Test the recursive Pcs.
-        let mut builder = Builder::<InnerConfig>::default();
-        let config = const_fri_config(&mut builder, &inner_fri_config());
-        let pcs = TwoAdicFriPcsVariable { config };
-        let rounds = builder.constant::<Array<_, TwoAdicPcsRoundVariable<_>>>(rounds_val);
-
-        // // Test proof verification.
-        let proofvar = InnerPcsProof::read(&mut builder);
-        let mut challenger = DuplexChallengerVariable::new(&mut builder);
-        for commit in batches_commits {
-            let commit: [InnerVal; DIGEST_SIZE] = commit.into();
-            let commit = builder.constant::<Array<_, _>>(commit.to_vec());
-            challenger.observe(&mut builder, commit);
-        }
-        challenger.sample_ext(&mut builder);
-        pcs.verify(&mut builder, &rounds, &proofvar, &mut challenger);
-
-        let program = builder.compile_program();
-        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, perm.clone());
-        runtime.witness_stream.extend(proof.write());
+    fn test_two_adic_fri_pcs_single_batch() {
+        use wp1_recursion_core::runtime::Runtime;
+        let (program, witness) = build_test_fri_with_cols_and_log2_rows(10, 16);
+        let mut runtime = Runtime::<InnerVal, InnerChallenge, _>::new(&program, inner_perm());
+        runtime.witness_stream = witness;
         runtime.run();
-        println!(
-            "The program executed successfully, number of cycles: {}",
-            runtime.clk.as_canonical_u32() / 4
-        );
+        runtime.print_stats();
     }
 }