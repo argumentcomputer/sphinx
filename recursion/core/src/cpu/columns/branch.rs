6ca192eca163d54f385800b479cd165302eeed3f
Kevin Jue <kjue235@gmail.com>
2024-04-29 10:52:29 -0700

    feat(recursion): cpu branch constraints (#578)

--- recursion/core/src/cpu/columns/branch.rs
+++ recursion/core/src/cpu/columns/branch.rs
@@ -1,5 +1,5 @@
 use std::mem::size_of;
-
+use wp1_core::air::BinomialExtension;
 use wp1_derive::AlignedBorrow;
 
 use crate::air::IsExtZeroOperation;
@@ -11,5 +11,8 @@ pub(crate) const NUM_BRANCH_COLS: usize = size_of::<BranchCols<u8>>();
 #[derive(AlignedBorrow, Default, Debug, Clone, Copy)]
 #[repr(C)]
 pub struct BranchCols<T> {
-    is_eq_zero: IsExtZeroOperation<T>,
+    pub(crate) comparison_diff: IsExtZeroOperation<T>,
+    pub(crate) comparison_diff_val: BinomialExtension<T>,
+    pub(crate) do_branch: T,
+    pub(crate) next_pc: T,
 }
