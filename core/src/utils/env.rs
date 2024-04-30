99961efcbaefb9ff88a68d2c95b8e2f2c667097a
John Guibas <jtguibas@gmail.com>
2024-04-29 18:32:54 -0700

    feat: prover tweaks pt4 (#632)

--- core/src/utils/env.rs
+++ core/src/utils/env.rs
@@ -4,7 +4,7 @@ use crate::{runtime::MAX_SHARD_CLK, utils::log2_strict_usize};
 pub fn shard_size() -> usize {
     let value = match std::env::var("SHARD_SIZE") {
         Ok(val) => val.parse().unwrap(),
-        Err(_) => 1 << 19,
+        Err(_) => 1 << 22,
     };
 
     assert!(
