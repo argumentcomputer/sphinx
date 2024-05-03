7dbfb64e5943fb6f129b8642c65b1ebd4f5dc4e0
Kevin Jue <kjue235@gmail.com>
2024-05-03 16:22:09 -0700

    chore(recursion): consolidate initial and finalize memory tables (#656)

--- recursion/core/src/memory/air.rs
+++ recursion/core/src/memory/air.rs
@@ -12,7 +12,6 @@ use wp1_core::{
 };
 
 use super::columns::MemoryInitCols;
-use crate::memory::MemoryChipKind;
 use crate::memory::MemoryGlobalChip;
 use crate::runtime::{ExecutionRecord, RecursionProgram};
 
@@ -20,9 +19,8 @@ pub(crate) const NUM_MEMORY_INIT_COLS: usize = size_of::<MemoryInitCols<u8>>();
 
 #[allow(dead_code)]
 impl MemoryGlobalChip {
-    pub fn new(kind: MemoryChipKind) -> Self {
+    pub fn new() -> Self {
         Self {
-            kind,
             fixed_log2_rows: None,
         }
     }
@@ -33,10 +31,7 @@ impl<F: PrimeField32> MachineAir<F> for MemoryGlobalChip {
     type Program = RecursionProgram<F>;
 
     fn name(&self) -> String {
-        match self.kind {
-            MemoryChipKind::Init => "MemoryInit".to_string(),
-            MemoryChipKind::Finalize => "MemoryFinalize".to_string(),
-        }
+        "MemoryGlobalChip".to_string()
     }
 
     fn generate_dependencies(&self, _: &Self::Record, _: &mut Self::Record) {
@@ -49,23 +44,28 @@ impl<F: PrimeField32> MachineAir<F> for MemoryGlobalChip {
         input: &ExecutionRecord<F>,
         _output: &mut ExecutionRecord<F>,
     ) -> RowMajorMatrix<F> {
-        let mut rows = match self.kind {
-            MemoryChipKind::Init => {
-                let addresses = &input.first_memory_record;
-                addresses
-                    .iter()
-                    .map(|(addr, value)| {
-                        let mut row = [F::zero(); NUM_MEMORY_INIT_COLS];
-                        let cols: &mut MemoryInitCols<F> = row.as_mut_slice().borrow_mut();
-                        cols.addr = *addr;
-                        cols.timestamp = F::zero();
-                        cols.value = *value;
-                        cols.is_real = F::one();
-                        row
-                    })
-                    .collect::<Vec<_>>()
-            }
-            MemoryChipKind::Finalize => input
+        let mut rows = Vec::new();
+
+        // Fill in the initial memory records.
+        rows.extend(
+            input
+                .first_memory_record
+                .iter()
+                .map(|(addr, value)| {
+                    let mut row = [F::zero(); NUM_MEMORY_INIT_COLS];
+                    let cols: &mut MemoryInitCols<F> = row.as_mut_slice().borrow_mut();
+                    cols.addr = *addr;
+                    cols.timestamp = F::zero();
+                    cols.value = *value;
+                    cols.is_initialize = F::one();
+                    row
+                })
+                .collect::<Vec<_>>(),
+        );
+
+        // Fill in the finalize memory records.
+        rows.extend(
+            input
                 .last_memory_record
                 .iter()
                 .map(|(addr, timestamp, value)| {
@@ -74,11 +74,11 @@ impl<F: PrimeField32> MachineAir<F> for MemoryGlobalChip {
                     cols.addr = *addr;
                     cols.timestamp = *timestamp;
                     cols.value = *value;
-                    cols.is_real = F::one();
+                    cols.is_finalize = F::one();
                     row
                 })
                 .collect::<Vec<_>>(),
-        };
+        );
 
         // Pad the trace to a power of two.
         pad_rows_fixed(
@@ -94,10 +94,7 @@ impl<F: PrimeField32> MachineAir<F> for MemoryGlobalChip {
     }
 
     fn included(&self, shard: &Self::Record) -> bool {
-        match self.kind {
-            MemoryChipKind::Init => !shard.first_memory_record.is_empty(),
-            MemoryChipKind::Finalize => !shard.last_memory_record.is_empty(),
-        }
+        !shard.first_memory_record.is_empty() || !shard.last_memory_record.is_empty()
     }
 }
 
@@ -116,35 +113,34 @@ where
         let local = main.row_slice(0);
         let local: &MemoryInitCols<AB::Var> = (*local).borrow();
 
-        match self.kind {
-            MemoryChipKind::Init => {
-                builder.send(AirInteraction::new(
-                    vec![
-                        local.timestamp.into(),
-                        local.addr.into(),
-                        local.value[0].into(),
-                        local.value[1].into(),
-                        local.value[2].into(),
-                        local.value[3].into(),
-                    ],
-                    local.is_real.into(),
-                    InteractionKind::Memory,
-                ));
-            }
-            MemoryChipKind::Finalize => {
-                builder.receive(AirInteraction::new(
-                    vec![
-                        local.timestamp.into(),
-                        local.addr.into(),
-                        local.value[0].into(),
-                        local.value[1].into(),
-                        local.value[2].into(),
-                        local.value[3].into(),
-                    ],
-                    local.is_real.into(),
-                    InteractionKind::Memory,
-                ));
-            }
-        };
+        // Verify that is_initilize and is_finalize are bool and that at most one is true.
+        builder.assert_bool(local.is_initialize);
+        builder.assert_bool(local.is_finalize);
+        builder.assert_bool(local.is_initialize + local.is_finalize);
+
+        builder.send(AirInteraction::new(
+            vec![
+                local.timestamp.into(),
+                local.addr.into(),
+                local.value[0].into(),
+                local.value[1].into(),
+                local.value[2].into(),
+                local.value[3].into(),
+            ],
+            local.is_initialize.into(),
+            InteractionKind::Memory,
+        ));
+        builder.receive(AirInteraction::new(
+            vec![
+                local.timestamp.into(),
+                local.addr.into(),
+                local.value[0].into(),
+                local.value[1].into(),
+                local.value[2].into(),
+                local.value[3].into(),
+            ],
+            local.is_finalize.into(),
+            InteractionKind::Memory,
+        ));
     }
 }
