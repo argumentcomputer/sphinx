2c3332e916b11a39310202dcc67a9a4e18cf7b0e
François Garillot <francois@garillot.net>
2024-05-07 08:53:56 -0400

    ci: un-ignore test_keccak_random

--- core/src/syscall/precompiles/keccak256/air.rs
+++ core/src/syscall/precompiles/keccak256/air.rs
@@ -143,7 +143,6 @@ mod test {
     const NUM_TEST_CASES: usize = 45;
 
     #[test]
-    #[ignore]
     fn test_keccak_random() {
         setup_logger();
         let mut rng = rand::rngs::StdRng::seed_from_u64(0);
