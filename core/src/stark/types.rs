2a2e4dda61968fcc5d9d267b5bc1ab6649f9bb80
Tamir Hemo <tamir@succinct.xyz>
2024-05-08 11:48:21 -0400

    feat: stark cleanup and verification (#556)

--- core/src/stark/types.rs
+++ core/src/stark/types.rs
@@ -132,7 +132,6 @@ pub const PROOF_MAX_NUM_PVS: usize = SP1_PROOF_NUM_PV_ELTS;
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(bound = "")]
 pub struct ShardProof<SC: StarkGenericConfig> {
-    pub index: usize,
     pub commitment: ShardCommitment<Com<SC>>,
     pub opened_values: ShardOpenedValues<Challenge<SC>>,
     pub opening_proof: OpeningProof<SC>,
