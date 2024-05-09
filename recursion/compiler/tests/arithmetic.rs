succinctlabs/sp1

feat: simplify compiler load/store  (#572)

recursion/compiler/tests/arithmetic.rs
@@ -4,8 +4,8 @@ use p3_field::AbstractField;
 use sp1_core::stark::StarkGenericConfig;
 use sp1_core::utils::BabyBearPoseidon2;
 use sp1_recursion_compiler::asm::AsmBuilder;
-use sp1_recursion_compiler::ir::ExtConst;
 use sp1_recursion_compiler::ir::{Ext, Felt};
+use sp1_recursion_compiler::ir::{ExtConst, Var};
 use sp1_recursion_core::runtime::Runtime;
 
 #[test]
@@ -33,15 +33,25 @@ fn test_compiler_arithmetic() {
     builder.assert_ext_eq(one_ext - one_ext, EF::zero().cons());
 
     for _ in 0..num_tests {
-        let a_val = rng.gen::<F>();
-        let b_val = rng.gen::<F>();
-        let a: Felt<_> = builder.eval(a_val);
-        let b: Felt<_> = builder.eval(b_val);
-        builder.assert_felt_eq(a + b, a_val + b_val);
-        builder.assert_felt_eq(a + b, a + b_val);
-        builder.assert_felt_eq(a * b, a_val * b_val);
-        builder.assert_felt_eq(a - b, a_val - b_val);
-        builder.assert_felt_eq(a / b, a_val / b_val);
+        let a_var_val = rng.gen::<F>();
+        let b_var_val = rng.gen::<F>();
+        let a_var: Var<_> = builder.eval(a_var_val);
+        let b_var: Var<_> = builder.eval(b_var_val);
+        builder.assert_var_eq(a_var + b_var, a_var_val + b_var_val);
+        builder.assert_var_eq(a_var * b_var, a_var_val * b_var_val);
+        builder.assert_var_eq(a_var - b_var, a_var_val - b_var_val);
+        builder.assert_var_eq(-a_var, -a_var_val);
+
+        let a_felt_val = rng.gen::<F>();
+        let b_felt_val = rng.gen::<F>();
+        let a: Felt<_> = builder.eval(a_felt_val);
+        let b: Felt<_> = builder.eval(b_felt_val);
+        builder.assert_felt_eq(a + b, a_felt_val + b_felt_val);
+        builder.assert_felt_eq(a + b, a + b_felt_val);
+        builder.assert_felt_eq(a * b, a_felt_val * b_felt_val);
+        builder.assert_felt_eq(a - b, a_felt_val - b_felt_val);
+        builder.assert_felt_eq(a / b, a_felt_val / b_felt_val);
+        builder.assert_felt_eq(-a, -a_felt_val);
 
         let a_ext_val = rng.gen::<EF>();
         let b_ext_val = rng.gen::<EF>();
@@ -51,6 +61,7 @@ fn test_compiler_arithmetic() {
         builder.assert_ext_eq(a_ext * b_ext, (a_ext_val * b_ext_val).cons());
         builder.assert_ext_eq(a_ext - b_ext, (a_ext_val - b_ext_val).cons());
         builder.assert_ext_eq(a_ext / b_ext, (a_ext_val / b_ext_val).cons());
+        builder.assert_ext_eq(-a_ext, (-a_ext_val).cons());
     }
 
     let program = builder.compile_program();

argumentcomputer/sphinx

recursion/compiler/tests/arithmetic.rs
@@ -1,10 +1,10 @@
 use p3_field::AbstractField;
-use rand::{thread_rng, Rng as _};
+use rand::{thread_rng, Rng};
 use wp1_core::stark::StarkGenericConfig;
 use wp1_core::utils::BabyBearPoseidon2;
 use wp1_recursion_compiler::asm::AsmBuilder;
-use wp1_recursion_compiler::ir::ExtConst;
 use wp1_recursion_compiler::ir::{Ext, Felt};
+use wp1_recursion_compiler::ir::{ExtConst, Var};
 use wp1_recursion_core::runtime::Runtime;
 
 #[test]
@@ -32,15 +32,25 @@ fn test_compiler_arithmetic() {
     builder.assert_ext_eq(one_ext - one_ext, EF::zero().cons());
 
     for _ in 0..num_tests {
-        let a_val = rng.gen::<F>();
-        let b_val = rng.gen::<F>();
-        let a: Felt<_> = builder.eval(a_val);
-        let b: Felt<_> = builder.eval(b_val);
-        builder.assert_felt_eq(a + b, a_val + b_val);
-        builder.assert_felt_eq(a + b, a + b_val);
-        builder.assert_felt_eq(a * b, a_val * b_val);
-        builder.assert_felt_eq(a - b, a_val - b_val);
-        builder.assert_felt_eq(a / b, a_val / b_val);
+        let a_var_val = rng.gen::<F>();
+        let b_var_val = rng.gen::<F>();
+        let a_var: Var<_> = builder.eval(a_var_val);
+        let b_var: Var<_> = builder.eval(b_var_val);
+        builder.assert_var_eq(a_var + b_var, a_var_val + b_var_val);
+        builder.assert_var_eq(a_var * b_var, a_var_val * b_var_val);
+        builder.assert_var_eq(a_var - b_var, a_var_val - b_var_val);
+        builder.assert_var_eq(-a_var, -a_var_val);
+
+        let a_felt_val = rng.gen::<F>();
+        let b_felt_val = rng.gen::<F>();
+        let a: Felt<_> = builder.eval(a_felt_val);
+        let b: Felt<_> = builder.eval(b_felt_val);
+        builder.assert_felt_eq(a + b, a_felt_val + b_felt_val);
+        builder.assert_felt_eq(a + b, a + b_felt_val);
+        builder.assert_felt_eq(a * b, a_felt_val * b_felt_val);
+        builder.assert_felt_eq(a - b, a_felt_val - b_felt_val);
+        builder.assert_felt_eq(a / b, a_felt_val / b_felt_val);
+        builder.assert_felt_eq(-a, -a_felt_val);
 
         let a_ext_val = rng.gen::<EF>();
         let b_ext_val = rng.gen::<EF>();
@@ -50,6 +60,7 @@ fn test_compiler_arithmetic() {
         builder.assert_ext_eq(a_ext * b_ext, (a_ext_val * b_ext_val).cons());
         builder.assert_ext_eq(a_ext - b_ext, (a_ext_val - b_ext_val).cons());
         builder.assert_ext_eq(a_ext / b_ext, (a_ext_val / b_ext_val).cons());
+        builder.assert_ext_eq(-a_ext, (-a_ext_val).cons());
     }
 
     let program = builder.compile_program();