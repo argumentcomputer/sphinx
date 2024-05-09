WindOctober/sp1

feat(recursion): recursion air builder (#574)

derive/src/lib.rs
@@ -94,7 +94,7 @@ pub fn aligned_borrow_derive(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(
     MachineAir,
-    attributes(sp1_core_path, execution_record_path, program_path)
+    attributes(sp1_core_path, execution_record_path, program_path, builder_path)
 )]
 pub fn machine_air_derive(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
@@ -104,6 +104,7 @@ pub fn machine_air_derive(input: TokenStream) -> TokenStream {
     let sp1_core_path = find_sp1_core_path(&ast.attrs);
     let execution_record_path = find_execution_record_path(&ast.attrs);
     let program_path = find_program_path(&ast.attrs);
+    let builder_path = find_builder_path(&ast.attrs);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     match &ast.data {
@@ -252,7 +253,7 @@ pub fn machine_air_derive(input: TokenStream) -> TokenStream {
             let mut new_generics = generics.clone();
             new_generics
                 .params
-                .push(syn::parse_quote! { AB: p3_air::PairBuilder + #sp1_core_path::air::SP1AirBuilder<F = F> });
+                .push(syn::parse_quote! { AB: p3_air::PairBuilder + #builder_path });
 
             let (air_impl_generics, _, _) = new_generics.split_for_impl();
 
@@ -343,4 +344,19 @@ fn find_program_path(attrs: &[syn::Attribute]) -> syn::Path {
         }
     }
     parse_quote!(crate::runtime::Program)
+}
+
+fn find_builder_path(attrs: &[syn::Attribute]) -> syn::Path {
+    for attr in attrs {
+        if attr.path.is_ident("builder_path") {
+            if let Ok(syn::Meta::NameValue(meta)) = attr.parse_meta() {
+                if let syn::Lit::Str(lit_str) = &meta.lit {
+                    if let Ok(path) = lit_str.parse::<syn::Path>() {
+                        return path;
+                    }
+                }
+            }
+        }
+    }
+    parse_quote!(crate::air::SP1AirBuilder<F = F>)
 }

recursion/core/src/air/builder.rs
@@ -1,0 +1,7 @@
+use p3_air::AirBuilderWithPublicValues;
+use sp1_core::air::{BaseAirBuilder, MachineAirBuilder};
+
+/// A trait which contains all helper methods for building SP1 recursion machine AIRs.
+pub trait SP1RecursionAirBuilder: MachineAirBuilder {}
+
+impl<AB: BaseAirBuilder + AirBuilderWithPublicValues> SP1RecursionAirBuilder for AB {}

argumentcomputer/sphinx

derive/src/lib.rs
@@ -87,7 +87,7 @@ pub fn aligned_borrow_derive(input: TokenStream) -> TokenStream {
 
 #[proc_macro_derive(
     MachineAir,
-    attributes(wp1_core_path, execution_record_path, program_path)
+    attributes(wp1_core_path, execution_record_path, program_path, builder_path)
 )]
 pub fn machine_air_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
@@ -97,6 +97,7 @@ pub fn machine_air_derive(input: TokenStream) -> TokenStream {
     let wp1_core_path = find_wp1_core_path(&ast.attrs);
     let execution_record_path = find_execution_record_path(&ast.attrs);
     let program_path = find_program_path(&ast.attrs);
+    let builder_path = find_builder_path(&ast.attrs);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     match &ast.data {
@@ -245,7 +246,7 @@ pub fn machine_air_derive(input: TokenStream) -> TokenStream {
             let mut new_generics = generics.clone();
             new_generics
                 .params
-                .push(syn::parse_quote! { AB: p3_air::PairBuilder + #wp1_core_path::air::SP1AirBuilder<F = F> });
+                .push(syn::parse_quote! { AB: p3_air::PairBuilder + #builder_path });
 
             let (air_impl_generics, _, _) = new_generics.split_for_impl();
 
@@ -340,4 +341,19 @@ fn find_program_path(attrs: &[syn::Attribute]) -> syn::Path {
         }
     }
     parse_quote!(crate::runtime::Program)
+}
+
+fn find_builder_path(attrs: &[syn::Attribute]) -> syn::Path {
+    for attr in attrs {
+        if attr.path.is_ident("builder_path") {
+            if let Ok(syn::Meta::NameValue(meta)) = attr.parse_meta() {
+                if let syn::Lit::Str(lit_str) = &meta.lit {
+                    if let Ok(path) = lit_str.parse::<syn::Path>() {
+                        return path;
+                    }
+                }
+            }
+        }
+    }
+    parse_quote!(crate::air::SP1AirBuilder<F = F>)
 }

recursion/core/src/air/builder.rs
@@ -1,0 +1,7 @@
+use p3_air::AirBuilderWithPublicValues;
+use wp1_core::air::{BaseAirBuilder, MachineAirBuilder};
+
+/// A trait which contains all helper methods for building SP1 recursion machine AIRs.
+pub trait SP1RecursionAirBuilder: MachineAirBuilder {}
+
+impl<AB: BaseAirBuilder + AirBuilderWithPublicValues> SP1RecursionAirBuilder for AB {}