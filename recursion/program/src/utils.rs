WindOctober/sp1

feat: complete reduce program (#565)

recursion/program/src/utils.rs
@@ -9,10 +9,14 @@ use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
 use sp1_core::stark::StarkGenericConfig;
 use sp1_core::utils::BabyBearPoseidon2;
 use sp1_recursion_compiler::asm::AsmConfig;
-use sp1_recursion_compiler::ir::{Builder, Felt, MemVariable, Var};
+use sp1_recursion_compiler::ir::{Array, Builder, Config, Felt, MemVariable, Var};
+use sp1_recursion_core::air::ChallengerPublicValues;
+use sp1_recursion_core::runtime::{DIGEST_SIZE, PERMUTATION_WIDTH};
 
+use crate::challenger::DuplexChallengerVariable;
 use crate::fri::types::FriConfigVariable;
 use crate::fri::TwoAdicMultiplicativeCosetVariable;
+use crate::types::VerifyingKeyVariable;
 
 type SC = BabyBearPoseidon2;
 type F = <SC as StarkGenericConfig>::Val;
@@ -62,7 +66,134 @@ pub fn clone<T: MemVariable<C>>(builder: &mut RecursionBuilder, var: &T) -> T {
     builder.get(&arr, 0)
 }
 
-pub fn felt2var(builder: &mut RecursionBuilder, felt: Felt<BabyBear>) -> Var<BabyBear> {
+pub fn clone_array<T: MemVariable<C>>(
+    builder: &mut RecursionBuilder,
+    arr: &Array<C, T>,
+) -> Array<C, T> {
+    let mut new_arr = builder.dyn_array(arr.len());
+    builder.range(0, arr.len()).for_each(|i, builder| {
+        let var = builder.get(arr, i);
+        builder.set(&mut new_arr, i, var);
+    });
+    new_arr
+}
+
+// TODO: this can be done much more efficiently, but in the meantime this should work
+pub fn felt2var<C: Config>(builder: &mut Builder<C>, felt: Felt<C::F>) -> Var<C::N> {
     let bits = builder.num2bits_f(felt);
     builder.bits2num_v(&bits)
+}
+
+pub fn var2felt<C: Config>(builder: &mut Builder<C>, var: Var<C::N>) -> Felt<C::F> {
+    let bits = builder.num2bits_v(var);
+    builder.bits2num_f(&bits)
+}
+
+/// Asserts that the challenger variable is equal to a challenger in public values.
+pub fn assert_challenger_eq_pv<C: Config>(
+    builder: &mut Builder<C>,
+    var: &DuplexChallengerVariable<C>,
+    values: ChallengerPublicValues<Felt<C::F>>,
+) {
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.sponge_state, i);
+        builder.assert_felt_eq(element, values.sponge_state[i]);
+    }
+    let num_inputs_var = felt2var(builder, values.num_inputs);
+    builder.assert_var_eq(var.nb_inputs, num_inputs_var);
+    let mut input_buffer_array: Array<_, Felt<_>> = builder.dyn_array(PERMUTATION_WIDTH);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut input_buffer_array, i, values.input_buffer[i]);
+    }
+    builder.range(0, num_inputs_var).for_each(|i, builder| {
+        let element = builder.get(&var.input_buffer, i);
+        let values_element = builder.get(&input_buffer_array, i);
+        builder.assert_felt_eq(element, values_element);
+    });
+    let num_outputs_var = felt2var(builder, values.num_outputs);
+    builder.assert_var_eq(var.nb_outputs, num_outputs_var);
+    let mut output_buffer_array: Array<_, Felt<_>> = builder.dyn_array(PERMUTATION_WIDTH);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut output_buffer_array, i, values.output_buffer[i]);
+    }
+    builder.range(0, num_outputs_var).for_each(|i, builder| {
+        let element = builder.get(&var.output_buffer, i);
+        let values_element = builder.get(&output_buffer_array, i);
+        builder.assert_felt_eq(element, values_element);
+    });
+}
+
+/// Assigns a challenger variable from a challenger in public values.
+pub fn assign_challenger_from_pv<C: Config>(
+    builder: &mut Builder<C>,
+    dst: &mut DuplexChallengerVariable<C>,
+    values: ChallengerPublicValues<Felt<C::F>>,
+) {
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.sponge_state, i, values.sponge_state[i]);
+    }
+    let num_inputs_var = felt2var(builder, values.num_inputs);
+    builder.assign(dst.nb_inputs, num_inputs_var);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.input_buffer, i, values.input_buffer[i]);
+    }
+    let num_outputs_var = felt2var(builder, values.num_outputs);
+    builder.assign(dst.nb_outputs, num_outputs_var);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.output_buffer, i, values.output_buffer[i]);
+    }
+}
+
+/// Commits a challenger variable to public values.
+pub fn commit_challenger<C: Config>(builder: &mut Builder<C>, var: &DuplexChallengerVariable<C>) {
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.sponge_state, i);
+        builder.commit_public_value(element);
+    }
+    let num_inputs_felt = var2felt(builder, var.nb_inputs);
+    builder.commit_public_value(num_inputs_felt);
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.input_buffer, i);
+        builder.commit_public_value(element);
+    }
+    let num_outputs_felt = var2felt(builder, var.nb_outputs);
+    builder.commit_public_value(num_outputs_felt);
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.output_buffer, i);
+        builder.commit_public_value(element);
+    }
+}
+
+/// Hash the verifying key + prep domains into a single digest.
+/// poseidon2( commit[0..8] || pc_start || prep_domains[N].{log_n, .size, .shift, .g})
+pub fn hash_vkey<C: Config>(
+    builder: &mut Builder<C>,
+    vk: &VerifyingKeyVariable<C>,
+    prep_domains: &Array<C, TwoAdicMultiplicativeCosetVariable<C>>,
+) -> Array<C, Felt<C::F>> {
+    let domain_slots: Var<_> = builder.eval(prep_domains.len() * 4);
+    let vkey_slots: Var<_> = builder.constant(C::N::from_canonical_usize(DIGEST_SIZE + 1));
+    let total_slots: Var<_> = builder.eval(vkey_slots + domain_slots);
+    let mut inputs = builder.dyn_array(total_slots);
+    builder.range(0, DIGEST_SIZE).for_each(|i, builder| {
+        let element = builder.get(&vk.commitment, i);
+        builder.set(&mut inputs, i, element);
+    });
+    builder.set(&mut inputs, DIGEST_SIZE, vk.pc_start);
+    let four: Var<_> = builder.constant(C::N::from_canonical_usize(4));
+    let one: Var<_> = builder.constant(C::N::one());
+    builder.range(0, prep_domains.len()).for_each(|i, builder| {
+        let domain = builder.get(prep_domains, i);
+        let log_n_index: Var<_> = builder.eval(vkey_slots + i * four);
+        let size_index: Var<_> = builder.eval(log_n_index + one);
+        let shift_index: Var<_> = builder.eval(size_index + one);
+        let g_index: Var<_> = builder.eval(shift_index + one);
+        let log_n_felt = var2felt(builder, domain.log_n);
+        let size_felt = var2felt(builder, domain.size);
+        builder.set(&mut inputs, log_n_index, log_n_felt);
+        builder.set(&mut inputs, size_index, size_felt);
+        builder.set(&mut inputs, shift_index, domain.shift);
+        builder.set(&mut inputs, g_index, domain.g);
+    });
+    builder.poseidon2_hash(&inputs)
 }

zkvm/entrypoint/src/syscalls/verify.rs
@@ -12,58 +12,51 @@ cfg_if::cfg_if! {
         use crate::zkvm::DEFERRED_PROOFS_DIGEST;
         use p3_baby_bear::BabyBear;
         use p3_field::AbstractField;
-        use sp1_primitives::poseidon2_hash;
+        use sp1_primitives::hash_deferred_proofs;
     }
 }
 
 #[no_mangle]
 #[allow(unused_variables)]
-pub fn syscall_verify_sp1_proof(vkey: &[u32; 8], pv_digest: &[u32; 8]) {
+pub fn syscall_verify_sp1_proof(vk_digest: &[u32; 8], pv_digest: &[u8; 32]) {
     #[cfg(target_os = "zkvm")]
     {
         // Call syscall to verify the next proof at runtime
         unsafe {
             asm!(
                 "ecall",
-                in("t0") crate::syscalls::VERIFY_SP1_PROOF,
-                in("a0") vkey.as_ptr(),
+                in("t0") VERIFY_SP1_PROOF,
+                in("a0") vk_digest.as_ptr(),
                 in("a1") pv_digest.as_ptr(),
             );
         }
 
-        // Update deferred_proofs_digest to be poseidon2_hash(deferred_proofs_digest || vkey || pv_digest)
-        let mut hash_input = Vec::with_capacity(24);
-        // First 8 bytes are previous hash (initially zero bytes)
+        // Update digest to p2_hash(prev_digest[0..8] || vkey_digest[0..8] || pv_digest[0..32])
+        let mut hash_input = Vec::with_capacity(48);
+        // First 8 elements are previous hash (initially zero)
         let deferred_proofs_digest;
         // SAFETY: we have sole access because zkvm is single threaded.
         unsafe {
             deferred_proofs_digest = DEFERRED_PROOFS_DIGEST.as_mut().unwrap();
-            hash_input.extend_from_slice(deferred_proofs_digest);
         }
-        // Next 8 bytes are vkey converted from u32s to BabyBear
-        let vkey_baby_bear = vkey
-            .iter()
-            .flat_map(|x| {
-                x.to_le_bytes()
-                    .iter()
-                    .map(|b| BabyBear::from_canonical_u8(*b))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-        hash_input.extend_from_slice(&vkey_baby_bear);
-        // Remaining bytes are proof converted from u32s to BabyBear
-        let pv_digest_baby_bear = pv_digest
-            .iter()
-            .flat_map(|x| {
-                x.to_le_bytes()
-                    .iter()
-                    .map(|b| BabyBear::from_canonical_u8(*b))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-        hash_input.extend_from_slice(&pv_digest_baby_bear);
+        hash_input.extend_from_slice(deferred_proofs_digest);
 
-        *deferred_proofs_digest = poseidon2_hash(hash_input);
+        // Next 8 elements are vkey_digest
+        let vk_digest_babybear = vk_digest
+            .iter()
+            .map(|x| BabyBear::from_canonical_u32(*x))
+            .collect::<Vec<_>>();
+        // Remaining 32 elements are pv_digest converted from u8 to BabyBear
+        let pv_digest_babybear = pv_digest
+            .iter()
+            .map(|b| BabyBear::from_canonical_u8(*b))
+            .collect::<Vec<_>>();
+
+        *deferred_proofs_digest = hash_deferred_proofs(
+            deferred_proofs_digest,
+            &vk_digest_babybear.try_into().unwrap(),
+            &pv_digest_babybear.try_into().unwrap(),
+        );
     }
 
     #[cfg(not(target_os = "zkvm"))]

zkvm/precompiles/src/verify.rs
@@ -1,6 +1,6 @@
 use crate::syscall_verify_sp1_proof;
 
-pub fn verify_sp1_proof(commitment: &[u32; 8], pv_digest: &[u32; 8]) {
+pub fn verify_sp1_proof(commitment: &[u32; 8], pv_digest: &[u8; 32]) {
     unsafe {
         syscall_verify_sp1_proof(commitment, pv_digest);
     }

argumentcomputer/sphinx

recursion/program/src/utils.rs
@@ -9,10 +9,14 @@ use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
 use wp1_core::stark::StarkGenericConfig;
 use wp1_core::utils::BabyBearPoseidon2;
 use wp1_recursion_compiler::asm::AsmConfig;
-use wp1_recursion_compiler::ir::{Builder, Felt, MemVariable, Var};
+use wp1_recursion_compiler::ir::{Array, Builder, Config, Felt, MemVariable, Var};
+use wp1_recursion_core::air::ChallengerPublicValues;
+use wp1_recursion_core::runtime::{DIGEST_SIZE, PERMUTATION_WIDTH};
 
+use crate::challenger::DuplexChallengerVariable;
 use crate::fri::types::FriConfigVariable;
 use crate::fri::TwoAdicMultiplicativeCosetVariable;
+use crate::types::VerifyingKeyVariable;
 
 type SC = BabyBearPoseidon2;
 type F = <SC as StarkGenericConfig>::Val;
@@ -62,7 +66,134 @@ pub fn clone<T: MemVariable<C>>(builder: &mut RecursionBuilder, var: &T) -> T {
     builder.get(&arr, 0)
 }
 
-pub fn felt2var(builder: &mut RecursionBuilder, felt: Felt<BabyBear>) -> Var<BabyBear> {
+pub fn clone_array<T: MemVariable<C>>(
+    builder: &mut RecursionBuilder,
+    arr: &Array<C, T>,
+) -> Array<C, T> {
+    let mut new_arr = builder.dyn_array(arr.len());
+    builder.range(0, arr.len()).for_each(|i, builder| {
+        let var = builder.get(arr, i);
+        builder.set(&mut new_arr, i, var);
+    });
+    new_arr
+}
+
+// TODO: this can be done much more efficiently, but in the meantime this should work
+pub fn felt2var<C: Config>(builder: &mut Builder<C>, felt: Felt<C::F>) -> Var<C::N> {
     let bits = builder.num2bits_f(felt);
     builder.bits2num_v(&bits)
+}
+
+pub fn var2felt<C: Config>(builder: &mut Builder<C>, var: Var<C::N>) -> Felt<C::F> {
+    let bits = builder.num2bits_v(var);
+    builder.bits2num_f(&bits)
+}
+
+/// Asserts that the challenger variable is equal to a challenger in public values.
+pub fn assert_challenger_eq_pv<C: Config>(
+    builder: &mut Builder<C>,
+    var: &DuplexChallengerVariable<C>,
+    values: ChallengerPublicValues<Felt<C::F>>,
+) {
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.sponge_state, i);
+        builder.assert_felt_eq(element, values.sponge_state[i]);
+    }
+    let num_inputs_var = felt2var(builder, values.num_inputs);
+    builder.assert_var_eq(var.nb_inputs, num_inputs_var);
+    let mut input_buffer_array: Array<_, Felt<_>> = builder.dyn_array(PERMUTATION_WIDTH);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut input_buffer_array, i, values.input_buffer[i]);
+    }
+    builder.range(0, num_inputs_var).for_each(|i, builder| {
+        let element = builder.get(&var.input_buffer, i);
+        let values_element = builder.get(&input_buffer_array, i);
+        builder.assert_felt_eq(element, values_element);
+    });
+    let num_outputs_var = felt2var(builder, values.num_outputs);
+    builder.assert_var_eq(var.nb_outputs, num_outputs_var);
+    let mut output_buffer_array: Array<_, Felt<_>> = builder.dyn_array(PERMUTATION_WIDTH);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut output_buffer_array, i, values.output_buffer[i]);
+    }
+    builder.range(0, num_outputs_var).for_each(|i, builder| {
+        let element = builder.get(&var.output_buffer, i);
+        let values_element = builder.get(&output_buffer_array, i);
+        builder.assert_felt_eq(element, values_element);
+    });
+}
+
+/// Assigns a challenger variable from a challenger in public values.
+pub fn assign_challenger_from_pv<C: Config>(
+    builder: &mut Builder<C>,
+    dst: &mut DuplexChallengerVariable<C>,
+    values: ChallengerPublicValues<Felt<C::F>>,
+) {
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.sponge_state, i, values.sponge_state[i]);
+    }
+    let num_inputs_var = felt2var(builder, values.num_inputs);
+    builder.assign(&dst.nb_inputs, num_inputs_var);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.input_buffer, i, values.input_buffer[i]);
+    }
+    let num_outputs_var = felt2var(builder, values.num_outputs);
+    builder.assign(&dst.nb_outputs, num_outputs_var);
+    for i in 0..PERMUTATION_WIDTH {
+        builder.set(&mut dst.output_buffer, i, values.output_buffer[i]);
+    }
+}
+
+/// Commits a challenger variable to public values.
+pub fn commit_challenger<C: Config>(builder: &mut Builder<C>, var: &DuplexChallengerVariable<C>) {
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.sponge_state, i);
+        builder.commit_public_value(element);
+    }
+    let num_inputs_felt = var2felt(builder, var.nb_inputs);
+    builder.commit_public_value(num_inputs_felt);
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.input_buffer, i);
+        builder.commit_public_value(element);
+    }
+    let num_outputs_felt = var2felt(builder, var.nb_outputs);
+    builder.commit_public_value(num_outputs_felt);
+    for i in 0..PERMUTATION_WIDTH {
+        let element = builder.get(&var.output_buffer, i);
+        builder.commit_public_value(element);
+    }
+}
+
+/// Hash the verifying key + prep domains into a single digest.
+/// poseidon2( commit[0..8] || pc_start || prep_domains[N].{log_n, .size, .shift, .g})
+pub fn hash_vkey<C: Config>(
+    builder: &mut Builder<C>,
+    vk: &VerifyingKeyVariable<C>,
+    prep_domains: &Array<C, TwoAdicMultiplicativeCosetVariable<C>>,
+) -> Array<C, Felt<C::F>> {
+    let domain_slots: Var<_> = builder.eval(prep_domains.len() * 4);
+    let vkey_slots: Var<_> = builder.constant(C::N::from_canonical_usize(DIGEST_SIZE + 1));
+    let total_slots: Var<_> = builder.eval(vkey_slots + domain_slots);
+    let mut inputs = builder.dyn_array(total_slots);
+    builder.range(0, DIGEST_SIZE).for_each(|i, builder| {
+        let element = builder.get(&vk.commitment, i);
+        builder.set(&mut inputs, i, element);
+    });
+    builder.set(&mut inputs, DIGEST_SIZE, vk.pc_start);
+    let four: Var<_> = builder.constant(C::N::from_canonical_usize(4));
+    let one: Var<_> = builder.constant(C::N::one());
+    builder.range(0, prep_domains.len()).for_each(|i, builder| {
+        let domain = builder.get(prep_domains, i);
+        let log_n_index: Var<_> = builder.eval(vkey_slots + i * four);
+        let size_index: Var<_> = builder.eval(log_n_index + one);
+        let shift_index: Var<_> = builder.eval(size_index + one);
+        let g_index: Var<_> = builder.eval(shift_index + one);
+        let log_n_felt = var2felt(builder, domain.log_n);
+        let size_felt = var2felt(builder, domain.size);
+        builder.set(&mut inputs, log_n_index, log_n_felt);
+        builder.set(&mut inputs, size_index, size_felt);
+        builder.set(&mut inputs, shift_index, domain.shift);
+        builder.set(&mut inputs, g_index, domain.g);
+    });
+    builder.poseidon2_hash(&inputs)
 }

zkvm/entrypoint/src/syscalls/verify.rs
@@ -11,13 +11,13 @@ cfg_if::cfg_if! {
         use crate::zkvm::DEFERRED_PROOFS_DIGEST;
         use p3_baby_bear::BabyBear;
         use p3_field::AbstractField;
-        use wp1_primitives::poseidon2_hash;
+        use wp1_primitives::hash_deferred_proofs;
     }
 }
 
 #[no_mangle]
 #[allow(unused_variables)]
-pub fn syscall_verify_wp1_proof(vkey: &[u32; 8], pv_digest: &[u32; 8]) {
+pub fn syscall_verify_wp1_proof(vk_digest: &[u32; 8], pv_digest: &[u8; 32]) {
     #[cfg(target_os = "zkvm")]
     {
         // Call syscall to verify the next proof at runtime
@@ -25,44 +25,37 @@ pub fn syscall_verify_wp1_proof(vkey: &[u32; 8], pv_digest: &[u32; 8]) {
             asm!(
                 "ecall",
                 in("t0") crate::syscalls::VERIFY_SP1_PROOF,
-                in("a0") vkey.as_ptr(),
+                in("a0") vk_digest.as_ptr(),
                 in("a1") pv_digest.as_ptr(),
             );
         }
 
-        // Update deferred_proofs_digest to be poseidon2_hash(deferred_proofs_digest || vkey || pv_digest)
-        let mut hash_input = Vec::with_capacity(24);
-        // First 8 bytes are previous hash (initially zero bytes)
+        // Update digest to p2_hash(prev_digest[0..8] || vkey_digest[0..8] || pv_digest[0..32])
+        let mut hash_input = Vec::with_capacity(48);
+        // First 8 elements are previous hash (initially zero)
         let deferred_proofs_digest;
         // SAFETY: we have sole access because zkvm is single threaded.
         unsafe {
             deferred_proofs_digest = DEFERRED_PROOFS_DIGEST.as_mut().unwrap();
-            hash_input.extend_from_slice(deferred_proofs_digest);
         }
-        // Next 8 bytes are vkey converted from u32s to BabyBear
-        let vkey_baby_bear = vkey
-            .iter()
-            .flat_map(|x| {
-                x.to_le_bytes()
-                    .iter()
-                    .map(|b| BabyBear::from_canonical_u8(*b))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-        hash_input.extend_from_slice(&vkey_baby_bear);
-        // Remaining bytes are proof converted from u32s to BabyBear
-        let pv_digest_baby_bear = pv_digest
-            .iter()
-            .flat_map(|x| {
-                x.to_le_bytes()
-                    .iter()
-                    .map(|b| BabyBear::from_canonical_u8(*b))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-        hash_input.extend_from_slice(&pv_digest_baby_bear);
+        hash_input.extend_from_slice(deferred_proofs_digest);
 
-        *deferred_proofs_digest = poseidon2_hash(hash_input);
+        // Next 8 elements are vkey_digest
+        let vk_digest_babybear = vk_digest
+            .iter()
+            .map(|x| BabyBear::from_canonical_u32(*x))
+            .collect::<Vec<_>>();
+        // Remaining 32 elements are pv_digest converted from u8 to BabyBear
+        let pv_digest_babybear = pv_digest
+            .iter()
+            .map(|b| BabyBear::from_canonical_u8(*b))
+            .collect::<Vec<_>>();
+
+        *deferred_proofs_digest = hash_deferred_proofs(
+            deferred_proofs_digest,
+            &vk_digest_babybear.try_into().unwrap(),
+            &pv_digest_babybear.try_into().unwrap(),
+        );
     }
 
     #[cfg(not(target_os = "zkvm"))]

zkvm/precompiles/src/verify.rs
@@ -1,6 +1,6 @@
 use crate::syscall_verify_wp1_proof;
 
-pub fn verify_wp1_proof(commitment: &[u32; 8], pv_digest: &[u32; 8]) {
+pub fn verify_wp1_proof(commitment: &[u32; 8], pv_digest: &[u8; 32]) {
     unsafe {
         syscall_verify_wp1_proof(commitment, pv_digest);
     }