WindOctober/sp1

feat: expression caching  (#586)
Co-authored-by: John Guibas <jtguibas@gmail.com>

recursion/compiler/tests/arithmetic.rs
@@ -4,7 +4,7 @@ use p3_field::AbstractField;
 use sp1_core::stark::StarkGenericConfig;
 use sp1_core::utils::BabyBearPoseidon2;
 use sp1_recursion_compiler::asm::AsmBuilder;
-use sp1_recursion_compiler::ir::{Ext, Felt};
+use sp1_recursion_compiler::ir::{Ext, Felt, SymbolicExt};
 use sp1_recursion_compiler::ir::{ExtConst, Var};
 use sp1_recursion_core::runtime::Runtime;
 
@@ -58,6 +58,17 @@ fn test_compiler_arithmetic() {
         let a_ext: Ext<_, _> = builder.eval(a_ext_val.cons());
         let b_ext: Ext<_, _> = builder.eval(b_ext_val.cons());
         builder.assert_ext_eq(a_ext + b_ext, (a_ext_val + b_ext_val).cons());
+        builder.assert_ext_eq(
+            -a_ext / b_ext + (a_ext * b_ext) * (a_ext * b_ext),
+            (-a_ext_val / b_ext_val + (a_ext_val * b_ext_val) * (a_ext_val * b_ext_val)).cons(),
+        );
+        let mut a_expr = SymbolicExt::from(a_ext);
+        let mut a_val = a_ext_val;
+        for _ in 0..10 {
+            a_expr += b_ext * a_val + EF::one();
+            a_val += b_ext_val * a_val + EF::one();
+            builder.assert_ext_eq(a_expr.clone(), a_val.cons())
+        }
         builder.assert_ext_eq(a_ext * b_ext, (a_ext_val * b_ext_val).cons());
         builder.assert_ext_eq(a_ext - b_ext, (a_ext_val - b_ext_val).cons());
         builder.assert_ext_eq(a_ext / b_ext, (a_ext_val / b_ext_val).cons());

recursion/program/src/fri/mod.rs
@@ -4,6 +4,7 @@ pub mod two_adic_pcs;
 pub mod types;
 
 pub use domain::*;
+use sp1_recursion_compiler::ir::ExtensionOperand;
 #[cfg(test)]
 pub(crate) use two_adic_pcs::tests::*;
 pub use two_adic_pcs::*;
@@ -17,8 +18,6 @@ use sp1_recursion_compiler::ir::Builder;
 use sp1_recursion_compiler::ir::Config;
 use sp1_recursion_compiler::ir::Ext;
 use sp1_recursion_compiler::ir::Felt;
-use sp1_recursion_compiler::ir::SymbolicExt;
-use sp1_recursion_compiler::ir::SymbolicFelt;
 use sp1_recursion_compiler::ir::SymbolicVar;
 use sp1_recursion_compiler::ir::Usize;
 use sp1_recursion_compiler::ir::Var;
@@ -142,9 +141,9 @@ where
     builder.cycle_tracker("verify-query");
     let folded_eval: Ext<C::F, C::EF> = builder.eval(C::F::zero());
     let two_adic_generator_f = config.get_two_adic_generator(builder, log_max_height);
-    let two_adic_generator_ef: Ext<_, _> = builder.eval(SymbolicExt::Base(
-        SymbolicFelt::Val(two_adic_generator_f).into(),
-    ));
+
+    let two_adic_gen_ext = two_adic_generator_f.to_operand().symbolic();
+    let two_adic_generator_ef: Ext<_, _> = builder.eval(two_adic_gen_ext);
 
     let x = builder.exp_reverse_bits_len(two_adic_generator_ef, index_bits, log_max_height);
 
@@ -163,7 +162,7 @@ where
 
             let index_bit = builder.get(index_bits, i);
             let index_sibling_mod_2: Var<C::N> =
-                builder.eval(SymbolicVar::Const(C::N::one()) - index_bit);
+                builder.eval(SymbolicVar::from(C::N::one()) - index_bit);
             let i_plus_one = builder.eval(i + C::N::one());
             let index_pair = index_bits.shift(builder, i_plus_one);
 

argumentcomputer/sphinx

recursion/compiler/tests/arithmetic.rs
@@ -3,7 +3,7 @@ use rand::{thread_rng, Rng};
 use wp1_core::stark::StarkGenericConfig;
 use wp1_core::utils::BabyBearPoseidon2;
 use wp1_recursion_compiler::asm::AsmBuilder;
-use wp1_recursion_compiler::ir::{Ext, Felt};
+use wp1_recursion_compiler::ir::{Ext, Felt, SymbolicExt};
 use wp1_recursion_compiler::ir::{ExtConst, Var};
 use wp1_recursion_core::runtime::Runtime;
 
@@ -57,6 +57,17 @@ fn test_compiler_arithmetic() {
         let a_ext: Ext<_, _> = builder.eval(a_ext_val.cons());
         let b_ext: Ext<_, _> = builder.eval(b_ext_val.cons());
         builder.assert_ext_eq(a_ext + b_ext, (a_ext_val + b_ext_val).cons());
+        builder.assert_ext_eq(
+            -a_ext / b_ext + (a_ext * b_ext) * (a_ext * b_ext),
+            (-a_ext_val / b_ext_val + (a_ext_val * b_ext_val) * (a_ext_val * b_ext_val)).cons(),
+        );
+        let mut a_expr = SymbolicExt::from(a_ext);
+        let mut a_val = a_ext_val;
+        for _ in 0..10 {
+            a_expr += b_ext * a_val + EF::one();
+            a_val += b_ext_val * a_val + EF::one();
+            builder.assert_ext_eq(a_expr.clone(), a_val.cons())
+        }
         builder.assert_ext_eq(a_ext * b_ext, (a_ext_val * b_ext_val).cons());
         builder.assert_ext_eq(a_ext - b_ext, (a_ext_val - b_ext_val).cons());
         builder.assert_ext_eq(a_ext / b_ext, (a_ext_val / b_ext_val).cons());

recursion/program/src/fri/mod.rs
@@ -4,13 +4,23 @@ pub mod two_adic_pcs;
 pub mod types;
 
 pub use domain::*;
-use p3_field::{AbstractField, Field, TwoAdicField};
 #[cfg(test)]
 pub(crate) use two_adic_pcs::tests::*;
 pub use two_adic_pcs::*;
-use wp1_recursion_compiler::ir::{
-    Array, Builder, Config, Ext, Felt, SymbolicExt, SymbolicFelt, SymbolicVar, Usize, Var,
-};
+use wp1_recursion_compiler::ir::ExtensionOperand;
+
+use p3_field::AbstractField;
+use p3_field::Field;
+use p3_field::TwoAdicField;
+
+use wp1_recursion_compiler::ir::Array;
+use wp1_recursion_compiler::ir::Builder;
+use wp1_recursion_compiler::ir::Config;
+use wp1_recursion_compiler::ir::Ext;
+use wp1_recursion_compiler::ir::Felt;
+use wp1_recursion_compiler::ir::SymbolicVar;
+use wp1_recursion_compiler::ir::Usize;
+use wp1_recursion_compiler::ir::Var;
 
 use self::types::DigestVariable;
 use self::types::DimensionsVariable;
@@ -130,9 +140,9 @@ where
     builder.cycle_tracker("verify-query");
     let folded_eval: Ext<C::F, C::EF> = builder.eval(C::F::zero());
     let two_adic_generator_f = config.get_two_adic_generator(builder, log_max_height);
-    let two_adic_generator_ef: Ext<_, _> = builder.eval(SymbolicExt::Base(
-        SymbolicFelt::Val(two_adic_generator_f).into(),
-    ));
+
+    let two_adic_gen_ext = two_adic_generator_f.to_operand().symbolic();
+    let two_adic_generator_ef: Ext<_, _> = builder.eval(two_adic_gen_ext);
 
     let x = builder.exp_reverse_bits_len(two_adic_generator_ef, index_bits, log_max_height);
 
@@ -151,7 +161,7 @@ where
 
             let index_bit = builder.get(index_bits, i);
             let index_sibling_mod_2: Var<C::N> =
-                builder.eval(SymbolicVar::Const(C::N::one()) - index_bit);
+                builder.eval(SymbolicVar::from(C::N::one()) - index_bit);
             let i_plus_one = builder.eval(i + C::N::one());
             let index_pair = index_bits.shift(builder, i_plus_one);
 