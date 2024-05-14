succinctlabs/sp1

chore(recursion): re-organized cpu chip and trace (#613)

recursion/core/src/cpu/air/alu.rs
@@ -1,0 +1,54 @@
+use p3_field::Field;
+use sp1_core::air::{BinomialExtension, ExtensionAirBuilder};
+
+use crate::{
+    air::{BinomialExtensionUtils, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the ALU operations.
+    pub fn eval_alu<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Convert operand values from Block<Var> to BinomialExtension<Expr>.
+        let a_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.a.value().map(|x| x.into()));
+        let b_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.b.value().map(|x| x.into()));
+        let c_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.c.value().map(|x| x.into()));
+
+        // Flag to check if the instruction is a field operation
+        let is_field_op = local.selectors.is_add
+            + local.selectors.is_sub
+            + local.selectors.is_mul
+            + local.selectors.is_div;
+
+        // Verify that the b and c registers are base elements for field operations.
+        builder
+            .when(is_field_op.clone())
+            .assert_is_base_element(b_ext.clone());
+        builder
+            .when(is_field_op)
+            .assert_is_base_element(c_ext.clone());
+
+        // Verify the actual operation.
+        builder
+            .when(local.selectors.is_add + local.selectors.is_eadd)
+            .assert_ext_eq(a_ext.clone(), b_ext.clone() + c_ext.clone());
+        builder
+            .when(local.selectors.is_sub + local.selectors.is_esub)
+            .assert_ext_eq(a_ext.clone(), b_ext.clone() - c_ext.clone());
+        // TODO:  Figure out why this fails in the groth16 proof.
+        // builder
+        //     .when(local.selectors.is_mul + local.selectors.is_emul)
+        //     .assert_ext_eq(a_ext.clone(), b_ext.clone() * c_ext.clone());
+        // // For div operation, we assert that b == a * c (equivalent to a == b / c).
+        builder
+            .when(local.selectors.is_div + local.selectors.is_ediv)
+            .assert_ext_eq(b_ext, a_ext * c_ext);
+    }
+}

recursion/core/src/cpu/air/branch.rs
@@ -1,0 +1,72 @@
+use p3_air::AirBuilder;
+use p3_field::{AbstractField, Field};
+use sp1_core::air::{BinomialExtension, ExtensionAirBuilder};
+
+use crate::{
+    air::{BinomialExtensionUtils, IsExtZeroOperation, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the branch operations.
+    pub fn eval_branch<AB>(
+        &self,
+        builder: &mut AB,
+        local: &CpuCols<AB::Var>,
+        next_pc: &mut AB::Expr,
+    ) where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        let branch_cols = local.opcode_specific.branch();
+        let is_branch_instruction = self.is_branch_instruction::<AB>(local);
+        let one = AB::Expr::one();
+
+        // If the instruction is a BNEINC, verify that the a value is incremented by one.
+        builder
+            .when(local.is_real)
+            .when(local.selectors.is_bneinc)
+            .assert_eq(local.a.value()[0], local.a.prev_value()[0] + one.clone());
+
+        // Convert operand values from Block<Var> to BinomialExtension<Expr>.  Note that it gets the
+        // previous value of the `a` and `b` operands, since BNENIC will modify `a`.
+        let a_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.a.value().map(|x| x.into()));
+        let b_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.b.value().map(|x| x.into()));
+
+        let comparison_diff = a_ext - b_ext;
+
+        // Verify branch_cols.camparison_diff col.
+        builder.when(is_branch_instruction.clone()).assert_ext_eq(
+            BinomialExtension::from(branch_cols.comparison_diff_val),
+            comparison_diff,
+        );
+
+        // Verify branch_cols.comparison_diff.result col.
+        IsExtZeroOperation::<AB::F>::eval(
+            builder,
+            BinomialExtension::from(branch_cols.comparison_diff_val),
+            branch_cols.comparison_diff,
+            is_branch_instruction.clone(),
+        );
+
+        // Verify branch_col.do_branch col.
+        let mut do_branch = local.selectors.is_beq * branch_cols.comparison_diff.result;
+        do_branch += local.selectors.is_bne * (one.clone() - branch_cols.comparison_diff.result);
+        do_branch += local.selectors.is_bneinc * (one.clone() - branch_cols.comparison_diff.result);
+        builder
+            .when(is_branch_instruction.clone())
+            .assert_eq(branch_cols.do_branch, do_branch);
+
+        // Verify branch_col.next_pc col.
+        let pc_offset = local.c.value().0[0];
+        let expected_next_pc =
+            builder.if_else(branch_cols.do_branch, local.pc + pc_offset, local.pc + one);
+        builder
+            .when(is_branch_instruction.clone())
+            .assert_eq(branch_cols.next_pc, expected_next_pc);
+
+        *next_pc = is_branch_instruction * branch_cols.next_pc;
+    }
+}

recursion/core/src/cpu/air/memory.rs
@@ -1,0 +1,45 @@
+use p3_field::Field;
+use sp1_core::runtime::MemoryAccessPosition;
+
+use crate::{
+    air::SP1RecursionAirBuilder,
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    // Eval the memory instructions.
+    pub fn eval_memory<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Constraint all the memory access.
+
+        // Evaluate the memory column.
+        let load_memory = local.selectors.is_load + local.selectors.is_store;
+        let index = local.c.value()[0];
+        let ptr = local.b.value()[0];
+        let _memory_addr = ptr + index * local.instruction.size_imm + local.instruction.offset_imm;
+        // TODO: comment this back in to constraint the memory_addr column.
+        // When load_memory is true, then we check that the local.memory_addr column equals the computed
+        // memory_addr column from the other columns. Otherwise it is 0.
+        // builder.assert_eq(memory_addr * load_memory.clone(), local.memory_addr);
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::Memory as u32),
+            local.memory_addr,
+            &local.memory,
+            load_memory,
+        );
+
+        // Constraints on the memory column depending on load or store.
+        // We read from memory when it is a load.
+        // builder
+        //     .when(local.selectors.is_load)
+        //     .assert_block_eq(local.memory.prev_value, *local.memory.value());
+        // // When there is a store, we ensure that we are writing the value of the a operand to the memory.
+        // builder
+        //     .when(local.selectors.is_store)
+        //     .assert_block_eq(local.a.value, local.memory.value);
+    }
+}

recursion/core/src/cpu/air/operands.rs
@@ -1,0 +1,52 @@
+use p3_field::{AbstractField, Field};
+use sp1_core::runtime::MemoryAccessPosition;
+
+use crate::{
+    air::{BlockBuilder, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the operands.
+    pub fn eval_operands<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Constraint the case of immediates for the b and c operands.
+        builder
+            .when(local.instruction.imm_b)
+            .assert_block_eq::<AB::Var, AB::Var>(*local.b.value(), local.instruction.op_b);
+        builder
+            .when(local.instruction.imm_c)
+            .assert_block_eq::<AB::Var, AB::Var>(*local.c.value(), local.instruction.op_c);
+
+        // Constraint the operand accesses.
+        let a_addr = local.fp.into() + local.instruction.op_a.into();
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::A as u32),
+            a_addr,
+            &local.a,
+            local.is_real.into(),
+        );
+        // If the instruction only reads from operand A, then verify that previous and current values are equal.
+        let is_op_a_read_only = self.is_op_a_read_only_instruction::<AB>(local);
+        builder
+            .when(is_op_a_read_only)
+            .assert_block_eq(*local.a.prev_value(), *local.a.value());
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::B as u32),
+            local.fp.into() + local.instruction.op_b[0].into(),
+            &local.b,
+            AB::Expr::one() - local.instruction.imm_b.into(),
+        );
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::C as u32),
+            local.fp.into() + local.instruction.op_c[0].into(),
+            &local.c,
+            AB::Expr::one() - local.instruction.imm_c.into(),
+        );
+    }
+}

recursion/core/src/cpu/columns/mod.rs
@@ -1,4 +1,8 @@
+use std::mem::{size_of, transmute};
+
 use crate::memory::{MemoryReadCols, MemoryReadWriteCols};
+use p3_air::BaseAir;
+use sp1_core::utils::indices_arr;
 use sp1_derive::AlignedBorrow;
 
 mod branch;
@@ -10,6 +14,23 @@ pub use instruction::*;
 pub use opcode::*;
 
 use self::opcode_specific::OpcodeSpecificCols;
+
+use super::CpuChip;
+
+pub const NUM_CPU_COLS: usize = size_of::<CpuCols<u8>>();
+
+const fn make_col_map() -> CpuCols<usize> {
+    let indices_arr = indices_arr::<NUM_CPU_COLS>();
+    unsafe { transmute::<[usize; NUM_CPU_COLS], CpuCols<usize>>(indices_arr) }
+}
+
+pub(crate) const CPU_COL_MAP: CpuCols<usize> = make_col_map();
+
+impl<F: Send + Sync> BaseAir<F> for CpuChip<F> {
+    fn width(&self) -> usize {
+        NUM_CPU_COLS
+    }
+}
 
 /// The column layout for the chip.
 #[derive(AlignedBorrow, Default, Clone, Debug)]

argumentcomputer/sphinx

recursion/core/src/cpu/air/alu.rs
@@ -1,0 +1,54 @@
+use p3_field::Field;
+use wp1_core::air::{BinomialExtension, ExtensionAirBuilder};
+
+use crate::{
+    air::{BinomialExtensionUtils, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the ALU operations.
+    pub fn eval_alu<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Convert operand values from Block<Var> to BinomialExtension<Expr>.
+        let a_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.a.value().map(|x| x.into()));
+        let b_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.b.value().map(|x| x.into()));
+        let c_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.c.value().map(|x| x.into()));
+
+        // Flag to check if the instruction is a field operation
+        let is_field_op = local.selectors.is_add
+            + local.selectors.is_sub
+            + local.selectors.is_mul
+            + local.selectors.is_div;
+
+        // Verify that the b and c registers are base elements for field operations.
+        builder
+            .when(is_field_op.clone())
+            .assert_is_base_element(b_ext.clone());
+        builder
+            .when(is_field_op)
+            .assert_is_base_element(c_ext.clone());
+
+        // Verify the actual operation.
+        builder
+            .when(local.selectors.is_add + local.selectors.is_eadd)
+            .assert_ext_eq(a_ext.clone(), b_ext.clone() + c_ext.clone());
+        builder
+            .when(local.selectors.is_sub + local.selectors.is_esub)
+            .assert_ext_eq(a_ext.clone(), b_ext.clone() - c_ext.clone());
+        // TODO:  Figure out why this fails in the groth16 proof.
+        // builder
+        //     .when(local.selectors.is_mul + local.selectors.is_emul)
+        //     .assert_ext_eq(a_ext.clone(), b_ext.clone() * c_ext.clone());
+        // // For div operation, we assert that b == a * c (equivalent to a == b / c).
+        builder
+            .when(local.selectors.is_div + local.selectors.is_ediv)
+            .assert_ext_eq(b_ext, a_ext * c_ext);
+    }
+}

recursion/core/src/cpu/air/branch.rs
@@ -1,0 +1,72 @@
+use p3_air::AirBuilder;
+use p3_field::{AbstractField, Field};
+use wp1_core::air::{BinomialExtension, ExtensionAirBuilder};
+
+use crate::{
+    air::{BinomialExtensionUtils, IsExtZeroOperation, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the branch operations.
+    pub fn eval_branch<AB>(
+        &self,
+        builder: &mut AB,
+        local: &CpuCols<AB::Var>,
+        next_pc: &mut AB::Expr,
+    ) where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        let branch_cols = local.opcode_specific.branch();
+        let is_branch_instruction = self.is_branch_instruction::<AB>(local);
+        let one = AB::Expr::one();
+
+        // If the instruction is a BNEINC, verify that the a value is incremented by one.
+        builder
+            .when(local.is_real)
+            .when(local.selectors.is_bneinc)
+            .assert_eq(local.a.value()[0], local.a.prev_value()[0] + one.clone());
+
+        // Convert operand values from Block<Var> to BinomialExtension<Expr>.  Note that it gets the
+        // previous value of the `a` and `b` operands, since BNENIC will modify `a`.
+        let a_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.a.value().map(|x| x.into()));
+        let b_ext: BinomialExtension<AB::Expr> =
+            BinomialExtensionUtils::from_block(local.b.value().map(|x| x.into()));
+
+        let comparison_diff = a_ext - b_ext;
+
+        // Verify branch_cols.camparison_diff col.
+        builder.when(is_branch_instruction.clone()).assert_ext_eq(
+            BinomialExtension::from(&branch_cols.comparison_diff_val),
+            comparison_diff,
+        );
+
+        // Verify branch_cols.comparison_diff.result col.
+        IsExtZeroOperation::<AB::F>::eval(
+            builder,
+            BinomialExtension::from(&branch_cols.comparison_diff_val),
+            branch_cols.comparison_diff,
+            &is_branch_instruction,
+        );
+
+        // Verify branch_col.do_branch col.
+        let mut do_branch = local.selectors.is_beq * branch_cols.comparison_diff.result;
+        do_branch += local.selectors.is_bne * (one.clone() - branch_cols.comparison_diff.result);
+        do_branch += local.selectors.is_bneinc * (one.clone() - branch_cols.comparison_diff.result);
+        builder
+            .when(is_branch_instruction.clone())
+            .assert_eq(branch_cols.do_branch, do_branch);
+
+        // Verify branch_col.next_pc col.
+        let pc_offset = local.c.value().0[0];
+        let expected_next_pc =
+            builder.if_else(branch_cols.do_branch, local.pc + pc_offset, local.pc + one);
+        builder
+            .when(is_branch_instruction.clone())
+            .assert_eq(branch_cols.next_pc, expected_next_pc);
+
+        *next_pc = is_branch_instruction * branch_cols.next_pc;
+    }
+}

recursion/core/src/cpu/air/memory.rs
@@ -1,0 +1,45 @@
+use p3_field::Field;
+use wp1_core::runtime::MemoryAccessPosition;
+
+use crate::{
+    air::SP1RecursionAirBuilder,
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    // Eval the memory instructions.
+    pub fn eval_memory<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Constraint all the memory access.
+
+        // Evaluate the memory column.
+        let load_memory = local.selectors.is_load + local.selectors.is_store;
+        let index = local.c.value()[0];
+        let ptr = local.b.value()[0];
+        let _memory_addr = ptr + index * local.instruction.size_imm + local.instruction.offset_imm;
+        // TODO: comment this back in to constraint the memory_addr column.
+        // When load_memory is true, then we check that the local.memory_addr column equals the computed
+        // memory_addr column from the other columns. Otherwise it is 0.
+        // builder.assert_eq(memory_addr * load_memory.clone(), local.memory_addr);
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::Memory as u32),
+            local.memory_addr,
+            &local.memory,
+            load_memory,
+        );
+
+        // Constraints on the memory column depending on load or store.
+        // We read from memory when it is a load.
+        // builder
+        //     .when(local.selectors.is_load)
+        //     .assert_block_eq(local.memory.prev_value, *local.memory.value());
+        // // When there is a store, we ensure that we are writing the value of the a operand to the memory.
+        // builder
+        //     .when(local.selectors.is_store)
+        //     .assert_block_eq(local.a.value, local.memory.value);
+    }
+}

recursion/core/src/cpu/air/operands.rs
@@ -1,0 +1,52 @@
+use p3_field::{AbstractField, Field};
+use wp1_core::runtime::MemoryAccessPosition;
+
+use crate::{
+    air::{BlockBuilder, SP1RecursionAirBuilder},
+    cpu::{CpuChip, CpuCols},
+    memory::MemoryCols,
+};
+
+impl<F: Field> CpuChip<F> {
+    /// Eval the operands.
+    pub fn eval_operands<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder<F = F>,
+    {
+        // Constraint the case of immediates for the b and c operands.
+        builder
+            .when(local.instruction.imm_b)
+            .assert_block_eq::<AB::Var, AB::Var>(*local.b.value(), local.instruction.op_b);
+        builder
+            .when(local.instruction.imm_c)
+            .assert_block_eq::<AB::Var, AB::Var>(*local.c.value(), local.instruction.op_c);
+
+        // Constraint the operand accesses.
+        let a_addr = local.fp.into() + local.instruction.op_a.into();
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::A as u32),
+            a_addr,
+            &local.a,
+            local.is_real.into(),
+        );
+        // If the instruction only reads from operand A, then verify that previous and current values are equal.
+        let is_op_a_read_only = self.is_op_a_read_only_instruction::<AB>(local);
+        builder
+            .when(is_op_a_read_only)
+            .assert_block_eq(*local.a.prev_value(), *local.a.value());
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::B as u32),
+            local.fp.into() + local.instruction.op_b[0].into(),
+            &local.b,
+            AB::Expr::one() - local.instruction.imm_b.into(),
+        );
+
+        builder.recursion_eval_memory_access(
+            local.clk + AB::F::from_canonical_u32(MemoryAccessPosition::C as u32),
+            local.fp.into() + local.instruction.op_c[0].into(),
+            &local.c,
+            AB::Expr::one() - local.instruction.imm_c.into(),
+        );
+    }
+}

recursion/core/src/cpu/columns/mod.rs
@@ -1,4 +1,8 @@
+use std::mem::{size_of, transmute};
+
 use crate::memory::{MemoryReadCols, MemoryReadWriteCols};
+use p3_air::BaseAir;
+use wp1_core::utils::indices_arr;
 use wp1_derive::AlignedBorrow;
 
 mod branch;
@@ -10,6 +14,23 @@ pub use instruction::*;
 pub use opcode::*;
 
 use self::opcode_specific::OpcodeSpecificCols;
+
+use super::CpuChip;
+
+pub const NUM_CPU_COLS: usize = size_of::<CpuCols<u8>>();
+
+const fn make_col_map() -> CpuCols<usize> {
+    let indices_arr = indices_arr::<NUM_CPU_COLS>();
+    unsafe { transmute::<[usize; NUM_CPU_COLS], CpuCols<usize>>(indices_arr) }
+}
+
+pub(crate) const CPU_COL_MAP: CpuCols<usize> = make_col_map();
+
+impl<F: Send + Sync> BaseAir<F> for CpuChip<F> {
+    fn width(&self) -> usize {
+        NUM_CPU_COLS
+    }
+}
 
 /// The column layout for the chip.
 #[derive(AlignedBorrow, Default, Clone, Debug)]