succinctlabs/sp1

refactor: Implement `Prover` on `MockProver` (#629)

sdk/src/lib.rs
@@ -7,18 +7,23 @@ pub mod proto {
 }
 pub mod auth;
 pub mod client;
+mod local;
+mod mock;
+mod network;
 pub mod prove;
 pub mod utils;
 
 use anyhow::{Ok, Result};
-use prove::{
-    LocalProver, NetworkProver, Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof,
-    SP1PlonkProof,
-};
+use local::LocalProver;
+use network::NetworkProver;
 pub use sp1_prover::{
     CoreSC, SP1CoreProof, SP1Prover, SP1ProvingKey, SP1PublicValues, SP1Stdin, SP1VerifyingKey,
 };
-use std::env;
+use sp1_prover::{Groth16Proof, InnerSC, PlonkBn254Proof};
+use std::{env, fs::File, path::Path};
+
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use sp1_core::stark::ShardProof;
 
 /// A client that can prove RISCV ELFs and verify those proofs.
 pub struct ProverClient {
@@ -61,7 +66,7 @@ impl ProverClient {
     }
 
     /// Proves the execution of the given elf with the given inputs.
-    pub fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<prove::SP1DefaultProof> {
+    pub fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof> {
         self.prover.prove(pk, stdin)
     }
 
@@ -70,21 +75,17 @@ impl ProverClient {
         &self,
         pk: &SP1ProvingKey,
         stdin: SP1Stdin,
-    ) -> Result<prove::SP1CompressedProof> {
+    ) -> Result<SP1CompressedProof> {
         self.prover.prove_compressed(pk, stdin)
     }
 
     /// Generates a groth16 proof, verifiable onchain, of the given elf and stdin.
-    pub fn prove_groth16(
-        &self,
-        pk: &SP1ProvingKey,
-        stdin: SP1Stdin,
-    ) -> Result<prove::SP1Groth16Proof> {
+    pub fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof> {
         self.prover.prove_groth16(pk, stdin)
     }
 
     /// Generates a PLONK proof, verifiable onchain, of the given elf and stdin.
-    pub fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<prove::SP1PlonkProof> {
+    pub fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof> {
         self.prover.prove_plonk(pk, stdin)
     }
 
@@ -111,4 +112,78 @@ impl ProverClient {
     pub fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
         self.prover.verify_groth16(proof, vkey)
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProofStatistics {
+    pub cycle_count: u64,
+    pub cost: u64,
+    pub total_time: u64,
+    pub latency: u64,
+}
+
+/// A proof of a RISCV ELF execution with given inputs and outputs.
+#[derive(Serialize, Deserialize)]
+#[serde(bound(serialize = "P: Serialize"))]
+#[serde(bound(deserialize = "P: DeserializeOwned"))]
+pub struct SP1ProofWithMetadata<P> {
+    pub proof: P,
+    pub stdin: SP1Stdin,
+    pub public_values: SP1PublicValues,
+}
+
+impl<P: Serialize + DeserializeOwned> SP1ProofWithMetadata<P> {
+    pub fn save(&self, path: impl AsRef<Path>) -> Result<()> {
+        bincode::serialize_into(File::create(path).expect("failed to open file"), self)
+            .map_err(Into::into)
+    }
+
+    pub fn load(path: impl AsRef<Path>) -> Result<Self> {
+        bincode::deserialize_from(File::open(path).expect("failed to open file"))
+            .map_err(Into::into)
+    }
+}
+
+impl<P: std::fmt::Debug> std::fmt::Debug for SP1ProofWithMetadata<P> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SP1ProofWithMetadata")
+            .field("proof", &self.proof)
+            .finish()
+    }
+}
+
+pub type SP1DefaultProof = SP1ProofWithMetadata<Vec<ShardProof<CoreSC>>>;
+
+pub type SP1CompressedProof = SP1ProofWithMetadata<ShardProof<InnerSC>>;
+
+pub type SP1PlonkProof = SP1ProofWithMetadata<PlonkBn254Proof>;
+
+pub type SP1Groth16Proof = SP1ProofWithMetadata<Groth16Proof>;
+
+pub trait Prover: Send + Sync {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey);
+
+    /// Prove the execution of a RISCV ELF with the given inputs.
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof>;
+
+    /// Generate a compressed proof of the execution of a RISCV ELF with the given inputs.
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof>;
+
+    /// Given an SP1 program and input, generate a PLONK proof that can be verified on-chain.
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof>;
+
+    /// Given an SP1 program and input, generate a Groth16 proof that can be verified on-chain.
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof>;
+
+    /// Verify that an SP1 proof is valid given its vkey and metadata.
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a compressed SP1 proof is valid given its vkey and metadata.
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a SP1 PLONK proof is valid given its vkey and metadata.
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a SP1 Groth16 proof is valid given its vkey and metadata.
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()>;
 }

sdk/src/local.rs
@@ -1,0 +1,125 @@
+#![allow(unused_variables)]
+use std::path::PathBuf;
+
+use crate::{
+    Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof, SP1PlonkProof,
+    SP1ProofWithMetadata, SP1ProvingKey, SP1VerifyingKey,
+};
+use anyhow::Result;
+use sha2::{Digest, Sha256};
+use sp1_core::{
+    air::PublicValues,
+    stark::{MachineProof, StarkGenericConfig},
+};
+use sp1_prover::{SP1Prover, SP1Stdin};
+
+pub struct LocalProver {
+    pub(crate) prover: SP1Prover,
+}
+
+impl Default for LocalProver {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl LocalProver {
+    pub fn new() -> Self {
+        let prover = SP1Prover::new();
+        Self { prover }
+    }
+
+    /// Get artifacts dir from SP1_CIRCUIT_DIR env var.
+    fn get_artifacts_dir(&self) -> PathBuf {
+        let artifacts_dir =
+            std::env::var("SP1_CIRCUIT_DIR").expect("SP1_CIRCUIT_DIR env var not set");
+        PathBuf::from(artifacts_dir)
+    }
+}
+
+impl Prover for LocalProver {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey) {
+        self.prover.setup(elf)
+    }
+
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof> {
+        let proof = self.prover.prove_core(pk, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: proof.shard_proofs,
+            stdin: proof.stdin,
+            public_values: proof.public_values,
+        })
+    }
+
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof> {
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        Ok(SP1CompressedProof {
+            proof: reduce_proof.proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof> {
+        let artifacts_dir = self.get_artifacts_dir();
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        let compress_proof = self.prover.compress(&pk.vk, reduce_proof);
+        let outer_proof = self.prover.wrap_bn254(&pk.vk, compress_proof);
+        let proof = self.prover.wrap_groth16(outer_proof, artifacts_dir);
+        Ok(SP1ProofWithMetadata {
+            proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof> {
+        let artifacts_dir = self.get_artifacts_dir();
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        let compress_proof = self.prover.compress(&pk.vk, reduce_proof);
+        let outer_proof = self.prover.wrap_bn254(&pk.vk, compress_proof);
+        let proof = self.prover.wrap_plonk(outer_proof, artifacts_dir);
+        Ok(SP1ProofWithMetadata {
+            proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        let pv = PublicValues::from_vec(proof.proof[0].public_values.clone());
+        let pv_digest: [u8; 32] = Sha256::digest(&proof.public_values.buffer.data).into();
+        if pv_digest != *pv.commit_digest_bytes() {
+            return Err(anyhow::anyhow!("Public values digest mismatch"));
+        }
+        let machine_proof = MachineProof {
+            shard_proofs: proof.proof.clone(),
+        };
+        let mut challenger = self.prover.core_machine.config().challenger();
+        Ok(self
+            .prover
+            .core_machine
+            .verify(&vkey.vk, &machine_proof, &mut challenger)?)
+    }
+
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+}

sdk/src/mock.rs
@@ -1,0 +1,77 @@
+#![allow(unused_variables)]
+use crate::{
+    Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof, SP1PlonkProof,
+    SP1ProofWithMetadata, SP1ProvingKey, SP1VerifyingKey,
+};
+use anyhow::Result;
+use sp1_prover::{Groth16Proof, PlonkBn254Proof, SP1Prover, SP1Stdin};
+
+pub struct MockProver {
+    pub(crate) prover: SP1Prover,
+}
+
+impl Default for MockProver {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl MockProver {
+    pub fn new() -> Self {
+        let prover = SP1Prover::new();
+        Self { prover }
+    }
+}
+
+impl Prover for MockProver {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey) {
+        self.prover.setup(elf)
+    }
+
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: vec![],
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof> {
+        unimplemented!()
+    }
+
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: PlonkBn254Proof::default(),
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: Groth16Proof::default(),
+            stdin,
+            public_values,
+        })
+    }
+
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+}

argumentcomputer/sphinx

sdk/src/lib.rs
@@ -5,18 +5,23 @@ pub mod proto {
 }
 pub mod auth;
 pub mod client;
+mod local;
+mod mock;
+mod network;
 pub mod prove;
 pub mod utils;
 
 use anyhow::{Ok, Result};
-use prove::{
-    LocalProver, NetworkProver, Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof,
-    SP1PlonkProof,
-};
-use std::env;
+use local::LocalProver;
+use network::NetworkProver;
+use std::{env, fs::File, path::Path};
 pub use wp1_prover::{
     CoreSC, SP1CoreProof, SP1Prover, SP1ProvingKey, SP1PublicValues, SP1Stdin, SP1VerifyingKey,
 };
+use wp1_prover::{Groth16Proof, InnerSC, PlonkBn254Proof};
+
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use wp1_core::stark::ShardProof;
 
 /// A client that can prove RISCV ELFs and verify those proofs.
 pub struct ProverClient {
@@ -105,4 +110,78 @@ impl ProverClient {
     pub fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
         self.prover.verify_groth16(proof, vkey)
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProofStatistics {
+    pub cycle_count: u64,
+    pub cost: u64,
+    pub total_time: u64,
+    pub latency: u64,
+}
+
+/// A proof of a RISCV ELF execution with given inputs and outputs.
+#[derive(Serialize, Deserialize)]
+#[serde(bound(serialize = "P: Serialize"))]
+#[serde(bound(deserialize = "P: DeserializeOwned"))]
+pub struct SP1ProofWithMetadata<P> {
+    pub proof: P,
+    pub stdin: SP1Stdin,
+    pub public_values: SP1PublicValues,
+}
+
+impl<P: Serialize + DeserializeOwned> SP1ProofWithMetadata<P> {
+    pub fn save(&self, path: impl AsRef<Path>) -> Result<()> {
+        bincode::serialize_into(File::create(path).expect("failed to open file"), self)
+            .map_err(Into::into)
+    }
+
+    pub fn load(path: impl AsRef<Path>) -> Result<Self> {
+        bincode::deserialize_from(File::open(path).expect("failed to open file"))
+            .map_err(Into::into)
+    }
+}
+
+impl<P: std::fmt::Debug> std::fmt::Debug for SP1ProofWithMetadata<P> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SP1ProofWithMetadata")
+            .field("proof", &self.proof)
+            .finish()
+    }
+}
+
+pub type SP1DefaultProof = SP1ProofWithMetadata<Vec<ShardProof<CoreSC>>>;
+
+pub type SP1CompressedProof = SP1ProofWithMetadata<ShardProof<InnerSC>>;
+
+pub type SP1PlonkProof = SP1ProofWithMetadata<PlonkBn254Proof>;
+
+pub type SP1Groth16Proof = SP1ProofWithMetadata<Groth16Proof>;
+
+pub trait Prover: Send + Sync {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey);
+
+    /// Prove the execution of a RISCV ELF with the given inputs.
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof>;
+
+    /// Generate a compressed proof of the execution of a RISCV ELF with the given inputs.
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof>;
+
+    /// Given an SP1 program and input, generate a PLONK proof that can be verified on-chain.
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof>;
+
+    /// Given an SP1 program and input, generate a Groth16 proof that can be verified on-chain.
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof>;
+
+    /// Verify that an SP1 proof is valid given its vkey and metadata.
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a compressed SP1 proof is valid given its vkey and metadata.
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a SP1 PLONK proof is valid given its vkey and metadata.
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()>;
+
+    /// Verify that a SP1 Groth16 proof is valid given its vkey and metadata.
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()>;
 }

sdk/src/local.rs
@@ -1,0 +1,125 @@
+#![allow(unused_variables)]
+use std::path::PathBuf;
+
+use crate::{
+    Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof, SP1PlonkProof,
+    SP1ProofWithMetadata, SP1ProvingKey, SP1VerifyingKey,
+};
+use anyhow::Result;
+use sha2::{Digest, Sha256};
+use wp1_core::{
+    air::PublicValues,
+    stark::{MachineProof, StarkGenericConfig},
+};
+use wp1_prover::{SP1Prover, SP1Stdin};
+
+pub(crate) struct LocalProver {
+    pub(crate) prover: SP1Prover,
+}
+
+impl Default for LocalProver {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl LocalProver {
+    pub(crate) fn new() -> Self {
+        let prover = SP1Prover::new();
+        Self { prover }
+    }
+
+    /// Get artifacts dir from SP1_CIRCUIT_DIR env var.
+    fn get_artifacts_dir(&self) -> PathBuf {
+        let artifacts_dir =
+            std::env::var("SP1_CIRCUIT_DIR").expect("SP1_CIRCUIT_DIR env var not set");
+        PathBuf::from(artifacts_dir)
+    }
+}
+
+impl Prover for LocalProver {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey) {
+        self.prover.setup(elf)
+    }
+
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof> {
+        let proof = self.prover.prove_core(pk, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: proof.shard_proofs,
+            stdin: proof.stdin,
+            public_values: proof.public_values,
+        })
+    }
+
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof> {
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        Ok(SP1CompressedProof {
+            proof: reduce_proof.proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof> {
+        let artifacts_dir = self.get_artifacts_dir();
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        let compress_proof = self.prover.compress(&pk.vk, reduce_proof);
+        let outer_proof = self.prover.wrap_bn254(&pk.vk, compress_proof);
+        let proof = self.prover.wrap_groth16(outer_proof, artifacts_dir);
+        Ok(SP1ProofWithMetadata {
+            proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof> {
+        let artifacts_dir = self.get_artifacts_dir();
+        let proof = self.prover.prove_core(pk, &stdin);
+        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
+        let public_values = proof.public_values.clone();
+        let reduce_proof = self.prover.reduce(&pk.vk, proof, deferred_proofs);
+        let compress_proof = self.prover.compress(&pk.vk, reduce_proof);
+        let outer_proof = self.prover.wrap_bn254(&pk.vk, compress_proof);
+        let proof = self.prover.wrap_plonk(&outer_proof, artifacts_dir);
+        Ok(SP1ProofWithMetadata {
+            proof,
+            stdin,
+            public_values,
+        })
+    }
+
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        let pv = PublicValues::from_vec(&proof.proof[0].public_values.clone());
+        let pv_digest: [u8; 32] = Sha256::digest(&proof.public_values.buffer.data).into();
+        if pv_digest != *pv.commit_digest_bytes() {
+            return Err(anyhow::anyhow!("Public values digest mismatch"));
+        }
+        let machine_proof = MachineProof {
+            shard_proofs: proof.proof.clone(),
+        };
+        let mut challenger = self.prover.core_machine.config().challenger();
+        Ok(self
+            .prover
+            .core_machine
+            .verify(&vkey.vk, &machine_proof, &mut challenger)?)
+    }
+
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        todo!()
+    }
+}

sdk/src/mock.rs
@@ -1,0 +1,77 @@
+#![allow(unused_variables)]
+use crate::{
+    Prover, SP1CompressedProof, SP1DefaultProof, SP1Groth16Proof, SP1PlonkProof,
+    SP1ProofWithMetadata, SP1ProvingKey, SP1VerifyingKey,
+};
+use anyhow::Result;
+use wp1_prover::{Groth16Proof, PlonkBn254Proof, SP1Prover, SP1Stdin};
+
+pub(crate) struct MockProver {
+    pub(crate) prover: SP1Prover,
+}
+
+impl Default for MockProver {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl MockProver {
+    pub(crate) fn new() -> Self {
+        let prover = SP1Prover::new();
+        Self { prover }
+    }
+}
+
+impl Prover for MockProver {
+    fn setup(&self, elf: &[u8]) -> (SP1ProvingKey, SP1VerifyingKey) {
+        self.prover.setup(elf)
+    }
+
+    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1DefaultProof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: vec![],
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof> {
+        unimplemented!()
+    }
+
+    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkProof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: PlonkBn254Proof::default(),
+            stdin,
+            public_values,
+        })
+    }
+
+    fn prove_groth16(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Groth16Proof> {
+        let public_values = SP1Prover::execute(&pk.elf, &stdin);
+        Ok(SP1ProofWithMetadata {
+            proof: Groth16Proof::default(),
+            stdin,
+            public_values,
+        })
+    }
+
+    fn verify(&self, proof: &SP1DefaultProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+
+    fn verify_compressed(&self, proof: &SP1CompressedProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn verify_groth16(&self, proof: &SP1Groth16Proof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+
+    fn verify_plonk(&self, proof: &SP1PlonkProof, vkey: &SP1VerifyingKey) -> Result<()> {
+        Ok(())
+    }
+}