WindOctober/sp1

fix(recursion): fixes for fri fold and poseidon2 (#654)

recursion/core/src/cpu/air/mod.rs
@@ -9,6 +9,7 @@ use std::borrow::Borrow;
 use p3_air::{Air, AirBuilder};
 use p3_field::{AbstractField, Field};
 use p3_matrix::Matrix;
+use sp1_core::air::BaseAirBuilder;
 
 use crate::{
     air::SP1RecursionAirBuilder,
@@ -28,12 +29,19 @@ where
         let zero = AB::Expr::zero();
         let one = AB::Expr::one();
 
+        // Constrain the program.
+        builder.send_program(local.pc, local.instruction, local.selectors, local.is_real);
+
+        // Constrain the operands.
         self.eval_operands(builder, local);
 
+        // Constrain memory instructions.
         self.eval_memory(builder, local);
 
+        // Constrain ALU instructions.
         self.eval_alu(builder, local);
 
+        // Constrain branches and jumps and constrain the next pc.
         {
             // Expression for the expected next_pc.  This will be added to in `eval_branch` and `eval_jump`
             // to account for possible jumps and branches.
@@ -49,23 +57,13 @@ where
                 - self.is_jump_instruction::<AB>(local);
             next_pc += not_branch_or_jump.clone() * (local.pc + one);
 
-            // Verify next row's pc is correct.
             builder
                 .when_transition()
                 .when(next.is_real)
                 .assert_eq(next_pc, next.pc);
         }
 
-        // Increment clk by 4 every cycle.
-        // builder
-        //     .when_transition()
-        //     .when(next.is_real)
-        //     .assert_eq(local.clk.into() + AB::F::from_canonical_u32(4), next.clk);
-
-        // Constraint the program.
-        // builder.send_program(local.pc, local.instruction, local.selectors, local.is_real);
-
-        // Constraint the syscalls.
+        // Constrain the syscalls.
         let send_syscall = local.selectors.is_poseidon + local.selectors.is_fri_fold;
         let operands = [
             local.clk.into(),
@@ -74,10 +72,35 @@ where
             local.c.value()[0] + local.instruction.offset_imm,
         ];
         builder.send_table(local.instruction.opcode, &operands, send_syscall);
+
+        // Constrain the clk.
+        self.eval_clk(builder, local, next);
     }
 }
 
 impl<F: Field> CpuChip<F> {
+    /// Eval the clk.
+    ///
+    /// For all instructions except for FRI fold, the next clk is the current clk + 4.
+    /// For FRI fold, the next clk is the current clk + number of FRI_FOLD iterations.  That value
+    /// is stored in the `a` operand.
+    pub fn eval_clk<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>, next: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder,
+    {
+        builder
+            .when_transition()
+            .when(next.is_real)
+            .when_not(local.selectors.is_fri_fold)
+            .assert_eq(local.clk.into() + AB::F::from_canonical_u32(4), next.clk);
+
+        builder
+            .when_transition()
+            .when(next.is_real)
+            .when(local.selectors.is_fri_fold)
+            .assert_eq(local.clk.into() + local.a.value()[0], next.clk);
+    }
+
     /// Expr to check for alu instructions.
     pub fn is_alu_instruction<AB>(&self, local: &CpuCols<AB::Var>) -> AB::Expr
     where

recursion/core/src/multi/mod.rs
@@ -1,11 +1,11 @@
 use std::borrow::{Borrow, BorrowMut};
 
 use itertools::Itertools;
-use p3_air::{Air, BaseAir};
-use p3_field::PrimeField32;
+use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::{AbstractField, PrimeField32};
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
-use sp1_core::air::MachineAir;
+use sp1_core::air::{BaseAirBuilder, MachineAir};
 use sp1_core::utils::pad_rows_fixed;
 use sp1_derive::AlignedBorrow;
 
@@ -106,16 +106,49 @@ where
 {
     fn eval(&self, builder: &mut AB) {
         let main = builder.main();
-        let cols = main.row_slice(0);
-        let cols: &MultiCols<AB::Var> = (*cols).borrow();
+        let (local, next) = (main.row_slice(0), main.row_slice(1));
+        let local: &MultiCols<AB::Var> = (*local).borrow();
+        let next: &MultiCols<AB::Var> = (*next).borrow();
+
+        let next_is_real = next.is_fri_fold + next.is_poseidon2;
+        let local_is_real = local.is_fri_fold + local.is_poseidon2;
+
+        // Assert that is_fri_fold and is_poseidon2 are bool and that at most one is set.
+        builder.assert_bool(local.is_fri_fold);
+        builder.assert_bool(local.is_poseidon2);
+        builder.assert_bool(local_is_real.clone());
+
+        // Fri fold requires that it's rows are contiguous, since each invocation spans multiple rows
+        // and it's AIR checks for consistencies among them.  The following constraints enforce that
+        // all the fri fold rows are first, then the posiedon2 rows, and finally any padded (non-real) rows.
+
+        // First verify that all real rows are contiguous.
+        builder.when_first_row().assert_one(local_is_real.clone());
+        builder
+            .when_transition()
+            .when_not(local_is_real.clone())
+            .assert_zero(next_is_real.clone());
+
+        // Next, verify that all fri fold rows are before the poseidon2 rows within the real rows section.
+        builder.when_first_row().assert_one(local.is_fri_fold);
+        builder
+            .when_transition()
+            .when(next_is_real)
+            .when(local.is_poseidon2)
+            .assert_one(next.is_poseidon2);
 
         let fri_fold_chip = FriFoldChip::default();
-        let mut sub_builder = builder.when(cols.is_fri_fold);
-        fri_fold_chip.eval_fri_fold(&mut sub_builder, cols.fri_fold());
+        let mut sub_builder = builder.when(local.is_fri_fold);
+        fri_fold_chip.eval_fri_fold(
+            &mut sub_builder,
+            local.fri_fold(),
+            next.fri_fold(),
+            AB::Expr::one() - next.is_fri_fold,
+        );
 
         let poseidon2_chip = Poseidon2Chip::default();
-        let mut sub_builder = builder.when(cols.is_poseidon2);
-        poseidon2_chip.eval_poseidon2(&mut sub_builder, cols.poseidon2());
+        let mut sub_builder = builder.when(local.is_poseidon2);
+        poseidon2_chip.eval_poseidon2(&mut sub_builder, local.poseidon2());
     }
 }
 // SAFETY: Each view is a valid interpretation of the underlying array.

argumentcomputer/sphinx

recursion/core/src/cpu/air/mod.rs
@@ -9,6 +9,7 @@ use std::borrow::Borrow;
 use p3_air::{Air, AirBuilder};
 use p3_field::{AbstractField, Field};
 use p3_matrix::Matrix;
+use wp1_core::air::BaseAirBuilder;
 
 use crate::{
     air::SP1RecursionAirBuilder,
@@ -28,12 +29,19 @@ where
         let zero = AB::Expr::zero();
         let one = AB::Expr::one();
 
+        // Constrain the program.
+        builder.send_program(local.pc, local.instruction, local.selectors, local.is_real);
+
+        // Constrain the operands.
         self.eval_operands(builder, local);
 
+        // Constrain memory instructions.
         self.eval_memory(builder, local);
 
+        // Constrain ALU instructions.
         self.eval_alu(builder, local);
 
+        // Constrain branches and jumps and constrain the next pc.
         {
             // Expression for the expected next_pc.  This will be added to in `eval_branch` and `eval_jump`
             // to account for possible jumps and branches.
@@ -49,23 +57,13 @@ where
                 - self.is_jump_instruction::<AB>(local);
             next_pc += not_branch_or_jump.clone() * (local.pc + one);
 
-            // Verify next row's pc is correct.
             builder
                 .when_transition()
                 .when(next.is_real)
                 .assert_eq(next_pc, next.pc);
         }
 
-        // Increment clk by 4 every cycle.
-        // builder
-        //     .when_transition()
-        //     .when(next.is_real)
-        //     .assert_eq(local.clk.into() + AB::F::from_canonical_u32(4), next.clk);
-
-        // Constraint the program.
-        // builder.send_program(local.pc, local.instruction, local.selectors, local.is_real);
-
-        // Constraint the syscalls.
+        // Constrain the syscalls.
         let send_syscall = local.selectors.is_poseidon + local.selectors.is_fri_fold;
         let operands = [
             local.clk.into(),
@@ -74,10 +72,35 @@ where
             local.c.value()[0] + local.instruction.offset_imm,
         ];
         builder.send_table(local.instruction.opcode, &operands, send_syscall);
+
+        // Constrain the clk.
+        self.eval_clk(builder, local, next);
     }
 }
 
 impl<F: Field> CpuChip<F> {
+    /// Eval the clk.
+    ///
+    /// For all instructions except for FRI fold, the next clk is the current clk + 4.
+    /// For FRI fold, the next clk is the current clk + number of FRI_FOLD iterations.  That value
+    /// is stored in the `a` operand.
+    pub fn eval_clk<AB>(&self, builder: &mut AB, local: &CpuCols<AB::Var>, next: &CpuCols<AB::Var>)
+    where
+        AB: SP1RecursionAirBuilder,
+    {
+        builder
+            .when_transition()
+            .when(next.is_real)
+            .when_not(local.selectors.is_fri_fold)
+            .assert_eq(local.clk.into() + AB::F::from_canonical_u32(4), next.clk);
+
+        builder
+            .when_transition()
+            .when(next.is_real)
+            .when(local.selectors.is_fri_fold)
+            .assert_eq(local.clk.into() + local.a.value()[0], next.clk);
+    }
+
     /// Expr to check for alu instructions.
     pub fn is_alu_instruction<AB>(&self, local: &CpuCols<AB::Var>) -> AB::Expr
     where

recursion/core/src/multi/mod.rs
@@ -1,11 +1,11 @@
 use std::borrow::{Borrow, BorrowMut};
 
 use itertools::Itertools;
-use p3_air::{Air, BaseAir};
-use p3_field::PrimeField32;
+use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::{AbstractField, PrimeField32};
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
-use wp1_core::air::MachineAir;
+use wp1_core::air::{BaseAirBuilder, MachineAir};
 use wp1_core::utils::pad_rows_fixed;
 use wp1_derive::AlignedBorrow;
 
@@ -106,16 +106,49 @@ where
 {
     fn eval(&self, builder: &mut AB) {
         let main = builder.main();
-        let cols = main.row_slice(0);
-        let cols: &MultiCols<AB::Var> = (*cols).borrow();
+        let (local, next) = (main.row_slice(0), main.row_slice(1));
+        let local: &MultiCols<AB::Var> = (*local).borrow();
+        let next: &MultiCols<AB::Var> = (*next).borrow();
+
+        let next_is_real = next.is_fri_fold + next.is_poseidon2;
+        let local_is_real = local.is_fri_fold + local.is_poseidon2;
+
+        // Assert that is_fri_fold and is_poseidon2 are bool and that at most one is set.
+        builder.assert_bool(local.is_fri_fold);
+        builder.assert_bool(local.is_poseidon2);
+        builder.assert_bool(local_is_real.clone());
+
+        // Fri fold requires that it's rows are contiguous, since each invocation spans multiple rows
+        // and it's AIR checks for consistencies among them.  The following constraints enforce that
+        // all the fri fold rows are first, then the posiedon2 rows, and finally any padded (non-real) rows.
+
+        // First verify that all real rows are contiguous.
+        builder.when_first_row().assert_one(local_is_real.clone());
+        builder
+            .when_transition()
+            .when_not(local_is_real.clone())
+            .assert_zero(next_is_real.clone());
+
+        // Next, verify that all fri fold rows are before the poseidon2 rows within the real rows section.
+        builder.when_first_row().assert_one(local.is_fri_fold);
+        builder
+            .when_transition()
+            .when(next_is_real)
+            .when(local.is_poseidon2)
+            .assert_one(next.is_poseidon2);
 
         let fri_fold_chip = FriFoldChip::default();
-        let mut sub_builder = builder.when(cols.is_fri_fold);
-        fri_fold_chip.eval_fri_fold(&mut sub_builder, cols.fri_fold());
+        let mut sub_builder = builder.when(local.is_fri_fold);
+        fri_fold_chip.eval_fri_fold(
+            &mut sub_builder,
+            local.fri_fold(),
+            next.fri_fold(),
+            AB::Expr::one() - next.is_fri_fold,
+        );
 
         let poseidon2_chip = Poseidon2Chip::default();
-        let mut sub_builder = builder.when(cols.is_poseidon2);
-        poseidon2_chip.eval_poseidon2(&mut sub_builder, cols.poseidon2());
+        let mut sub_builder = builder.when(local.is_poseidon2);
+        poseidon2_chip.eval_poseidon2(&mut sub_builder, local.poseidon2());
     }
 }
 // SAFETY: Each view is a valid interpretation of the underlying array.