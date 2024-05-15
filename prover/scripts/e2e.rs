succinctlabs/sp1

feat: add `groth16` verification to gnark server (#631)

prover/scripts/e2e.rs
@@ -7,7 +7,8 @@ use sp1_prover::SP1Prover;
 use sp1_recursion_circuit::stark::build_wrap_circuit;
 use sp1_recursion_circuit::witness::Witnessable;
 use sp1_recursion_compiler::ir::Witness;
-use sp1_recursion_gnark_ffi::Groth16Prover;
+use sp1_recursion_gnark_ffi::{convert, verify, Groth16Prover};
+use subtle_encoding::hex;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -67,6 +68,13 @@ pub fn main() {
     tracing::info!("gnark prove");
     let proof = groth16_prover.prove(witness.clone());
 
+    tracing::info!("verify gnark proof");
+    let verified = verify(proof.clone(), &args.build_dir.clone().into());
+    assert!(verified);
+
+    tracing::info!("convert gnark proof");
+    let solidity_proof = convert(proof.clone(), &args.build_dir.clone().into());
+
     // tracing::info!("sanity check plonk bn254 build");
     // PlonkBn254Prover::build(
     //     constraints.clone(),
@@ -77,5 +85,9 @@ pub fn main() {
     // tracing::info!("sanity check plonk bn254 prove");
     // let proof = PlonkBn254Prover::prove(witness.clone(), args.build_dir.clone().into());
 
-    println!("{:?}", proof);
+    println!(
+        "{:?}",
+        String::from_utf8(hex::encode(proof.encoded_proof)).unwrap()
+    );
+    println!("solidity proof: {:?}", solidity_proof);
 }

argumentcomputer/sphinx

prover/scripts/e2e.rs
@@ -1,10 +1,11 @@
 use clap::Parser;
+use subtle_encoding::hex;
 use wp1_core::io::SP1Stdin;
 use wp1_prover::SP1Prover;
 use wp1_recursion_circuit::stark::build_wrap_circuit;
 use wp1_recursion_circuit::witness::Witnessable;
 use wp1_recursion_compiler::ir::Witness;
-use wp1_recursion_gnark_ffi::Groth16Prover;
+use wp1_recursion_gnark_ffi::{convert, verify, Groth16Prover};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -60,6 +61,13 @@ pub fn main() {
     tracing::info!("gnark prove");
     let proof = groth16_prover.prove(witness.clone());
 
+    tracing::info!("verify gnark proof");
+    let verified = verify(&proof, &args.build_dir.clone().into());
+    assert!(verified);
+
+    tracing::info!("convert gnark proof");
+    let solidity_proof = convert(&proof, &args.build_dir.clone().into());
+
     // tracing::info!("sanity check plonk bn254 build");
     // PlonkBn254Prover::build(
     //     constraints.clone(),
@@ -70,5 +78,9 @@ pub fn main() {
     // tracing::info!("sanity check plonk bn254 prove");
     // let proof = PlonkBn254Prover::prove(witness.clone(), args.build_dir.clone().into());
 
-    println!("{:?}", proof);
+    println!(
+        "{:?}",
+        String::from_utf8(hex::encode(proof.encoded_proof)).unwrap()
+    );
+    println!("solidity proof: {:?}", solidity_proof);
 }