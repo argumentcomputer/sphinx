WindOctober/sp1

feat: improve circuit by 3-4x (#648)

recursion/circuit/src/lib.rs
@@ -20,12 +20,110 @@ pub const RATE: usize = 16;
 
 #[cfg(test)]
 mod tests {
+    use p3_baby_bear::BabyBear;
     use p3_bn254_fr::Bn254Fr;
     use p3_field::AbstractField;
     use sp1_recursion_compiler::config::OuterConfig;
     use sp1_recursion_compiler::constraints::ConstraintCompiler;
-    use sp1_recursion_compiler::ir::{Builder, Witness};
+    use sp1_recursion_compiler::ir::Config;
+    use sp1_recursion_compiler::ir::Ext;
+    use sp1_recursion_compiler::ir::ExtConst;
+    use sp1_recursion_compiler::ir::{Builder, Felt, Witness};
     use sp1_recursion_gnark_ffi::Groth16Prover;
+
+    #[test]
+    fn test_base_babybear() {
+        let mut builder = Builder::<OuterConfig>::default();
+        let a_val = BabyBear::from_wrapped_u32(3124235823);
+        let b_val = BabyBear::from_wrapped_u32(3252375321);
+        let a: Felt<_> = builder.eval(a_val);
+        let b: Felt<_> = builder.eval(b_val);
+
+        // Testing base addition.
+        let a_plus_b: Felt<_> = builder.eval(a + b);
+        builder.assert_felt_eq(a_plus_b, a_val + b_val);
+
+        // Testing base subtraction.
+        let a_minus_b: Felt<_> = builder.eval(a - b);
+        builder.assert_felt_eq(a_minus_b, a_val - b_val);
+
+        // Testing base multiplication.
+        let a_times_b: Felt<_> = builder.eval(a * b);
+        builder.assert_felt_eq(a_times_b, a_val * b_val);
+
+        // Testing large linear combination.
+        let dot_product: Felt<_> = builder.eval(a * a + b * b + a * b);
+        builder.assert_felt_eq(dot_product, a_val * a_val + b_val * b_val + a_val * b_val);
+
+        // Testing high degree multiplication.
+        let a_times_b_times_c: Felt<_> =
+            builder.eval(a_val * b_val * a_val * b_val * a_val * b_val);
+        builder.assert_felt_eq(
+            a_times_b_times_c,
+            a_val * b_val * a_val * b_val * a_val * b_val,
+        );
+
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
+        let constraints = backend.emit(builder.operations);
+
+        let witness = Witness::default();
+        Groth16Prover::test::<OuterConfig>(constraints.clone(), witness);
+    }
+
+    #[test]
+    fn test_extension_babybear() {
+        let mut builder = Builder::<OuterConfig>::default();
+        let one_val = <OuterConfig as Config>::EF::from_wrapped_u32(1);
+        let a_val = <OuterConfig as Config>::EF::from_wrapped_u32(3124235823);
+        let b_val = <OuterConfig as Config>::EF::from_wrapped_u32(3252375321);
+        let one: Ext<_, _> = builder.eval(BabyBear::one());
+        let a: Ext<_, _> = builder.eval(a_val.cons());
+        let b: Ext<_, _> = builder.eval(b_val.cons());
+
+        // Testing extension addition.
+        let a_plus_b: Ext<_, _> = builder.eval(a + b);
+        builder.assert_ext_eq(a_plus_b, (a_val + b_val).cons());
+
+        // // Testing negation.
+        // let neg_a: Ext<_, _> = builder.eval(-a);
+        // builder.assert_ext_eq(neg_a, (-a_val).cons());
+
+        // Testing extension subtraction.
+        let a_minus_b: Ext<_, _> = builder.eval(a - b);
+        builder.assert_ext_eq(a_minus_b, (a_val - b_val).cons());
+
+        // Testing base multiplication.
+        let a_times_b: Ext<_, _> = builder.eval(a * b);
+        builder.assert_ext_eq(a_times_b, (a_val * b_val).cons());
+
+        // Testing base division.
+        let a_div_b: Ext<_, _> = builder.eval(a / b);
+        builder.assert_ext_eq(a_div_b, (a_val / b_val).cons());
+
+        // Testing base inversion.
+        let a_inv: Ext<_, _> = builder.eval(one / a);
+        builder.assert_ext_eq(a_inv, (one_val / a_val).cons());
+
+        // Testing large linear combination.
+        let dot_product: Ext<_, _> = builder.eval(a * a + b * b + a * b);
+        builder.assert_ext_eq(
+            dot_product,
+            (a_val * a_val + b_val * b_val + a_val * b_val).cons(),
+        );
+
+        // Testing high degree multiplication.
+        let a_times_b_times_c: Ext<_, _> = builder.eval(a * b * a * b * a * b);
+        builder.assert_ext_eq(
+            a_times_b_times_c,
+            (a_val * b_val * a_val * b_val * a_val * b_val).cons(),
+        );
+
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
+        let constraints = backend.emit(builder.operations);
+
+        let witness = Witness::default();
+        Groth16Prover::test::<OuterConfig>(constraints.clone(), witness);
+    }
 
     #[test]
     fn test_commit() {

recursion/core/src/poseidon2/external.rs
@@ -6,7 +6,7 @@ use p3_field::AbstractField;
 use p3_field::PrimeField32;
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
-use sp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
+use sp1_core::air::{BaseAirBuilder, ExtensionAirBuilder, MachineAir, SP1AirBuilder};
 use sp1_core::utils::pad_rows_fixed;
 use sp1_derive::AlignedBorrow;
 use sp1_primitives::RC_16_30_U32;
@@ -25,7 +25,7 @@ pub const WIDTH: usize = 16;
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
 pub struct Poseidon2Chip {
-    fixed_trace_log2: Option<usize>,
+    pub fixed_log2_rows: Option<usize>,
 }
 
 /// The column layout for the chip.
@@ -33,7 +33,7 @@ pub struct Poseidon2Chip {
 #[repr(C)]
 pub struct Poseidon2Cols<T> {
     pub input: [T; WIDTH],
-    pub rounds: [T; 31],
+    pub rounds: [T; 22],
     pub add_rc: [T; WIDTH],
     pub sbox_deg_3: [T; WIDTH],
     pub sbox_deg_7: [T; WIDTH],
@@ -65,7 +65,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         let mut rows = Vec::new();
 
         let rounds_f = 8;
-        let rounds_p = 22;
+        let rounds_p = 13;
         let rounds = rounds_f + rounds_p + 1;
         let rounds_p_beginning = 1 + rounds_f / 2;
         let p_end = rounds_p_beginning + rounds_p;
@@ -145,7 +145,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         pad_rows_fixed(
             &mut rows,
             || [F::zero(); NUM_POSEIDON2_COLS],
-            self.fixed_trace_log2,
+            self.fixed_log2_rows,
         );
 
         // Convert the trace to a row major matrix.
@@ -166,17 +166,14 @@ impl<F> BaseAir<F> for Poseidon2Chip {
     }
 }
 
-impl<AB> Air<AB> for Poseidon2Chip
-where
-    AB: SP1AirBuilder,
-{
-    fn eval(&self, builder: &mut AB) {
-        let main = builder.main();
-        let local = main.row_slice(0);
-        let local: &Poseidon2Cols<AB::Var> = (*local).borrow();
-
+impl Poseidon2Chip {
+    pub fn eval_poseidon2<AB: BaseAirBuilder + ExtensionAirBuilder>(
+        &self,
+        builder: &mut AB,
+        local: &Poseidon2Cols<AB::Var>,
+    ) {
         let rounds_f = 8;
-        let rounds_p = 22;
+        let rounds_p = 13;
         let rounds = rounds_f + rounds_p;
 
         // Convert the u32 round constants to field elements.
@@ -295,8 +292,10 @@ where
         builder.assert_eq(local.is_initial, local.rounds[0]);
 
         // Constrain the external flag.
-        let is_external_first_half = (0..4).map(|i| local.rounds[i + 1].into()).sum::<AB::Expr>();
-        let is_external_second_half = (26..30)
+        let is_external_first_half = (0..rounds_f / 2)
+            .map(|i| local.rounds[i + 1].into())
+            .sum::<AB::Expr>();
+        let is_external_second_half = ((rounds_f / 2 + rounds_p)..(rounds_f + rounds_p))
             .map(|i| local.rounds[i + 1].into())
             .sum::<AB::Expr>();
         builder.assert_eq(
@@ -305,10 +304,22 @@ where
         );
 
         // Constrain the internal flag.
-        let is_internal = (4..26)
+        let is_internal = (4..17)
             .map(|i| local.rounds[i + 1].into())
             .sum::<AB::Expr>();
         builder.assert_eq(local.is_internal, is_internal);
+    }
+}
+
+impl<AB> Air<AB> for Poseidon2Chip
+where
+    AB: SP1AirBuilder,
+{
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let local = main.row_slice(0);
+        let local: &Poseidon2Cols<AB::Var> = (*local).borrow();
+        self.eval_poseidon2::<AB>(builder, local);
     }
 }
 
@@ -344,7 +355,7 @@ mod tests {
     #[test]
     fn generate_trace() {
         let chip = Poseidon2Chip {
-            fixed_trace_log2: None,
+            fixed_log2_rows: None,
         };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
@@ -389,7 +400,7 @@ mod tests {
         let mut challenger = config.challenger();
 
         let chip = Poseidon2Chip {
-            fixed_trace_log2: None,
+            fixed_log2_rows: None,
         };
 
         let test_inputs = (0..16)

argumentcomputer/sphinx

recursion/circuit/src/lib.rs
@@ -20,12 +20,110 @@ pub const RATE: usize = 16;
 
 #[cfg(test)]
 mod tests {
+    use p3_baby_bear::BabyBear;
     use p3_bn254_fr::Bn254Fr;
     use p3_field::AbstractField;
     use wp1_recursion_compiler::config::OuterConfig;
     use wp1_recursion_compiler::constraints::ConstraintCompiler;
-    use wp1_recursion_compiler::ir::{Builder, Witness};
+    use wp1_recursion_compiler::ir::Config;
+    use wp1_recursion_compiler::ir::Ext;
+    use wp1_recursion_compiler::ir::ExtConst;
+    use wp1_recursion_compiler::ir::{Builder, Felt, Witness};
     use wp1_recursion_gnark_ffi::Groth16Prover;
+
+    #[test]
+    fn test_base_babybear() {
+        let mut builder = Builder::<OuterConfig>::default();
+        let a_val = BabyBear::from_wrapped_u32(3124235823);
+        let b_val = BabyBear::from_wrapped_u32(3252375321);
+        let a: Felt<_> = builder.eval(a_val);
+        let b: Felt<_> = builder.eval(b_val);
+
+        // Testing base addition.
+        let a_plus_b: Felt<_> = builder.eval(a + b);
+        builder.assert_felt_eq(a_plus_b, a_val + b_val);
+
+        // Testing base subtraction.
+        let a_minus_b: Felt<_> = builder.eval(a - b);
+        builder.assert_felt_eq(a_minus_b, a_val - b_val);
+
+        // Testing base multiplication.
+        let a_times_b: Felt<_> = builder.eval(a * b);
+        builder.assert_felt_eq(a_times_b, a_val * b_val);
+
+        // Testing large linear combination.
+        let dot_product: Felt<_> = builder.eval(a * a + b * b + a * b);
+        builder.assert_felt_eq(dot_product, a_val * a_val + b_val * b_val + a_val * b_val);
+
+        // Testing high degree multiplication.
+        let a_times_b_times_c: Felt<_> =
+            builder.eval(a_val * b_val * a_val * b_val * a_val * b_val);
+        builder.assert_felt_eq(
+            a_times_b_times_c,
+            a_val * b_val * a_val * b_val * a_val * b_val,
+        );
+
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
+        let constraints = backend.emit(builder.operations);
+
+        let witness = Witness::default();
+        Groth16Prover::test::<OuterConfig>(&constraints, witness);
+    }
+
+    #[test]
+    fn test_extension_babybear() {
+        let mut builder = Builder::<OuterConfig>::default();
+        let one_val = <OuterConfig as Config>::EF::from_wrapped_u32(1);
+        let a_val = <OuterConfig as Config>::EF::from_wrapped_u32(3124235823);
+        let b_val = <OuterConfig as Config>::EF::from_wrapped_u32(3252375321);
+        let one: Ext<_, _> = builder.eval(BabyBear::one());
+        let a: Ext<_, _> = builder.eval(a_val.cons());
+        let b: Ext<_, _> = builder.eval(b_val.cons());
+
+        // Testing extension addition.
+        let a_plus_b: Ext<_, _> = builder.eval(a + b);
+        builder.assert_ext_eq(a_plus_b, (a_val + b_val).cons());
+
+        // // Testing negation.
+        // let neg_a: Ext<_, _> = builder.eval(-a);
+        // builder.assert_ext_eq(neg_a, (-a_val).cons());
+
+        // Testing extension subtraction.
+        let a_minus_b: Ext<_, _> = builder.eval(a - b);
+        builder.assert_ext_eq(a_minus_b, (a_val - b_val).cons());
+
+        // Testing base multiplication.
+        let a_times_b: Ext<_, _> = builder.eval(a * b);
+        builder.assert_ext_eq(a_times_b, (a_val * b_val).cons());
+
+        // Testing base division.
+        let a_div_b: Ext<_, _> = builder.eval(a / b);
+        builder.assert_ext_eq(a_div_b, (a_val / b_val).cons());
+
+        // Testing base inversion.
+        let a_inv: Ext<_, _> = builder.eval(one / a);
+        builder.assert_ext_eq(a_inv, (one_val / a_val).cons());
+
+        // Testing large linear combination.
+        let dot_product: Ext<_, _> = builder.eval(a * a + b * b + a * b);
+        builder.assert_ext_eq(
+            dot_product,
+            (a_val * a_val + b_val * b_val + a_val * b_val).cons(),
+        );
+
+        // Testing high degree multiplication.
+        let a_times_b_times_c: Ext<_, _> = builder.eval(a * b * a * b * a * b);
+        builder.assert_ext_eq(
+            a_times_b_times_c,
+            (a_val * b_val * a_val * b_val * a_val * b_val).cons(),
+        );
+
+        let mut backend = ConstraintCompiler::<OuterConfig>::default();
+        let constraints = backend.emit(builder.operations);
+
+        let witness = Witness::default();
+        Groth16Prover::test::<OuterConfig>(&constraints, witness);
+    }
 
     #[test]
     fn test_commit() {

recursion/core/src/poseidon2/external.rs
@@ -8,7 +8,7 @@ use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use std::borrow::BorrowMut;
 use tracing::instrument;
-use wp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
+use wp1_core::air::{BaseAirBuilder, ExtensionAirBuilder, MachineAir, SP1AirBuilder};
 use wp1_core::utils::pad_rows_fixed;
 use wp1_derive::AlignedBorrow;
 use wp1_primitives::RC_16_30_U32;
@@ -25,7 +25,7 @@ pub(crate) const WIDTH: usize = 16;
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
 pub struct Poseidon2Chip {
-    fixed_trace_log2: Option<usize>,
+    pub fixed_log2_rows: Option<usize>,
 }
 
 /// The column layout for the chip.
@@ -33,7 +33,7 @@ pub struct Poseidon2Chip {
 #[repr(C)]
 pub struct Poseidon2Cols<T> {
     pub input: [T; WIDTH],
-    pub rounds: [T; 31],
+    pub rounds: [T; 22],
     pub add_rc: [T; WIDTH],
     pub sbox_deg_3: [T; WIDTH],
     pub sbox_deg_7: [T; WIDTH],
@@ -65,7 +65,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         let mut rows = Vec::new();
 
         let rounds_f = 8;
-        let rounds_p = 22;
+        let rounds_p = 13;
         let rounds = rounds_f + rounds_p + 1;
         let rounds_p_beginning = 1 + rounds_f / 2;
         let p_end = rounds_p_beginning + rounds_p;
@@ -145,7 +145,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         pad_rows_fixed(
             &mut rows,
             || [F::zero(); NUM_POSEIDON2_COLS],
-            self.fixed_trace_log2,
+            self.fixed_log2_rows,
         );
 
         // Convert the trace to a row major matrix.
@@ -166,17 +166,14 @@ impl<F> BaseAir<F> for Poseidon2Chip {
     }
 }
 
-impl<AB> Air<AB> for Poseidon2Chip
-where
-    AB: SP1AirBuilder,
-{
-    fn eval(&self, builder: &mut AB) {
-        let main = builder.main();
-        let local = main.row_slice(0);
-        let local: &Poseidon2Cols<AB::Var> = (*local).borrow();
-
+impl Poseidon2Chip {
+    pub fn eval_poseidon2<AB: BaseAirBuilder + ExtensionAirBuilder>(
+        &self,
+        builder: &mut AB,
+        local: &Poseidon2Cols<AB::Var>,
+    ) {
         let rounds_f = 8;
-        let rounds_p = 22;
+        let rounds_p = 13;
         let rounds = rounds_f + rounds_p;
 
         // Convert the u32 round constants to field elements.
@@ -291,8 +288,10 @@ where
         builder.assert_eq(local.is_initial, local.rounds[0]);
 
         // Constrain the external flag.
-        let is_external_first_half = (0..4).map(|i| local.rounds[i + 1].into()).sum::<AB::Expr>();
-        let is_external_second_half = (26..30)
+        let is_external_first_half = (0..rounds_f / 2)
+            .map(|i| local.rounds[i + 1].into())
+            .sum::<AB::Expr>();
+        let is_external_second_half = ((rounds_f / 2 + rounds_p)..(rounds_f + rounds_p))
             .map(|i| local.rounds[i + 1].into())
             .sum::<AB::Expr>();
         builder.assert_eq(
@@ -301,10 +300,22 @@ where
         );
 
         // Constrain the internal flag.
-        let is_internal = (4..26)
+        let is_internal = (4..17)
             .map(|i| local.rounds[i + 1].into())
             .sum::<AB::Expr>();
         builder.assert_eq(local.is_internal, is_internal);
+    }
+}
+
+impl<AB> Air<AB> for Poseidon2Chip
+where
+    AB: SP1AirBuilder,
+{
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let local = main.row_slice(0);
+        let local: &Poseidon2Cols<AB::Var> = (*local).borrow();
+        self.eval_poseidon2::<AB>(builder, local);
     }
 }
 
@@ -341,7 +352,7 @@ mod tests {
     #[ignore = "broken in upstream, will be fixed by https://github.com/succinctlabs/sp1/pull/672"]
     fn generate_trace() {
         let chip = Poseidon2Chip {
-            fixed_trace_log2: None,
+            fixed_log2_rows: None,
         };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
@@ -386,7 +397,7 @@ mod tests {
         let mut challenger = config.challenger();
 
         let chip = Poseidon2Chip {
-            fixed_trace_log2: None,
+            fixed_log2_rows: None,
         };
 
         let test_inputs = (0..16)