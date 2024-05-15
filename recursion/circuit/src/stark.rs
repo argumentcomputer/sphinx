WindOctober/sp1

feat: regularize proof shape (#641)

recursion/circuit/src/stark.rs
@@ -14,7 +14,7 @@ use sp1_core::{
 };
 use sp1_recursion_compiler::config::OuterConfig;
 use sp1_recursion_compiler::constraints::{Constraint, ConstraintCompiler};
-use sp1_recursion_compiler::ir::{Builder, Config, Felt};
+use sp1_recursion_compiler::ir::{Builder, Config};
 use sp1_recursion_compiler::ir::{Usize, Witness};
 use sp1_recursion_compiler::prelude::SymbolicVar;
 use sp1_recursion_core::stark::config::{outer_fri_config, BabyBearPoseidon2Outer};
@@ -235,8 +235,8 @@ type OuterF = <BabyBearPoseidon2Outer as StarkGenericConfig>::Val;
 type OuterC = OuterConfig;
 
 pub fn build_wrap_circuit(
-    vk: &StarkVerifyingKey<OuterSC>,
-    dummy_proof: ShardProof<OuterSC>,
+    template_vk: &StarkVerifyingKey<OuterSC>,
+    template_proof: ShardProof<OuterSC>,
 ) -> Vec<Constraint> {
     let dev_mode = std::env::var("SP1_DEV_WRAPPER")
         .unwrap_or("true".to_string())
@@ -244,21 +244,21 @@ pub fn build_wrap_circuit(
         .eq("true");
 
     let outer_config = OuterSC::new();
-    let outer_machine = RecursionAirSkinnyDeg7::<OuterF>::machine(outer_config);
+    let outer_machine = RecursionAirSkinnyDeg7::<OuterF>::wrap_machine(outer_config);
 
     let mut builder = Builder::<OuterConfig>::default();
     let mut challenger = MultiField32ChallengerVariable::new(&mut builder);
 
-    let preprocessed_commit_val: [Bn254Fr; 1] = vk.commit.into();
+    let preprocessed_commit_val: [Bn254Fr; 1] = template_vk.commit.into();
     let preprocessed_commit: OuterDigestVariable<OuterC> =
         [builder.eval(preprocessed_commit_val[0])];
     challenger.observe_commitment(&mut builder, preprocessed_commit);
-    let pc_start: Felt<_> = builder.eval(vk.pc_start);
+    let pc_start = builder.eval(template_vk.pc_start);
     challenger.observe(&mut builder, pc_start);
 
     if !dev_mode {
         let chips = outer_machine
-            .shard_chips_ordered(&dummy_proof.chip_ordering)
+            .shard_chips_ordered(&template_proof.chip_ordering)
             .map(|chip| chip.name())
             .collect::<Vec<_>>();
 
@@ -266,7 +266,7 @@ pub fn build_wrap_circuit(
             .chips()
             .iter()
             .map(|chip| {
-                dummy_proof
+                template_proof
                     .chip_ordering
                     .get(&chip.name())
                     .copied()
@@ -275,7 +275,7 @@ pub fn build_wrap_circuit(
             .collect::<Vec<_>>();
 
         let chip_quotient_data = outer_machine
-            .shard_chips_ordered(&dummy_proof.chip_ordering)
+            .shard_chips_ordered(&template_proof.chip_ordering)
             .map(|chip| {
                 let log_quotient_degree = chip.log_quotient_degree();
                 QuotientDataValues {
@@ -286,8 +286,8 @@ pub fn build_wrap_circuit(
             .collect();
 
         let mut witness = Witness::default();
-        dummy_proof.write(&mut witness);
-        let proof = dummy_proof.read(&mut builder);
+        template_proof.write(&mut witness);
+        let proof = template_proof.read(&mut builder);
 
         let ShardCommitment { main_commit, .. } = &proof.commitment;
         challenger.observe_commitment(&mut builder, *main_commit);
@@ -300,7 +300,7 @@ pub fn build_wrap_circuit(
 
         StarkVerifierCircuit::<OuterC, OuterSC>::verify_shard(
             &mut builder,
-            vk,
+            template_vk,
             &outer_machine,
             &mut challenger.clone(),
             &proof,

recursion/core/src/fri_fold/mod.rs
@@ -9,7 +9,7 @@ use p3_field::PrimeField32;
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use sp1_core::air::{BinomialExtension, MachineAir};
-use sp1_core::utils::pad_to_power_of_two;
+use sp1_core::utils::pad_rows_fixed;
 use sp1_derive::AlignedBorrow;
 use std::borrow::BorrowMut;
 use tracing::instrument;
@@ -21,7 +21,9 @@ use crate::runtime::{ExecutionRecord, RecursionProgram};
 pub const NUM_FRI_FOLD_COLS: usize = core::mem::size_of::<FriFoldCols<u8>>();
 
 #[derive(Default)]
-pub struct FriFoldChip;
+pub struct FriFoldChip {
+    pub fixed_log2_rows: Option<usize>,
+}
 
 #[derive(Debug, Clone)]
 pub struct FriFoldEvent<F> {
@@ -94,16 +96,16 @@ impl<F: PrimeField32> MachineAir<F> for FriFoldChip {
         // This is a no-op.
     }
 
-    #[instrument(name = "generate fri fold trace", level = "debug", skip_all)]
+    #[instrument(name = "generate fri fold trace", level = "debug", skip_all, fields(rows = input.fri_fold_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
         _: &mut ExecutionRecord<F>,
     ) -> RowMajorMatrix<F> {
-        let trace_values = input
+        let mut rows = input
             .fri_fold_events
             .iter()
-            .flat_map(|event| {
+            .map(|event| {
                 let mut row = [F::zero(); NUM_FRI_FOLD_COLS];
 
                 let cols: &mut FriFoldCols<F> = row.as_mut_slice().borrow_mut();
@@ -129,15 +131,19 @@ impl<F: PrimeField32> MachineAir<F> for FriFoldChip {
                     .populate(&event.alpha_pow_at_log_height);
                 cols.ro_at_log_height.populate(&event.ro_at_log_height);
 
-                row.into_iter()
+                row
             })
             .collect_vec();
 
-        // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(trace_values, NUM_FRI_FOLD_COLS);
-
         // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_FRI_FOLD_COLS, F>(&mut trace.values);
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_FRI_FOLD_COLS],
+            self.fixed_log2_rows,
+        );
+
+        // Convert the trace to a row major matrix.
+        let trace = RowMajorMatrix::new(rows.into_iter().flatten().collect(), NUM_FRI_FOLD_COLS);
 
         #[cfg(debug_assertions)]
         println!(

recursion/core/src/poseidon2/external.rs
@@ -7,7 +7,7 @@ use p3_field::PrimeField32;
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use sp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
-use sp1_core::utils::pad_to_power_of_two;
+use sp1_core::utils::pad_rows_fixed;
 use sp1_derive::AlignedBorrow;
 use sp1_primitives::RC_16_30_U32;
 use std::borrow::BorrowMut;
@@ -24,7 +24,9 @@ pub const WIDTH: usize = 16;
 
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
-pub struct Poseidon2Chip;
+pub struct Poseidon2Chip {
+    fixed_trace_log2: Option<usize>,
+}
 
 /// The column layout for the chip.
 #[derive(AlignedBorrow, Default, Clone, Copy)]
@@ -54,7 +56,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         // This is a no-op.
     }
 
-    #[instrument(name = "generate poseidon2 trace", level = "debug", skip_all)]
+    #[instrument(name = "generate poseidon2 trace", level = "debug", skip_all, fields(rows = input.poseidon2_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
@@ -139,16 +141,18 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
             }
         }
 
-        // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(
-            rows.into_iter().flatten().collect::<Vec<_>>(),
-            NUM_POSEIDON2_COLS,
+        // Pad the trace to a power of two.
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_POSEIDON2_COLS],
+            self.fixed_trace_log2,
         );
 
-        // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_POSEIDON2_COLS, F>(&mut trace.values);
-
-        trace
+        // Convert the trace to a row major matrix.
+        RowMajorMatrix::new(
+            rows.into_iter().flatten().collect::<Vec<_>>(),
+            NUM_POSEIDON2_COLS,
+        )
     }
 
     fn included(&self, record: &Self::Record) -> bool {
@@ -339,7 +343,9 @@ mod tests {
 
     #[test]
     fn generate_trace() {
-        let chip = Poseidon2Chip;
+        let chip = Poseidon2Chip {
+            fixed_trace_log2: None,
+        };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
             [BabyBear::from_canonical_u32(2); WIDTH],
@@ -382,7 +388,9 @@ mod tests {
         let config = BabyBearPoseidon2::new();
         let mut challenger = config.challenger();
 
-        let chip = Poseidon2Chip;
+        let chip = Poseidon2Chip {
+            fixed_trace_log2: None,
+        };
 
         let test_inputs = (0..16)
             .map(|i| [BabyBear::from_canonical_u32(i); WIDTH])

recursion/core/src/poseidon2_wide/external.rs
@@ -6,7 +6,7 @@ use p3_field::{AbstractField, PrimeField32};
 use p3_matrix::dense::RowMajorMatrix;
 use p3_matrix::Matrix;
 use sp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
-use sp1_core::utils::pad_to_power_of_two;
+use sp1_core::utils::pad_rows_fixed;
 use sp1_derive::AlignedBorrow;
 use sp1_primitives::RC_16_30_U32;
 use std::borrow::BorrowMut;
@@ -32,7 +32,9 @@ pub const NUM_ROUNDS: usize = NUM_EXTERNAL_ROUNDS + NUM_INTERNAL_ROUNDS;
 
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
-pub struct Poseidon2WideChip<const DEGREE: usize>;
+pub struct Poseidon2WideChip<const DEGREE: usize> {
+    pub fixed_log2_rows: Option<usize>,
+}
 
 #[derive(AlignedBorrow, Clone, Copy)]
 #[repr(C)]
@@ -90,7 +92,7 @@ impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for Poseidon2WideChip<D
         // This is a no-op.
     }
 
-    #[instrument(name = "generate poseidon2 wide trace", level = "debug", skip_all)]
+    #[instrument(name = "generate poseidon2 wide trace", level = "debug", skip_all, fields(rows = input.poseidon2_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
@@ -149,14 +151,18 @@ impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for Poseidon2WideChip<D
             rows.push(row);
         }
 
+        // Pad the trace to a power of two.
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_POSEIDON2_WIDE_COLS],
+            self.fixed_log2_rows,
+        );
+
         // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(
+        let trace = RowMajorMatrix::new(
             rows.into_iter().flatten().collect::<Vec<_>>(),
             NUM_POSEIDON2_WIDE_COLS,
         );
-
-        // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_POSEIDON2_WIDE_COLS, F>(&mut trace.values);
 
         #[cfg(debug_assertions)]
         println!(
@@ -445,7 +451,9 @@ mod tests {
     /// A test generating a trace for a single permutation that checks that the output is correct
     #[test]
     fn generate_trace() {
-        let chip = Poseidon2WideChip::<3>;
+        let chip = Poseidon2WideChip::<3> {
+            fixed_log2_rows: None,
+        };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
             [BabyBear::from_canonical_u32(2); WIDTH],
@@ -492,7 +500,9 @@ mod tests {
         let config = BabyBearPoseidon2Inner::new();
         let mut challenger = config.challenger();
 
-        let chip = Poseidon2WideChip::<3>;
+        let chip = Poseidon2WideChip::<3> {
+            fixed_log2_rows: None,
+        };
 
         let test_inputs = (0..1000)
             .map(|i| [BabyBear::from_canonical_u32(i); WIDTH])

argumentcomputer/sphinx

recursion/circuit/src/stark.rs
@@ -14,7 +14,7 @@ use wp1_core::{
 };
 use wp1_recursion_compiler::config::OuterConfig;
 use wp1_recursion_compiler::constraints::{Constraint, ConstraintCompiler};
-use wp1_recursion_compiler::ir::{Builder, Config, Felt};
+use wp1_recursion_compiler::ir::{Builder, Config};
 use wp1_recursion_compiler::ir::{Usize, Witness};
 use wp1_recursion_compiler::prelude::SymbolicVar;
 use wp1_recursion_core::stark::config::{outer_fri_config, BabyBearPoseidon2Outer};
@@ -235,8 +235,8 @@ type OuterF = <BabyBearPoseidon2Outer as StarkGenericConfig>::Val;
 type OuterC = OuterConfig;
 
 pub fn build_wrap_circuit(
-    vk: &StarkVerifyingKey<OuterSC>,
-    dummy_proof: &ShardProof<OuterSC>,
+    template_vk: &StarkVerifyingKey<OuterSC>,
+    template_proof: &ShardProof<OuterSC>,
 ) -> Vec<Constraint> {
     let dev_mode = std::env::var("SP1_DEV_WRAPPER")
         .unwrap_or("true".to_string())
@@ -244,21 +244,21 @@ pub fn build_wrap_circuit(
         .eq("true");
 
     let outer_config = OuterSC::new();
-    let outer_machine = RecursionAirSkinnyDeg7::<OuterF>::machine(outer_config);
+    let outer_machine = RecursionAirSkinnyDeg7::<OuterF>::wrap_machine(outer_config);
 
     let mut builder = Builder::<OuterConfig>::default();
     let mut challenger = MultiField32ChallengerVariable::new(&mut builder);
 
-    let preprocessed_commit_val: [Bn254Fr; 1] = vk.commit.into();
+    let preprocessed_commit_val: [Bn254Fr; 1] = template_vk.commit.into();
     let preprocessed_commit: OuterDigestVariable<OuterC> =
         [builder.eval(preprocessed_commit_val[0])];
     challenger.observe_commitment(&mut builder, preprocessed_commit);
-    let pc_start: Felt<_> = builder.eval(vk.pc_start);
+    let pc_start = builder.eval(template_vk.pc_start);
     challenger.observe(&mut builder, pc_start);
 
     if !dev_mode {
         let chips = outer_machine
-            .shard_chips_ordered(&dummy_proof.chip_ordering)
+            .shard_chips_ordered(&template_proof.chip_ordering)
             .map(|chip| chip.name())
             .collect::<Vec<_>>();
 
@@ -266,7 +266,7 @@ pub fn build_wrap_circuit(
             .chips()
             .iter()
             .map(|chip| {
-                dummy_proof
+                template_proof
                     .chip_ordering
                     .get(&chip.name())
                     .copied()
@@ -275,7 +275,7 @@ pub fn build_wrap_circuit(
             .collect::<Vec<_>>();
 
         let chip_quotient_data = outer_machine
-            .shard_chips_ordered(&dummy_proof.chip_ordering)
+            .shard_chips_ordered(&template_proof.chip_ordering)
             .map(|chip| {
                 let log_quotient_degree = chip.log_quotient_degree();
                 QuotientDataValues {
@@ -286,8 +286,8 @@ pub fn build_wrap_circuit(
             .collect::<Vec<_>>();
 
         let mut witness = Witness::default();
-        dummy_proof.write(&mut witness);
-        let proof = dummy_proof.read(&mut builder);
+        template_proof.write(&mut witness);
+        let proof = template_proof.read(&mut builder);
 
         let ShardCommitment { main_commit, .. } = &proof.commitment;
         challenger.observe_commitment(&mut builder, *main_commit);
@@ -300,7 +300,7 @@ pub fn build_wrap_circuit(
 
         StarkVerifierCircuit::<OuterC, OuterSC>::verify_shard(
             &mut builder,
-            vk,
+            template_vk,
             &outer_machine,
             &mut challenger.clone(),
             &proof,

recursion/core/src/fri_fold/mod.rs
@@ -11,7 +11,7 @@ use p3_matrix::Matrix;
 use std::borrow::BorrowMut;
 use tracing::instrument;
 use wp1_core::air::{BinomialExtension, MachineAir};
-use wp1_core::utils::pad_to_power_of_two;
+use wp1_core::utils::pad_rows_fixed;
 use wp1_derive::AlignedBorrow;
 
 use crate::air::SP1RecursionAirBuilder;
@@ -21,7 +21,9 @@ use crate::runtime::{ExecutionRecord, RecursionProgram};
 pub const NUM_FRI_FOLD_COLS: usize = core::mem::size_of::<FriFoldCols<u8>>();
 
 #[derive(Default)]
-pub struct FriFoldChip;
+pub struct FriFoldChip {
+    pub fixed_log2_rows: Option<usize>,
+}
 
 #[derive(Debug, Clone)]
 pub struct FriFoldEvent<F> {
@@ -94,16 +96,16 @@ impl<F: PrimeField32> MachineAir<F> for FriFoldChip {
         // This is a no-op.
     }
 
-    #[instrument(name = "generate fri fold trace", level = "debug", skip_all)]
+    #[instrument(name = "generate fri fold trace", level = "debug", skip_all, fields(rows = input.fri_fold_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
         _: &mut ExecutionRecord<F>,
     ) -> RowMajorMatrix<F> {
-        let trace_values = input
+        let mut rows = input
             .fri_fold_events
             .iter()
-            .flat_map(|event| {
+            .map(|event| {
                 let mut row = [F::zero(); NUM_FRI_FOLD_COLS];
 
                 let cols: &mut FriFoldCols<F> = row.as_mut_slice().borrow_mut();
@@ -129,15 +131,19 @@ impl<F: PrimeField32> MachineAir<F> for FriFoldChip {
                     .populate(&event.alpha_pow_at_log_height);
                 cols.ro_at_log_height.populate(&event.ro_at_log_height);
 
-                row.into_iter()
+                row
             })
             .collect_vec();
 
-        // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(trace_values, NUM_FRI_FOLD_COLS);
-
         // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_FRI_FOLD_COLS, F>(&mut trace.values);
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_FRI_FOLD_COLS],
+            self.fixed_log2_rows,
+        );
+
+        // Convert the trace to a row major matrix.
+        let trace = RowMajorMatrix::new(rows.into_iter().flatten().collect(), NUM_FRI_FOLD_COLS);
 
         #[cfg(debug_assertions)]
         println!(

recursion/core/src/poseidon2/external.rs
@@ -9,7 +9,7 @@ use p3_matrix::Matrix;
 use std::borrow::BorrowMut;
 use tracing::instrument;
 use wp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
-use wp1_core::utils::pad_to_power_of_two;
+use wp1_core::utils::pad_rows_fixed;
 use wp1_derive::AlignedBorrow;
 use wp1_primitives::RC_16_30_U32;
 
@@ -24,7 +24,9 @@ pub(crate) const WIDTH: usize = 16;
 
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
-pub struct Poseidon2Chip;
+pub struct Poseidon2Chip {
+    fixed_trace_log2: Option<usize>,
+}
 
 /// The column layout for the chip.
 #[derive(AlignedBorrow, Default, Clone, Copy)]
@@ -54,7 +56,7 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
         // This is a no-op.
     }
 
-    #[instrument(name = "generate poseidon2 trace", level = "debug", skip_all)]
+    #[instrument(name = "generate poseidon2 trace", level = "debug", skip_all, fields(rows = input.poseidon2_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
@@ -139,16 +141,18 @@ impl<F: PrimeField32> MachineAir<F> for Poseidon2Chip {
             }
         }
 
-        // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(
-            rows.into_iter().flatten().collect::<Vec<_>>(),
-            NUM_POSEIDON2_COLS,
+        // Pad the trace to a power of two.
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_POSEIDON2_COLS],
+            self.fixed_trace_log2,
         );
 
-        // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_POSEIDON2_COLS, F>(&mut trace.values);
-
-        trace
+        // Convert the trace to a row major matrix.
+        RowMajorMatrix::new(
+            rows.into_iter().flatten().collect::<Vec<_>>(),
+            NUM_POSEIDON2_COLS,
+        )
     }
 
     fn included(&self, record: &Self::Record) -> bool {
@@ -336,7 +340,9 @@ mod tests {
     #[test]
     #[ignore = "broken in upstream, will be fixed by https://github.com/succinctlabs/sp1/pull/672"]
     fn generate_trace() {
-        let chip = Poseidon2Chip;
+        let chip = Poseidon2Chip {
+            fixed_trace_log2: None,
+        };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
             [BabyBear::from_canonical_u32(2); WIDTH],
@@ -380,7 +386,9 @@ mod tests {
         let config = BabyBearPoseidon2::new();
         let mut challenger = config.challenger();
 
-        let chip = Poseidon2Chip;
+        let chip = Poseidon2Chip {
+            fixed_trace_log2: None,
+        };
 
         let test_inputs = (0..16)
             .map(|i| [BabyBear::from_canonical_u32(i); WIDTH])

recursion/core/src/poseidon2_wide/external.rs
@@ -8,7 +8,7 @@ use p3_matrix::Matrix;
 use std::borrow::BorrowMut;
 use tracing::instrument;
 use wp1_core::air::{BaseAirBuilder, MachineAir, SP1AirBuilder};
-use wp1_core::utils::pad_to_power_of_two;
+use wp1_core::utils::pad_rows_fixed;
 use wp1_derive::AlignedBorrow;
 use wp1_primitives::RC_16_30_U32;
 
@@ -32,7 +32,9 @@ pub const NUM_ROUNDS: usize = NUM_EXTERNAL_ROUNDS + NUM_INTERNAL_ROUNDS;
 
 /// A chip that implements addition for the opcode ADD.
 #[derive(Default)]
-pub struct Poseidon2WideChip<const DEGREE: usize>;
+pub struct Poseidon2WideChip<const DEGREE: usize> {
+    pub fixed_log2_rows: Option<usize>,
+}
 
 #[derive(AlignedBorrow, Clone, Copy)]
 #[repr(C)]
@@ -90,7 +92,7 @@ impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for Poseidon2WideChip<D
         // This is a no-op.
     }
 
-    #[instrument(name = "generate poseidon2 wide trace", level = "debug", skip_all)]
+    #[instrument(name = "generate poseidon2 wide trace", level = "debug", skip_all, fields(rows = input.poseidon2_events.len()))]
     fn generate_trace(
         &self,
         input: &ExecutionRecord<F>,
@@ -149,14 +151,18 @@ impl<F: PrimeField32, const DEGREE: usize> MachineAir<F> for Poseidon2WideChip<D
             rows.push(row);
         }
 
+        // Pad the trace to a power of two.
+        pad_rows_fixed(
+            &mut rows,
+            || [F::zero(); NUM_POSEIDON2_WIDE_COLS],
+            self.fixed_log2_rows,
+        );
+
         // Convert the trace to a row major matrix.
-        let mut trace = RowMajorMatrix::new(
+        let trace = RowMajorMatrix::new(
             rows.into_iter().flatten().collect::<Vec<_>>(),
             NUM_POSEIDON2_WIDE_COLS,
         );
-
-        // Pad the trace to a power of two.
-        pad_to_power_of_two::<NUM_POSEIDON2_WIDE_COLS, F>(&mut trace.values);
 
         #[cfg(debug_assertions)]
         println!(
@@ -446,7 +452,9 @@ mod tests {
     #[test]
     #[ignore = "broken in upstream, will be fixed by https://github.com/succinctlabs/sp1/pull/672"]
     fn generate_trace() {
-        let chip = Poseidon2WideChip::<3>;
+        let chip = Poseidon2WideChip::<3> {
+            fixed_log2_rows: None,
+        };
         let test_inputs = vec![
             [BabyBear::from_canonical_u32(1); WIDTH],
             [BabyBear::from_canonical_u32(2); WIDTH],
@@ -494,7 +502,9 @@ mod tests {
         let config = BabyBearPoseidon2Inner::new();
         let mut challenger = config.challenger();
 
-        let chip = Poseidon2WideChip::<3>;
+        let chip = Poseidon2WideChip::<3> {
+            fixed_log2_rows: None,
+        };
 
         let test_inputs = (0..1000)
             .map(|i| [BabyBear::from_canonical_u32(i); WIDTH])