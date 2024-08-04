96761bf7fdeb396300906d007df61dd6543f5d4b
porcuquine <porcuquine@users.noreply.github.com>
2024-08-04 14:19:42 -0700

    Use idiomatic sum.

--- examples/sum/program/src/main.rs
+++ examples/sum/program/src/main.rs
@@ -6,7 +6,7 @@ sp1_zkvm::entrypoint!(main);
 pub fn main() {
     let input = sp1_zkvm::io::read::<Vec<u64>>();
 
-    let output = input.iter().fold(0, |acc, i| acc + i);
+    let output : u64 = input.iter().sum();
 
     sp1_zkvm::io::commit(&input);
     sp1_zkvm::io::commit(&output);
