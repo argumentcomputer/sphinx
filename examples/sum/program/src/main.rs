82ed4b0f6627ca1c223bbb0118591d01eb2a5712
porcuquine <porcuquine@users.noreply.github.com>
2024-08-15 13:21:13 -0700

    Use sphinx entrypoint.

--- examples/sum/program/src/main.rs
+++ examples/sum/program/src/main.rs
@@ -1,13 +1,13 @@
 //! A simple program to be proven inside the zkVM.
 
 #![no_main]
-sp1_zkvm::entrypoint!(main);
+sphinx_zkvm::entrypoint!(main);
 
 pub fn main() {
-    let input = sp1_zkvm::io::read::<Vec<u64>>();
+    let input = sphinx_zkvm::io::read::<Vec<u64>>();
 
-    let output : u64 = input.iter().sum();
+    let output: u64 = input.iter().sum();
 
-    sp1_zkvm::io::commit(&input);
-    sp1_zkvm::io::commit(&output);
+    sphinx_zkvm::io::commit(&input);
+    sphinx_zkvm::io::commit(&output);
 }
