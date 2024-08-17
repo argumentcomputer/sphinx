WindOctober/sp1

feat: improve network prover error output (#991)

sdk/src/network/client.rs
@@ -5,27 +5,24 @@ use crate::{
     proto::network::{UnclaimProofRequest, UnclaimReason},
 };
 use anyhow::{Context, Ok, Result};
-use futures::future::join_all;
+use futures::{future::join_all, Future};
 use reqwest::{Client as HttpClient, Url};
 use reqwest_middleware::ClientWithMiddleware as HttpClientWithMiddleware;
 use serde::de::DeserializeOwned;
 use sp1_prover::SP1Stdin;
+use std::result::Result::Ok as StdOk;
 use std::time::{SystemTime, UNIX_EPOCH};
-use twirp::Client as TwirpClient;
+use twirp::{Client as TwirpClient, ClientError};
 
 use crate::proto::network::{
     ClaimProofRequest, ClaimProofResponse, CreateProofRequest, FulfillProofRequest,
     FulfillProofResponse, GetNonceRequest, GetProofRequestsRequest, GetProofRequestsResponse,
-    GetProofStatusRequest, GetProofStatusResponse, GetRelayStatusRequest, GetRelayStatusResponse,
-    NetworkServiceClient, ProofMode, ProofStatus, RelayProofRequest, SubmitProofRequest,
-    TransactionStatus,
+    GetProofStatusRequest, GetProofStatusResponse, NetworkServiceClient, ProofMode, ProofStatus,
+    SubmitProofRequest,
 };
 
 /// The default RPC endpoint for the Succinct prover network.
 pub const DEFAULT_PROVER_NETWORK_RPC: &str = "https://rpc.succinct.xyz/";
-
-/// The default SP1 Verifier address on all chains.
-const DEFAULT_SP1_VERIFIER_ADDRESS: &str = "0xed2107448519345059eab9cddab42ddc78fbebe9";
 
 /// The timeout for a proof request to be fulfilled.
 const TIMEOUT: Duration = Duration::from_secs(60 * 60);
@@ -37,11 +34,12 @@ pub struct NetworkClient {
 }
 
 impl NetworkClient {
+    /// Returns the currently configured RPC endpoint for the Succinct prover network.
     pub fn rpc_url() -> String {
         env::var("PROVER_NETWORK_RPC").unwrap_or_else(|_| DEFAULT_PROVER_NETWORK_RPC.to_string())
     }
 
-    // Create a new NetworkClient with the given private key for authentication.
+    /// Create a new NetworkClient with the given private key for authentication.
     pub fn new(private_key: &str) -> Self {
         let auth = NetworkAuth::new(private_key);
 
@@ -68,45 +66,31 @@ impl NetworkClient {
         }
     }
 
-    // Get the address for the SP1 Verifier contract.
-    pub fn get_sp1_verifier_address() -> [u8; 20] {
-        let verifier_hex = env::var("SP1_VERIFIER_ADDRESS")
-            .unwrap_or_else(|_| DEFAULT_SP1_VERIFIER_ADDRESS.to_string());
-        let verifier_bytes = hex::decode(verifier_hex.trim_start_matches("0x"))
-            .expect("Invalid SP1_VERIFIER_ADDRESS format");
-
-        verifier_bytes
-            .try_into()
-            .expect("SP1_VERIFIER_ADDRESS must be 20 bytes")
-    }
-
     /// Gets the latest nonce for this auth's account.
     pub async fn get_nonce(&self) -> Result<u64> {
         let res = self
-            .rpc
-            .get_nonce(GetNonceRequest {
+            .with_error_handling(self.rpc.get_nonce(GetNonceRequest {
                 address: self.auth.get_address().to_vec(),
-            })
+            }))
             .await?;
         Ok(res.nonce)
     }
 
-    // Upload a file to the specified url.
+    /// Upload a file to the specified url.
     async fn upload_file(&self, url: &str, data: Vec<u8>) -> Result<()> {
         self.http.put(url).body(data).send().await?;
         Ok(())
     }
 
-    // Get the status of a given proof. If the status is ProofFulfilled, the proof is also returned.
+    /// Get the status of a given proof. If the status is ProofFulfilled, the proof is also returned.
     pub async fn get_proof_status<P: DeserializeOwned>(
         &self,
         proof_id: &str,
     ) -> Result<(GetProofStatusResponse, Option<P>)> {
         let res = self
-            .rpc
-            .get_proof_status(GetProofStatusRequest {
+            .with_error_handling(self.rpc.get_proof_status(GetProofStatusRequest {
                 proof_id: proof_id.to_string(),
-            })
+            }))
             .await
             .context("Failed to get proof status")?;
 
@@ -131,44 +115,15 @@ impl NetworkClient {
         Ok((res, proof))
     }
 
-    // Get all the proof requests for a given status.
+    /// Get all the proof requests for a given status.
     pub async fn get_proof_requests(
         &self,
         status: ProofStatus,
     ) -> Result<GetProofRequestsResponse> {
-        let res = self
-            .rpc
-            .get_proof_requests(GetProofRequestsRequest {
-                status: status.into(),
-            })
-            .await?;
-
-        Ok(res)
-    }
-
-    // Get the status of a relay transaction request.
-    pub async fn get_relay_status(
-        &self,
-        tx_id: &str,
-    ) -> Result<(GetRelayStatusResponse, Option<String>, Option<String>)> {
-        let res = self
-            .rpc
-            .get_relay_status(GetRelayStatusRequest {
-                tx_id: tx_id.to_string(),
-            })
-            .await?;
-
-        let tx_hash = match res.status() {
-            TransactionStatus::TransactionScheduled => None,
-            _ => Some(format!("0x{}", hex::encode(res.tx_hash.clone()))),
-        };
-
-        let simulation_url = match res.status() {
-            TransactionStatus::TransactionFailed => Some(res.simulation_url.clone()),
-            _ => None,
-        };
-
-        Ok((res, tx_hash, simulation_url))
+        self.with_error_handling(self.rpc.get_proof_requests(GetProofRequestsRequest {
+            status: status.into(),
+        }))
+        .await
     }
 
     /// Creates a proof request for the given ELF and stdin.
@@ -190,15 +145,15 @@ impl NetworkClient {
             .auth
             .sign_create_proof_message(nonce, deadline, mode.into(), version)
             .await?;
+
         let res = self
-            .rpc
-            .create_proof(CreateProofRequest {
+            .with_error_handling(self.rpc.create_proof(CreateProofRequest {
                 signature: create_proof_signature.to_vec(),
                 nonce,
                 deadline,
                 mode: mode.into(),
                 version: version.to_string(),
-            })
+            }))
             .await?;
 
         let program_bytes = bincode::serialize(elf)?;
@@ -215,37 +170,34 @@ impl NetworkClient {
             .auth
             .sign_submit_proof_message(nonce, &res.proof_id)
             .await?;
-        self.rpc
-            .submit_proof(SubmitProofRequest {
-                signature: submit_proof_signature.to_vec(),
-                nonce,
-                proof_id: res.proof_id.clone(),
-            })
-            .await?;
+
+        self.with_error_handling(self.rpc.submit_proof(SubmitProofRequest {
+            signature: submit_proof_signature.to_vec(),
+            nonce,
+            proof_id: res.proof_id.clone(),
+        }))
+        .await?;
 
         Ok(res.proof_id)
     }
 
-    // Claim a proof that was requested. This commits to generating a proof and fulfilling it.
-    // Returns an error if the proof is not in a PROOF_REQUESTED state.
+    /// Claim a proof that was requested. This commits to generating a proof and fulfilling it.
+    /// Returns an error if the proof is not in a PROOF_REQUESTED state.
     pub async fn claim_proof(&self, proof_id: &str) -> Result<ClaimProofResponse> {
         let nonce = self.get_nonce().await?;
         let signature = self.auth.sign_claim_proof_message(nonce, proof_id).await?;
-        let res = self
-            .rpc
-            .claim_proof(ClaimProofRequest {
-                signature,
-                nonce,
-                proof_id: proof_id.to_string(),
-            })
-            .await?;
 
-        Ok(res)
+        self.with_error_handling(self.rpc.claim_proof(ClaimProofRequest {
+            signature,
+            nonce,
+            proof_id: proof_id.to_string(),
+        }))
+        .await
     }
 
-    // Unclaim a proof that was claimed. This should only be called if the proof has not been
-    // fulfilled yet. Returns an error if the proof is not in a PROOF_CLAIMED state or if the caller
-    // is not the claimer.
+    /// Unclaim a proof that was claimed. This should only be called if the proof has not been
+    /// fulfilled yet. Returns an error if the proof is not in a PROOF_CLAIMED state or if the caller
+    /// is not the claimer.
     pub async fn unclaim_proof(
         &self,
         proof_id: String,
@@ -257,21 +209,21 @@ impl NetworkClient {
             .auth
             .sign_unclaim_proof_message(nonce, proof_id.clone(), reason, description.clone())
             .await?;
-        self.rpc
-            .unclaim_proof(UnclaimProofRequest {
-                signature,
-                nonce,
-                proof_id,
-                reason: reason.into(),
-                description,
-            })
-            .await?;
+
+        self.with_error_handling(self.rpc.unclaim_proof(UnclaimProofRequest {
+            signature,
+            nonce,
+            proof_id,
+            reason: reason.into(),
+            description,
+        }))
+        .await?;
 
         Ok(())
     }
 
-    // Fulfill a proof. Should only be called after the proof has been uploaded. Returns an error
-    // if the proof is not in a PROOF_CLAIMED state or if the caller is not the claimer.
+    /// Fulfill a proof. Should only be called after the proof has been uploaded. Returns an error
+    /// if the proof is not in a PROOF_CLAIMED state or if the caller is not the claimer.
     pub async fn fulfill_proof(&self, proof_id: &str) -> Result<FulfillProofResponse> {
         let nonce = self.get_nonce().await?;
         let signature = self
@@ -279,41 +231,34 @@ impl NetworkClient {
             .sign_fulfill_proof_message(nonce, proof_id)
             .await?;
         let res = self
-            .rpc
-            .fulfill_proof(FulfillProofRequest {
+            .with_error_handling(self.rpc.fulfill_proof(FulfillProofRequest {
                 signature,
                 nonce,
                 proof_id: proof_id.to_string(),
-            })
+            }))
             .await?;
 
         Ok(res)
     }
 
-    // Relay a proof. Returns an error if the proof is not in a PROOF_FULFILLED state.
-    pub async fn relay_proof(
-        &self,
-        proof_id: &str,
-        chain_id: u32,
-        verifier: [u8; 20],
-        callback: [u8; 20],
-        callback_data: &[u8],
-    ) -> Result<String> {
-        let nonce = self.get_nonce().await?;
-        let signature = self
-            .auth
-            .sign_relay_proof_message(nonce, proof_id, chain_id, verifier, callback, callback_data)
-            .await?;
-        let req = RelayProofRequest {
-            signature,
-            nonce,
-            proof_id: proof_id.to_string(),
-            chain_id,
-            verifier: verifier.to_vec(),
-            callback: callback.to_vec(),
-            callback_data: callback_data.to_vec(),
-        };
-        let result = self.rpc.relay_proof(req).await?;
-        Ok(result.tx_id)
+    /// Awaits the future, then handles Succinct prover network errors.
+    async fn with_error_handling<T, F>(&self, future: F) -> Result<T>
+    where
+        F: Future<Output = std::result::Result<T, ClientError>>,
+    {
+        let result = future.await;
+        self.handle_twirp_error(result)
+    }
+
+    /// Handles Twirp errors by formatting them into more readable error messages.
+    fn handle_twirp_error<T>(&self, result: std::result::Result<T, ClientError>) -> Result<T> {
+        match result {
+            StdOk(response) => StdOk(response),
+            Err(ClientError::TwirpError(err)) => {
+                let display_err = format!("error: \"{:?}\" message: {:?}", err.code, err.msg);
+                Err(anyhow::anyhow!(display_err))
+            }
+            Err(err) => Err(err.into()),
+        }
     }
 }

argumentcomputer/sphinx

sdk/src/network/client.rs
@@ -8,27 +8,23 @@ use crate::{
     proto::network::{UnclaimProofRequest, UnclaimReason},
 };
 use anyhow::{Context, Ok, Result};
-use futures::future::join_all;
-
+use futures::{future::join_all, Future};
 use reqwest::{Client as HttpClient, Url};
 use reqwest_middleware::ClientWithMiddleware as HttpClientWithMiddleware;
 use serde::de::DeserializeOwned;
 use sphinx_prover::SphinxStdin;
-use twirp::Client as TwirpClient;
+use std::result::Result::Ok as StdOk;
+use twirp::{Client as TwirpClient, ClientError};
 
 use crate::proto::network::{
     ClaimProofRequest, ClaimProofResponse, CreateProofRequest, FulfillProofRequest,
     FulfillProofResponse, GetNonceRequest, GetProofRequestsRequest, GetProofRequestsResponse,
-    GetProofStatusRequest, GetProofStatusResponse, GetRelayStatusRequest, GetRelayStatusResponse,
-    NetworkServiceClient, ProofMode, ProofStatus, RelayProofRequest, SubmitProofRequest,
-    TransactionStatus,
+    GetProofStatusRequest, GetProofStatusResponse, NetworkServiceClient, ProofMode, ProofStatus,
+    SubmitProofRequest,
 };
 
 /// The default RPC endpoint for the Succinct prover network.
 pub const DEFAULT_PROVER_NETWORK_RPC: &str = "https://rpc.succinct.xyz/";
-
-/// The default SP1 Verifier address on all chains.
-const DEFAULT_SPHINX_VERIFIER_ADDRESS: &str = "0xed2107448519345059eab9cddab42ddc78fbebe9";
 
 /// The timeout for a proof request to be fulfilled.
 const TIMEOUT: Duration = Duration::from_secs(60 * 60);
@@ -40,11 +36,12 @@ pub struct NetworkClient {
 }
 
 impl NetworkClient {
+    /// Returns the currently configured RPC endpoint for the Succinct prover network.
     pub fn rpc_url() -> String {
         env::var("PROVER_NETWORK_RPC").unwrap_or_else(|_| DEFAULT_PROVER_NETWORK_RPC.to_string())
     }
 
-    // Create a new NetworkClient with the given private key for authentication.
+    /// Create a new NetworkClient with the given private key for authentication.
     pub fn new(private_key: &str) -> Self {
         let auth = NetworkAuth::new(private_key);
 
@@ -71,45 +68,31 @@ impl NetworkClient {
         }
     }
 
-    // Get the address for the SP1 Verifier contract.
-    pub fn get_sphinx_verifier_address() -> [u8; 20] {
-        let verifier_hex = env::var("SP1_VERIFIER_ADDRESS")
-            .unwrap_or_else(|_| DEFAULT_SPHINX_VERIFIER_ADDRESS.to_string());
-        let verifier_bytes = hex::decode(verifier_hex.trim_start_matches("0x"))
-            .expect("Invalid SP1_VERIFIER_ADDRESS format");
-
-        verifier_bytes
-            .try_into()
-            .expect("SP1_VERIFIER_ADDRESS must be 20 bytes")
-    }
-
     /// Gets the latest nonce for this auth's account.
     pub async fn get_nonce(&self) -> Result<u64> {
         let res = self
-            .rpc
-            .get_nonce(GetNonceRequest {
+            .with_error_handling(self.rpc.get_nonce(GetNonceRequest {
                 address: self.auth.get_address().to_vec(),
-            })
+            }))
             .await?;
         Ok(res.nonce)
     }
 
-    // Upload a file to the specified url.
+    /// Upload a file to the specified url.
     async fn upload_file(&self, url: &str, data: Vec<u8>) -> Result<()> {
         self.http.put(url).body(data).send().await?;
         Ok(())
     }
 
-    // Get the status of a given proof. If the status is ProofFulfilled, the proof is also returned.
+    /// Get the status of a given proof. If the status is ProofFulfilled, the proof is also returned.
     pub async fn get_proof_status<P: DeserializeOwned>(
         &self,
         proof_id: &str,
     ) -> Result<(GetProofStatusResponse, Option<P>)> {
         let res = self
-            .rpc
-            .get_proof_status(GetProofStatusRequest {
+            .with_error_handling(self.rpc.get_proof_status(GetProofStatusRequest {
                 proof_id: proof_id.to_string(),
-            })
+            }))
             .await
             .context("Failed to get proof status")?;
 
@@ -134,44 +117,15 @@ impl NetworkClient {
         Ok((res, proof))
     }
 
-    // Get all the proof requests for a given status.
+    /// Get all the proof requests for a given status.
     pub async fn get_proof_requests(
         &self,
         status: ProofStatus,
     ) -> Result<GetProofRequestsResponse> {
-        let res = self
-            .rpc
-            .get_proof_requests(GetProofRequestsRequest {
-                status: status.into(),
-            })
-            .await?;
-
-        Ok(res)
-    }
-
-    // Get the status of a relay transaction request.
-    pub async fn get_relay_status(
-        &self,
-        tx_id: &str,
-    ) -> Result<(GetRelayStatusResponse, Option<String>, Option<String>)> {
-        let res = self
-            .rpc
-            .get_relay_status(GetRelayStatusRequest {
-                tx_id: tx_id.to_string(),
-            })
-            .await?;
-
-        let tx_hash = match res.status() {
-            TransactionStatus::TransactionScheduled => None,
-            _ => Some(format!("0x{}", hex::encode(res.tx_hash.clone()))),
-        };
-
-        let simulation_url = match res.status() {
-            TransactionStatus::TransactionFailed => Some(res.simulation_url.clone()),
-            _ => None,
-        };
-
-        Ok((res, tx_hash, simulation_url))
+        self.with_error_handling(self.rpc.get_proof_requests(GetProofRequestsRequest {
+            status: status.into(),
+        }))
+        .await
     }
 
     /// Creates a proof request for the given ELF and stdin.
@@ -193,15 +147,15 @@ impl NetworkClient {
             .auth
             .sign_create_proof_message(nonce, deadline, mode.into(), version)
             .await?;
+
         let res = self
-            .rpc
-            .create_proof(CreateProofRequest {
+            .with_error_handling(self.rpc.create_proof(CreateProofRequest {
                 signature: create_proof_signature.clone(),
                 nonce,
                 deadline,
                 mode: mode.into(),
                 version: version.to_string(),
-            })
+            }))
             .await?;
 
         let program_bytes = bincode::serialize(elf)?;
@@ -218,37 +172,34 @@ impl NetworkClient {
             .auth
             .sign_submit_proof_message(nonce, &res.proof_id)
             .await?;
-        self.rpc
-            .submit_proof(SubmitProofRequest {
-                signature: submit_proof_signature.clone(),
-                nonce,
-                proof_id: res.proof_id.clone(),
-            })
-            .await?;
+
+        self.with_error_handling(self.rpc.submit_proof(SubmitProofRequest {
+            signature: submit_proof_signature.clone(),
+            nonce,
+            proof_id: res.proof_id.clone(),
+        }))
+        .await?;
 
         Ok(res.proof_id)
     }
 
-    // Claim a proof that was requested. This commits to generating a proof and fulfilling it.
-    // Returns an error if the proof is not in a PROOF_REQUESTED state.
+    /// Claim a proof that was requested. This commits to generating a proof and fulfilling it.
+    /// Returns an error if the proof is not in a PROOF_REQUESTED state.
     pub async fn claim_proof(&self, proof_id: &str) -> Result<ClaimProofResponse> {
         let nonce = self.get_nonce().await?;
         let signature = self.auth.sign_claim_proof_message(nonce, proof_id).await?;
-        let res = self
-            .rpc
-            .claim_proof(ClaimProofRequest {
-                signature,
-                nonce,
-                proof_id: proof_id.to_string(),
-            })
-            .await?;
 
-        Ok(res)
+        self.with_error_handling(self.rpc.claim_proof(ClaimProofRequest {
+            signature,
+            nonce,
+            proof_id: proof_id.to_string(),
+        }))
+        .await
     }
 
-    // Unclaim a proof that was claimed. This should only be called if the proof has not been
-    // fulfilled yet. Returns an error if the proof is not in a PROOF_CLAIMED state or if the caller
-    // is not the claimer.
+    /// Unclaim a proof that was claimed. This should only be called if the proof has not been
+    /// fulfilled yet. Returns an error if the proof is not in a PROOF_CLAIMED state or if the caller
+    /// is not the claimer.
     pub async fn unclaim_proof(
         &self,
         proof_id: String,
@@ -260,21 +211,21 @@ impl NetworkClient {
             .auth
             .sign_unclaim_proof_message(nonce, proof_id.clone(), reason, description.clone())
             .await?;
-        self.rpc
-            .unclaim_proof(UnclaimProofRequest {
-                signature,
-                nonce,
-                proof_id,
-                reason: reason.into(),
-                description,
-            })
-            .await?;
+
+        self.with_error_handling(self.rpc.unclaim_proof(UnclaimProofRequest {
+            signature,
+            nonce,
+            proof_id,
+            reason: reason.into(),
+            description,
+        }))
+        .await?;
 
         Ok(())
     }
 
-    // Fulfill a proof. Should only be called after the proof has been uploaded. Returns an error
-    // if the proof is not in a PROOF_CLAIMED state or if the caller is not the claimer.
+    /// Fulfill a proof. Should only be called after the proof has been uploaded. Returns an error
+    /// if the proof is not in a PROOF_CLAIMED state or if the caller is not the claimer.
     pub async fn fulfill_proof(&self, proof_id: &str) -> Result<FulfillProofResponse> {
         let nonce = self.get_nonce().await?;
         let signature = self
@@ -282,41 +233,34 @@ impl NetworkClient {
             .sign_fulfill_proof_message(nonce, proof_id)
             .await?;
         let res = self
-            .rpc
-            .fulfill_proof(FulfillProofRequest {
+            .with_error_handling(self.rpc.fulfill_proof(FulfillProofRequest {
                 signature,
                 nonce,
                 proof_id: proof_id.to_string(),
-            })
+            }))
             .await?;
 
         Ok(res)
     }
 
-    // Relay a proof. Returns an error if the proof is not in a PROOF_FULFILLED state.
-    pub async fn relay_proof(
-        &self,
-        proof_id: &str,
-        chain_id: u32,
-        verifier: [u8; 20],
-        callback: [u8; 20],
-        callback_data: &[u8],
-    ) -> Result<String> {
-        let nonce = self.get_nonce().await?;
-        let signature = self
-            .auth
-            .sign_relay_proof_message(nonce, proof_id, chain_id, verifier, callback, callback_data)
-            .await?;
-        let req = RelayProofRequest {
-            signature,
-            nonce,
-            proof_id: proof_id.to_string(),
-            chain_id,
-            verifier: verifier.to_vec(),
-            callback: callback.to_vec(),
-            callback_data: callback_data.to_vec(),
-        };
-        let result = self.rpc.relay_proof(req).await?;
-        Ok(result.tx_id)
+    /// Awaits the future, then handles Succinct prover network errors.
+    async fn with_error_handling<T, F>(&self, future: F) -> Result<T>
+    where
+        F: Future<Output = std::result::Result<T, ClientError>>,
+    {
+        let result = future.await;
+        self.handle_twirp_error(result)
+    }
+
+    /// Handles Twirp errors by formatting them into more readable error messages.
+    fn handle_twirp_error<T>(&self, result: std::result::Result<T, ClientError>) -> Result<T> {
+        match result {
+            StdOk(response) => StdOk(response),
+            Err(ClientError::TwirpError(err)) => {
+                let display_err = format!("error: \"{:?}\" message: {:?}", err.code, err.msg);
+                Err(anyhow::anyhow!(display_err))
+            }
+            Err(err) => Err(err.into()),
+        }
     }
 }