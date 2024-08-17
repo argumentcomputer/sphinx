WindOctober/sp1

feat: plonk circuit optimizations (#972)

prover/src/lib.rs
@@ -39,6 +39,7 @@ use sp1_core::{
     utils::{BabyBearPoseidon2, SP1CoreProverError},
 };
 use sp1_primitives::hash_deferred_proof;
+use sp1_primitives::types::RecursionProgramType;
 use sp1_recursion_circuit::witness::Witnessable;
 use sp1_recursion_compiler::config::InnerConfig;
 use sp1_recursion_compiler::ir::Witness;
@@ -164,14 +165,20 @@ impl SP1Prover {
         let (compress_pk, compress_vk) = compress_machine.setup(&compress_program);
 
         // Get the compress program, machine, and keys.
-        let shrink_program =
-            SP1RootVerifier::<InnerConfig, _, _>::build(&compress_machine, &compress_vk, true);
+        let shrink_program = SP1RootVerifier::<InnerConfig, _, _>::build(
+            &compress_machine,
+            &compress_vk,
+            RecursionProgramType::Shrink,
+        );
         let shrink_machine = CompressAir::wrap_machine_dyn(InnerSC::compressed());
         let (shrink_pk, shrink_vk) = shrink_machine.setup(&shrink_program);
 
         // Get the wrap program, machine, and keys.
-        let wrap_program =
-            SP1RootVerifier::<InnerConfig, _, _>::build(&shrink_machine, &shrink_vk, false);
+        let wrap_program = SP1RootVerifier::<InnerConfig, _, _>::build(
+            &shrink_machine,
+            &shrink_vk,
+            RecursionProgramType::Wrap,
+        );
         let wrap_machine = WrapAir::wrap_machine(OuterSC::default());
         let (wrap_pk, wrap_vk) = wrap_machine.setup(&wrap_program);
 

recursion/compiler/src/ir/builder.rs
@@ -2,6 +2,7 @@ use std::{iter::Zip, vec::IntoIter};
 
 use backtrace::Backtrace;
 use p3_field::AbstractField;
+use sp1_primitives::types::RecursionProgramType;
 
 use super::{
     Array, Config, DslIr, Ext, Felt, FromConstant, SymbolicExt, SymbolicFelt, SymbolicUsize,
@@ -103,10 +104,17 @@ pub struct Builder<C: Config> {
     pub(crate) p2_hash_num: Var<C::N>,
     pub(crate) debug: bool,
     pub(crate) is_sub_builder: bool,
+    pub program_type: RecursionProgramType,
 }
 
 impl<C: Config> Default for Builder<C> {
     fn default() -> Self {
+        Self::new(RecursionProgramType::Core)
+    }
+}
+
+impl<C: Config> Builder<C> {
+    pub fn new(program_type: RecursionProgramType) -> Self {
         // We need to create a temporary placeholder for the p2_hash_num variable.
         let placeholder_p2_hash_num = Var::new(0);
 
@@ -122,14 +130,13 @@ impl<C: Config> Default for Builder<C> {
             p2_hash_num: placeholder_p2_hash_num,
             debug: false,
             is_sub_builder: false,
+            program_type,
         };
 
         new_builder.p2_hash_num = new_builder.uninit();
         new_builder
     }
-}
 
-impl<C: Config> Builder<C> {
     /// Creates a new builder with a given number of counts for each type.
     pub fn new_sub_builder(
         var_count: u32,
@@ -138,6 +145,7 @@ impl<C: Config> Builder<C> {
         nb_public_values: Option<Var<C::N>>,
         p2_hash_num: Var<C::N>,
         debug: bool,
+        program_type: RecursionProgramType,
     ) -> Self {
         Self {
             felt_count,
@@ -153,6 +161,7 @@ impl<C: Config> Builder<C> {
             p2_hash_num,
             debug,
             is_sub_builder: true,
+            program_type,
         }
     }
 
@@ -546,6 +555,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
         f(&mut f_builder);
         self.builder.p2_hash_num = f_builder.p2_hash_num;
@@ -597,6 +607,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
 
         // Execute the `then` and `else_then` blocks and collect the instructions.
@@ -612,6 +623,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
         else_f(&mut else_builder);
         self.builder.p2_hash_num = else_builder.p2_hash_num;
@@ -749,6 +761,7 @@ impl<'a, C: Config> RangeBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
 
         f(loop_variable, &mut loop_body_builder);

recursion/program/src/machine/deferred.rs
@@ -11,6 +11,7 @@ use sp1_core::air::{Word, POSEIDON_NUM_WORDS, PV_DIGEST_NUM_WORDS};
 use sp1_core::stark::StarkMachine;
 use sp1_core::stark::{Com, RiscvAir, ShardProof, StarkGenericConfig, StarkVerifyingKey};
 use sp1_core::utils::BabyBearPoseidon2;
+use sp1_primitives::types::RecursionProgramType;
 use sp1_recursion_compiler::config::InnerConfig;
 use sp1_recursion_compiler::ir::{Array, Builder, Config, Felt, Var};
 use sp1_recursion_compiler::prelude::DslVariable;
@@ -83,7 +84,7 @@ where
 {
     /// Create a new instance of the program for the [BabyBearPoseidon2] config.
     pub fn build(machine: &StarkMachine<BabyBearPoseidon2, A>) -> RecursionProgram<BabyBear> {
-        let mut builder = Builder::<InnerConfig>::default();
+        let mut builder = Builder::<InnerConfig>::new(RecursionProgramType::Deferred);
         let input: SP1DeferredMemoryLayoutVariable<_> = builder.uninit();
         SP1DeferredMemoryLayout::<BabyBearPoseidon2, A>::witness(&input, &mut builder);
 

recursion/program/src/machine/root.rs
@@ -8,6 +8,7 @@ use sp1_core::air::MachineAir;
 use sp1_core::stark::StarkMachine;
 use sp1_core::stark::{Com, ShardProof, StarkGenericConfig, StarkVerifyingKey};
 use sp1_core::utils::BabyBearPoseidon2;
+use sp1_primitives::types::RecursionProgramType;
 use sp1_recursion_compiler::config::InnerConfig;
 use sp1_recursion_compiler::ir::{Builder, Config, Felt, Var};
 use sp1_recursion_compiler::prelude::DslVariable;
@@ -52,9 +53,15 @@ where
     pub fn build(
         machine: &StarkMachine<BabyBearPoseidon2, A>,
         vk: &StarkVerifyingKey<BabyBearPoseidon2>,
-        is_compress: bool,
+        program_type: RecursionProgramType,
     ) -> RecursionProgram<BabyBear> {
-        let mut builder = Builder::<InnerConfig>::default();
+        assert!(matches!(
+            program_type,
+            RecursionProgramType::Shrink | RecursionProgramType::Wrap
+        ));
+
+        let mut builder = Builder::<InnerConfig>::new(program_type);
+
         let proof: ShardProofVariable<_> = builder.uninit();
         ShardProofHint::<BabyBearPoseidon2, A>::witness(&proof, &mut builder);
 
@@ -62,7 +69,7 @@ where
             config: const_fri_config(&mut builder, machine.config().pcs().fri_config()),
         };
 
-        SP1RootVerifier::verify(&mut builder, &pcs, machine, vk, &proof, is_compress);
+        SP1RootVerifier::verify(&mut builder, &pcs, machine, vk, &proof);
 
         builder.compile_program()
     }
@@ -89,7 +96,6 @@ where
         machine: &StarkMachine<SC, A>,
         vk: &StarkVerifyingKey<SC>,
         proof: &ShardProofVariable<C>,
-        is_compress: bool,
     ) {
         // Get the verifying key info from the vk.
         let vk = proof_data_from_vk(builder, vk, machine);
@@ -135,9 +141,9 @@ where
         // checking the `is_complete` flag in this program.
         builder.assert_felt_eq(public_values.is_complete, C::F::one());
 
-        // If the proof is a compress proof, assert that the vk is the same as the compress vk from
-        // the public values.
-        if is_compress {
+        // If this is a Shrink program (when it's verifying a compress proof), then assert that the
+        // vk is the same as the compress vk from the public values.
+        if matches!(builder.program_type, RecursionProgramType::Shrink) {
             let vk_digest = hash_vkey(builder, &vk);
             for (i, reduce_digest_elem) in public_values.compress_vk_digest.iter().enumerate() {
                 let vk_digest_elem = builder.get(&vk_digest, i);

argumentcomputer/sphinx

prover/src/lib.rs
@@ -36,6 +36,7 @@ use sphinx_core::{
     utils::{BabyBearPoseidon2, SphinxCoreProverError},
 };
 use sphinx_primitives::hash_deferred_proof;
+use sphinx_primitives::types::RecursionProgramType;
 use sphinx_recursion_circuit::witness::Witnessable;
 use sphinx_recursion_compiler::config::InnerConfig;
 use sphinx_recursion_compiler::ir::Witness;
@@ -166,14 +167,20 @@ impl SphinxProver {
         let (compress_pk, compress_vk) = compress_machine.setup(&compress_program);
 
         // Get the compress program, machine, and keys.
-        let shrink_program =
-            SphinxRootVerifier::<InnerConfig, _, _>::build(&compress_machine, &compress_vk, true);
+        let shrink_program = SphinxRootVerifier::<InnerConfig, _, _>::build(
+            &compress_machine,
+            &compress_vk,
+            RecursionProgramType::Shrink,
+        );
         let shrink_machine = CompressAir::wrap_machine_dyn(InnerSC::compressed());
         let (shrink_pk, shrink_vk) = shrink_machine.setup(&shrink_program);
 
         // Get the wrap program, machine, and keys.
-        let wrap_program =
-            SphinxRootVerifier::<InnerConfig, _, _>::build(&shrink_machine, &shrink_vk, false);
+        let wrap_program = SphinxRootVerifier::<InnerConfig, _, _>::build(
+            &shrink_machine,
+            &shrink_vk,
+            RecursionProgramType::Wrap,
+        );
         let wrap_machine = WrapAir::wrap_machine(OuterSC::default());
         let (wrap_pk, wrap_vk) = wrap_machine.setup(&wrap_program);
 

recursion/compiler/src/ir/builder.rs
@@ -2,6 +2,7 @@ use std::{iter::Zip, vec::IntoIter};
 
 use backtrace::Backtrace;
 use p3_field::AbstractField;
+use sphinx_primitives::types::RecursionProgramType;
 
 use super::{
     Array, Config, DslIr, Ext, Felt, FromConstant, SymbolicExt, SymbolicFelt, SymbolicUsize,
@@ -103,10 +104,17 @@ pub struct Builder<C: Config> {
     pub(crate) p2_hash_num: Var<C::N>,
     pub(crate) debug: bool,
     pub(crate) is_sub_builder: bool,
+    pub program_type: RecursionProgramType,
 }
 
 impl<C: Config> Default for Builder<C> {
     fn default() -> Self {
+        Self::new(RecursionProgramType::Core)
+    }
+}
+
+impl<C: Config> Builder<C> {
+    pub fn new(program_type: RecursionProgramType) -> Self {
         // We need to create a temporary placeholder for the p2_hash_num variable.
         let placeholder_p2_hash_num = Var::new(0);
 
@@ -122,14 +130,13 @@ impl<C: Config> Default for Builder<C> {
             p2_hash_num: placeholder_p2_hash_num,
             debug: false,
             is_sub_builder: false,
+            program_type,
         };
 
         new_builder.p2_hash_num = new_builder.uninit();
         new_builder
     }
-}
 
-impl<C: Config> Builder<C> {
     /// Creates a new builder with a given number of counts for each type.
     pub fn new_sub_builder(
         var_count: u32,
@@ -138,6 +145,7 @@ impl<C: Config> Builder<C> {
         nb_public_values: Option<Var<C::N>>,
         p2_hash_num: Var<C::N>,
         debug: bool,
+        program_type: RecursionProgramType,
     ) -> Self {
         Self {
             felt_count,
@@ -153,6 +161,7 @@ impl<C: Config> Builder<C> {
             p2_hash_num,
             debug,
             is_sub_builder: true,
+            program_type,
         }
     }
 
@@ -546,6 +555,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
         f(&mut f_builder);
         self.builder.p2_hash_num = f_builder.p2_hash_num;
@@ -597,6 +607,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
 
         // Execute the `then` and `else_then` blocks and collect the instructions.
@@ -612,6 +623,7 @@ impl<'a, C: Config> IfBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
         else_f(&mut else_builder);
         self.builder.p2_hash_num = else_builder.p2_hash_num;
@@ -749,6 +761,7 @@ impl<'a, C: Config> RangeBuilder<'a, C> {
             self.builder.nb_public_values,
             self.builder.p2_hash_num,
             self.builder.debug,
+            self.builder.program_type,
         );
 
         f(loop_variable, &mut loop_body_builder);

recursion/program/src/machine/deferred.rs
@@ -11,6 +11,7 @@ use sphinx_core::air::{Word, POSEIDON_NUM_WORDS, PV_DIGEST_NUM_WORDS};
 use sphinx_core::stark::StarkMachine;
 use sphinx_core::stark::{Com, RiscvAir, ShardProof, StarkGenericConfig, StarkVerifyingKey};
 use sphinx_core::utils::BabyBearPoseidon2;
+use sphinx_primitives::types::RecursionProgramType;
 use sphinx_recursion_compiler::config::InnerConfig;
 use sphinx_recursion_compiler::ir::{Array, Builder, Config, Felt, Var};
 use sphinx_recursion_compiler::prelude::DslVariable;
@@ -83,7 +84,7 @@ where
 {
     /// Create a new instance of the program for the [BabyBearPoseidon2] config.
     pub fn build(machine: &StarkMachine<BabyBearPoseidon2, A>) -> RecursionProgram<BabyBear> {
-        let mut builder = Builder::<InnerConfig>::default();
+        let mut builder = Builder::<InnerConfig>::new(RecursionProgramType::Deferred);
         let input: SphinxDeferredMemoryLayoutVariable<_> = builder.uninit();
         SphinxDeferredMemoryLayout::<BabyBearPoseidon2, A>::witness(&input, &mut builder);
 

recursion/program/src/machine/root.rs
@@ -8,6 +8,7 @@ use sphinx_core::air::MachineAir;
 use sphinx_core::stark::StarkMachine;
 use sphinx_core::stark::{Com, ShardProof, StarkGenericConfig, StarkVerifyingKey};
 use sphinx_core::utils::BabyBearPoseidon2;
+use sphinx_primitives::types::RecursionProgramType;
 use sphinx_recursion_compiler::config::InnerConfig;
 use sphinx_recursion_compiler::ir::{Builder, Config, Felt, Var};
 use sphinx_recursion_compiler::prelude::DslVariable;
@@ -52,9 +53,15 @@ where
     pub fn build(
         machine: &StarkMachine<BabyBearPoseidon2, A>,
         vk: &StarkVerifyingKey<BabyBearPoseidon2>,
-        is_compress: bool,
+        program_type: RecursionProgramType,
     ) -> RecursionProgram<BabyBear> {
-        let mut builder = Builder::<InnerConfig>::default();
+        assert!(matches!(
+            program_type,
+            RecursionProgramType::Shrink | RecursionProgramType::Wrap
+        ));
+
+        let mut builder = Builder::<InnerConfig>::new(program_type);
+
         let proof: ShardProofVariable<_> = builder.uninit();
         ShardProofHint::<BabyBearPoseidon2, A>::witness(&proof, &mut builder);
 
@@ -62,7 +69,7 @@ where
             config: const_fri_config(&mut builder, machine.config().pcs().fri_config()),
         };
 
-        SphinxRootVerifier::verify(&mut builder, &pcs, machine, vk, &proof, is_compress);
+        SphinxRootVerifier::verify(&mut builder, &pcs, machine, vk, &proof);
 
         builder.compile_program()
     }
@@ -89,7 +96,6 @@ where
         machine: &StarkMachine<SC, A>,
         vk: &StarkVerifyingKey<SC>,
         proof: &ShardProofVariable<C>,
-        is_compress: bool,
     ) {
         // Get the verifying key info from the vk.
         let vk = proof_data_from_vk(builder, vk, machine);
@@ -135,9 +141,9 @@ where
         // checking the `is_complete` flag in this program.
         builder.assert_felt_eq(public_values.is_complete, C::F::one());
 
-        // If the proof is a compress proof, assert that the vk is the same as the compress vk from
-        // the public values.
-        if is_compress {
+        // If this is a Shrink program (when it's verifying a compress proof), then assert that the
+        // vk is the same as the compress vk from the public values.
+        if matches!(builder.program_type, RecursionProgramType::Shrink) {
             let vk_digest = hash_vkey(builder, &vk);
             for (i, reduce_digest_elem) in public_values.compress_vk_digest.iter().enumerate() {
                 let vk_digest_elem = builder.get(&vk_digest, i);