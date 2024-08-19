succinctlabs/sp1

feat: (breaking changes to SDK API) use builder pattern for SDK execute/prove/verify (#940)
- CLI test now fails because of the breaking changes
- Refactor a lot of the parallel three-type proof code in SDK to use the new `SP1Proof` enum.

examples/ssz-withdrawals/script/src/main.rs
@@ -1,4 +1,4 @@
-use sp1_sdk::{utils, ProverClient, SP1Proof, SP1Stdin};
+use sp1_sdk::{utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -10,7 +10,7 @@ fn main() {
     let stdin = SP1Stdin::new();
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
-    let proof = client.prove(&pk, stdin).expect("proving failed");
+    let proof = client.prove(&pk, stdin).run().expect("proving failed");
 
     // Verify proof.
     client.verify(&proof, &vk).expect("verification failed");
@@ -19,7 +19,8 @@ fn main() {
     proof
         .save("proof-with-pis.bin")
         .expect("saving proof failed");
-    let deserialized_proof = SP1Proof::load("proof-with-pis.bin").expect("loading proof failed");
+    let deserialized_proof =
+        SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
 
     // Verify the deserialized proof.
     client

prover/scripts/fibonacci_sweep.rs
@@ -3,7 +3,10 @@
 use std::{fs::File, io::BufWriter, io::Write, time::Instant};
 
 use itertools::iproduct;
-use sp1_core::io::SP1Stdin;
+use sp1_core::{
+    io::SP1Stdin,
+    utils::{SP1ProverOpts, SP1ProverOpts},
+};
 use sp1_prover::SP1Prover;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
@@ -62,7 +65,9 @@ fn main() {
             proofs: vec![],
         };
         let leaf_proving_start = Instant::now();
-        let proof = prover.prove_core(&pk, &stdin).unwrap();
+        let proof = prover
+            .prove_core(&pk, &stdin, SP1ProverOpts::default(), SP1Context::default())
+            .unwrap();
         let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
 
         let recursion_proving_start = Instant::now();

prover/src/build.rs
@@ -2,7 +2,9 @@ use std::borrow::Borrow;
 use std::path::PathBuf;
 
 use p3_baby_bear::BabyBear;
+use sp1_core::runtime::SP1Context;
 use sp1_core::stark::StarkVerifyingKey;
+use sp1_core::utils::SP1ProverOpts;
 use sp1_core::{io::SP1Stdin, stark::ShardProof};
 pub use sp1_recursion_circuit::stark::build_wrap_circuit;
 pub use sp1_recursion_circuit::witness::Witnessable;
@@ -121,6 +123,8 @@ pub fn dummy_proof() -> (StarkVerifyingKey<OuterSC>, ShardProof<OuterSC>) {
 
     tracing::info!("initializing prover");
     let prover = SP1Prover::new();
+    let opts = SP1ProverOpts::default();
+    let context = SP1Context::default();
 
     tracing::info!("setup elf");
     let (pk, vk) = prover.setup(elf);
@@ -128,16 +132,16 @@ pub fn dummy_proof() -> (StarkVerifyingKey<OuterSC>, ShardProof<OuterSC>) {
     tracing::info!("prove core");
     let mut stdin = SP1Stdin::new();
     stdin.write(&500u32);
-    let core_proof = prover.prove_core(&pk, &stdin).unwrap();
+    let core_proof = prover.prove_core(&pk, &stdin, opts, context).unwrap();
 
     tracing::info!("compress");
-    let compressed_proof = prover.compress(&vk, core_proof, vec![]).unwrap();
+    let compressed_proof = prover.compress(&vk, core_proof, vec![], opts).unwrap();
 
     tracing::info!("shrink");
-    let shrink_proof = prover.shrink(compressed_proof).unwrap();
+    let shrink_proof = prover.shrink(compressed_proof, opts).unwrap();
 
     tracing::info!("wrap");
-    let wrapped_proof = prover.wrap_bn254(shrink_proof).unwrap();
+    let wrapped_proof = prover.wrap_bn254(shrink_proof, opts).unwrap();
 
     (prover.wrap_vk, wrapped_proof.proof)
 }

sdk/src/provers/local.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
+use sp1_core::{runtime::SP1Context, utils::SP1ProverOpts};
 use sp1_prover::{SP1Prover, SP1Stdin};
 
 use crate::{
-    Prover, SP1CompressedProof, SP1PlonkBn254Proof, SP1Proof, SP1ProofWithPublicValues,
-    SP1ProvingKey, SP1VerifyingKey,
+    Prover, SP1Proof, SP1ProofKind, SP1ProofWithPublicValues, SP1ProvingKey, SP1VerifyingKey,
 };
 
 use super::ProverType;
@@ -34,36 +34,36 @@ impl Prover for LocalProver {
         &self.prover
     }
 
-    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Proof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
-        Ok(SP1ProofWithPublicValues {
-            proof: proof.proof.0,
-            stdin: proof.stdin,
-            public_values: proof.public_values,
-            sp1_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_compressed(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1CompressedProof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
+    fn prove<'a>(
+        &'a self,
+        pk: &SP1ProvingKey,
+        stdin: SP1Stdin,
+        opts: SP1ProverOpts,
+        context: SP1Context<'a>,
+        kind: SP1ProofKind,
+    ) -> Result<SP1ProofWithPublicValues> {
+        let proof = self.prover.prove_core(pk, &stdin, opts, context)?;
+        if kind == SP1ProofKind::Core {
+            return Ok(SP1ProofWithPublicValues {
+                proof: SP1Proof::Core(proof.proof.0),
+                stdin: proof.stdin,
+                public_values: proof.public_values,
+                sp1_version: self.version().to_string(),
+            });
+        }
         let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
         let public_values = proof.public_values.clone();
-        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs)?;
-        Ok(SP1CompressedProof {
-            proof: reduce_proof.proof,
-            stdin,
-            public_values,
-            sp1_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkBn254Proof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
-        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
-        let public_values = proof.public_values.clone();
-        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs)?;
-        let compress_proof = self.prover.shrink(reduce_proof)?;
-        let outer_proof = self.prover.wrap_bn254(compress_proof)?;
+        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs, opts)?;
+        if kind == SP1ProofKind::Compressed {
+            return Ok(SP1ProofWithPublicValues {
+                proof: SP1Proof::Compressed(reduce_proof.proof),
+                stdin,
+                public_values,
+                sp1_version: self.version().to_string(),
+            });
+        }
+        let compress_proof = self.prover.shrink(reduce_proof, opts)?;
+        let outer_proof = self.prover.wrap_bn254(compress_proof, opts)?;
 
         let plonk_bn254_aritfacts = if sp1_prover::build::sp1_dev_mode() {
             sp1_prover::build::try_build_plonk_bn254_artifacts_dev(
@@ -76,12 +76,15 @@ impl Prover for LocalProver {
         let proof = self
             .prover
             .wrap_plonk_bn254(outer_proof, &plonk_bn254_aritfacts);
-        Ok(SP1ProofWithPublicValues {
-            proof,
-            stdin,
-            public_values,
-            sp1_version: self.version().to_string(),
-        })
+        if kind == SP1ProofKind::Plonk {
+            return Ok(SP1ProofWithPublicValues {
+                proof: SP1Proof::Plonk(proof),
+                stdin,
+                public_values,
+                sp1_version: self.version().to_string(),
+            });
+        }
+        unreachable!()
     }
 }
 

sdk/src/provers/mock.rs
@@ -1,10 +1,11 @@
 #![allow(unused_variables)]
 use crate::{
-    Prover, SP1CompressedProof, SP1PlonkBn254Proof, SP1Proof, SP1ProofWithPublicValues,
-    SP1ProvingKey, SP1VerificationError, SP1VerifyingKey,
+    Prover, SP1Proof, SP1ProofKind, SP1ProofWithPublicValues, SP1ProvingKey, SP1VerificationError,
+    SP1VerifyingKey,
 };
 use anyhow::Result;
 use p3_field::PrimeField;
+use sp1_core::{runtime::SP1Context, utils::SP1ProverOpts};
 use sp1_prover::{
     verify::verify_plonk_bn254_public_inputs, HashableKey, PlonkBn254Proof, SP1Prover, SP1Stdin,
 };
@@ -37,66 +38,57 @@ impl Prover for MockProver {
         unimplemented!("MockProver does not support SP1Prover")
     }
 
-    fn prove(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1Proof> {
-        let (public_values, _) = SP1Prover::execute(&pk.elf, &stdin)?;
-        Ok(SP1ProofWithPublicValues {
-            proof: vec![],
-            stdin,
-            public_values,
-            sp1_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_compressed(
-        &self,
-        _pk: &SP1ProvingKey,
-        _stdin: SP1Stdin,
-    ) -> Result<SP1CompressedProof> {
-        unimplemented!()
-    }
-
-    fn prove_plonk(&self, pk: &SP1ProvingKey, stdin: SP1Stdin) -> Result<SP1PlonkBn254Proof> {
-        let (public_values, _) = SP1Prover::execute(&pk.elf, &stdin)?;
-        Ok(SP1PlonkBn254Proof {
-            proof: PlonkBn254Proof {
-                public_inputs: [
-                    pk.vk.hash_bn254().as_canonical_biguint().to_string(),
-                    public_values.hash().to_string(),
-                ],
-                encoded_proof: "".to_string(),
-                raw_proof: "".to_string(),
-                plonk_vkey_hash: [0; 32],
-            },
-            stdin,
-            public_values,
-            sp1_version: self.version().to_string(),
-        })
+    fn prove<'a>(
+        &'a self,
+        pk: &SP1ProvingKey,
+        stdin: SP1Stdin,
+        opts: SP1ProverOpts,
+        context: SP1Context<'a>,
+        kind: SP1ProofKind,
+    ) -> Result<SP1ProofWithPublicValues> {
+        match kind {
+            SP1ProofKind::Core => {
+                let (public_values, _) = SP1Prover::execute(&pk.elf, &stdin, context)?;
+                Ok(SP1ProofWithPublicValues {
+                    proof: SP1Proof::Core(vec![]),
+                    stdin,
+                    public_values,
+                    sp1_version: self.version().to_string(),
+                })
+            }
+            SP1ProofKind::Compressed => unimplemented!(),
+            SP1ProofKind::Plonk => {
+                let (public_values, _) = SP1Prover::execute(&pk.elf, &stdin, context)?;
+                Ok(SP1ProofWithPublicValues {
+                    proof: SP1Proof::Plonk(PlonkBn254Proof {
+                        public_inputs: [
+                            pk.vk.hash_bn254().as_canonical_biguint().to_string(),
+                            public_values.hash().to_string(),
+                        ],
+                        encoded_proof: "".to_string(),
+                        raw_proof: "".to_string(),
+                        plonk_vkey_hash: [0; 32],
+                    }),
+                    stdin,
+                    public_values,
+                    sp1_version: self.version().to_string(),
+                })
+            }
+        }
     }
 
     fn verify(
         &self,
-        _proof: &SP1Proof,
-        _vkey: &SP1VerifyingKey,
-    ) -> Result<(), SP1VerificationError> {
-        Ok(())
-    }
-
-    fn verify_compressed(
-        &self,
-        _proof: &SP1CompressedProof,
-        _vkey: &SP1VerifyingKey,
-    ) -> Result<(), SP1VerificationError> {
-        Ok(())
-    }
-
-    fn verify_plonk(
-        &self,
-        proof: &SP1PlonkBn254Proof,
+        bundle: &SP1ProofWithPublicValues,
         vkey: &SP1VerifyingKey,
     ) -> Result<(), SP1VerificationError> {
-        verify_plonk_bn254_public_inputs(vkey, &proof.public_values, &proof.proof.public_inputs)
-            .map_err(SP1VerificationError::Plonk)?;
-        Ok(())
+        match &bundle.proof {
+            SP1Proof::Plonk(PlonkBn254Proof { public_inputs, .. }) => {
+                verify_plonk_bn254_public_inputs(vkey, &bundle.public_values, public_inputs)
+                    .map_err(SP1VerificationError::Plonk)
+            }
+            _ => Ok(()),
+        }
     }
 }
 

argumentcomputer/sphinx

examples/ssz-withdrawals/script/src/main.rs
@@ -1,4 +1,4 @@
-use sphinx_sdk::{utils, ProverClient, SphinxProof, SphinxStdin};
+use sphinx_sdk::{utils, ProverClient, SphinxProofWithPublicValues, SphinxStdin};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
@@ -10,7 +10,7 @@ fn main() {
     let stdin = SphinxStdin::new();
     let client = ProverClient::new();
     let (pk, vk) = client.setup(ELF);
-    let proof = client.prove(&pk, stdin).expect("proving failed");
+    let proof = client.prove(&pk, stdin).run().expect("proving failed");
 
     // Verify proof.
     client.verify(&proof, &vk).expect("verification failed");
@@ -19,7 +19,8 @@ fn main() {
     proof
         .save("proof-with-pis.bin")
         .expect("saving proof failed");
-    let deserialized_proof = SphinxProof::load("proof-with-pis.bin").expect("loading proof failed");
+    let deserialized_proof =
+        SphinxProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
 
     // Verify the deserialized proof.
     client

prover/scripts/fibonacci_sweep.rs
@@ -3,6 +3,11 @@
 use std::{fs::File, io::BufWriter, io::Write, time::Instant};
 
 use itertools::iproduct;
+use sphinx_core::{
+    io::SphinxStdin,
+    utils::{SphinxProverOpts, SphinxProverOpts},
+};
+use sphinx_prover::SphinxProver;
 use tracing_subscriber::EnvFilter;
 use tracing_subscriber::{fmt::format::FmtSpan, util::SubscriberInitExt};
 use sphinx_core::io::SphinxStdin;
@@ -62,7 +67,9 @@ fn main() {
             proofs: vec![],
         };
         let leaf_proving_start = Instant::now();
-        let proof = prover.prove_core(&pk, &stdin).unwrap();
+        let proof = prover
+            .prove_core(&pk, &stdin, SphinxProverOpts::default(), SphinxContext::default())
+            .unwrap();
         let leaf_proving_duration = leaf_proving_start.elapsed().as_secs_f64();
 
         let recursion_proving_start = Instant::now();

prover/src/build.rs
@@ -2,7 +2,9 @@ use std::borrow::Borrow;
 use std::path::PathBuf;
 
 use p3_baby_bear::BabyBear;
+use sphinx_core::runtime::SphinxContext;
 use sphinx_core::stark::StarkVerifyingKey;
+use sphinx_core::utils::SphinxProverOpts;
 use sphinx_core::{io::SphinxStdin, stark::ShardProof};
 pub use sphinx_recursion_circuit::stark::build_wrap_circuit;
 pub use sphinx_recursion_circuit::witness::Witnessable;
@@ -121,6 +123,8 @@ pub fn dummy_proof() -> (StarkVerifyingKey<OuterSC>, ShardProof<OuterSC>) {
 
     tracing::info!("initializing prover");
     let prover = SphinxProver::new();
+    let opts = SphinxProverOpts::default();
+    let context = SphinxContext::default();
 
     tracing::info!("setup elf");
     let (pk, vk) = prover.setup(elf);
@@ -128,16 +132,16 @@ pub fn dummy_proof() -> (StarkVerifyingKey<OuterSC>, ShardProof<OuterSC>) {
     tracing::info!("prove core");
     let mut stdin = SphinxStdin::new();
     stdin.write(&500u32);
-    let core_proof = prover.prove_core(&pk, &stdin).unwrap();
+    let core_proof = prover.prove_core(&pk, &stdin, opts, context).unwrap();
 
     tracing::info!("compress");
-    let compressed_proof = prover.compress(&vk, core_proof, vec![]).unwrap();
+    let compressed_proof = prover.compress(&vk, core_proof, vec![], opts).unwrap();
 
     tracing::info!("shrink");
-    let shrink_proof = prover.shrink(compressed_proof).unwrap();
+    let shrink_proof = prover.shrink(compressed_proof, opts).unwrap();
 
     tracing::info!("wrap");
-    let wrapped_proof = prover.wrap_bn254(shrink_proof).unwrap();
+    let wrapped_proof = prover.wrap_bn254(shrink_proof, opts).unwrap();
 
     (prover.wrap_vk, wrapped_proof.proof)
 }

sdk/src/provers/local.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
+use sphinx_core::{runtime::SphinxContext, utils::SphinxProverOpts};
 use sphinx_prover::{SphinxProver, SphinxStdin};
 
 use crate::{
-    Prover, SphinxCompressedProof, SphinxPlonkBn254Proof, SphinxProof, SphinxProofWithPublicValues,
-    SphinxProvingKey, SphinxVerifyingKey,
+    Prover, SphinxProof, SphinxProofKind, SphinxProofWithPublicValues, SphinxProvingKey,
+    SphinxVerifyingKey,
 };
 
 use super::ProverType;
@@ -34,44 +35,36 @@ impl Prover for LocalProver {
         &self.prover
     }
 
-    fn prove(&self, pk: &SphinxProvingKey, stdin: SphinxStdin) -> Result<SphinxProof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
-        Ok(SphinxProofWithPublicValues {
-            proof: proof.proof.0,
-            stdin: proof.stdin,
-            public_values: proof.public_values,
-            sphinx_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_compressed(
-        &self,
+    fn prove<'a>(
+        &'a self,
         pk: &SphinxProvingKey,
         stdin: SphinxStdin,
-    ) -> Result<SphinxCompressedProof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
+        opts: SphinxProverOpts,
+        context: SphinxContext<'a>,
+        kind: SphinxProofKind,
+    ) -> Result<SphinxProofWithPublicValues> {
+        let proof = self.prover.prove_core(pk, &stdin, opts, context)?;
+        if kind == SphinxProofKind::Core {
+            return Ok(SphinxProofWithPublicValues {
+                proof: SphinxProof::Core(proof.proof.0),
+                stdin: proof.stdin,
+                public_values: proof.public_values,
+                sphinx_version: self.version().to_string(),
+            });
+        }
         let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
         let public_values = proof.public_values.clone();
-        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs)?;
-        Ok(SphinxCompressedProof {
-            proof: reduce_proof.proof,
-            stdin,
-            public_values,
-            sphinx_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_plonk(
-        &self,
-        pk: &SphinxProvingKey,
-        stdin: SphinxStdin,
-    ) -> Result<SphinxPlonkBn254Proof> {
-        let proof = self.prover.prove_core(pk, &stdin)?;
-        let deferred_proofs = stdin.proofs.iter().map(|p| p.0.clone()).collect();
-        let public_values = proof.public_values.clone();
-        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs)?;
-        let compress_proof = self.prover.shrink(reduce_proof)?;
-        let outer_proof = self.prover.wrap_bn254(compress_proof)?;
+        let reduce_proof = self.prover.compress(&pk.vk, proof, deferred_proofs, opts)?;
+        if kind == SphinxProofKind::Compressed {
+            return Ok(SphinxProofWithPublicValues {
+                proof: SphinxProof::Compressed(reduce_proof.proof),
+                stdin,
+                public_values,
+                sphinx_version: self.version().to_string(),
+            });
+        }
+        let compress_proof = self.prover.shrink(reduce_proof, opts)?;
+        let outer_proof = self.prover.wrap_bn254(compress_proof, opts)?;
 
         let plonk_bn254_aritfacts = if sphinx_prover::build::sphinx_dev_mode() {
             sphinx_prover::build::try_build_plonk_bn254_artifacts_dev(
@@ -84,12 +77,15 @@ impl Prover for LocalProver {
         let proof = self
             .prover
             .wrap_plonk_bn254(outer_proof, &plonk_bn254_aritfacts);
-        Ok(SphinxProofWithPublicValues {
-            proof,
-            stdin,
-            public_values,
-            sphinx_version: self.version().to_string(),
-        })
+        if kind == SphinxProofKind::Plonk {
+            return Ok(SphinxProofWithPublicValues {
+                proof: SphinxProof::Plonk(proof),
+                stdin,
+                public_values,
+                sphinx_version: self.version().to_string(),
+            });
+        }
+        unreachable!()
     }
 }
 

sdk/src/provers/mock.rs
@@ -1,10 +1,11 @@
 #![allow(unused_variables)]
 use crate::{
-    Prover, SphinxCompressedProof, SphinxPlonkBn254Proof, SphinxProof, SphinxProofWithPublicValues,
-    SphinxProvingKey, SphinxVerificationError, SphinxVerifyingKey,
+    Prover, SphinxProof, SphinxProofKind, SphinxProofWithPublicValues, SphinxProvingKey,
+    SphinxVerificationError, SphinxVerifyingKey,
 };
 use anyhow::Result;
 use p3_field::PrimeField;
+use sphinx_core::{runtime::SphinxContext, utils::SphinxProverOpts};
 use sphinx_prover::{
     types::HashableKey, verify::verify_plonk_bn254_public_inputs, PlonkBn254Proof, SphinxProver,
     SphinxStdin,
@@ -38,70 +39,57 @@ impl Prover for MockProver {
         unimplemented!("MockProver does not support SP1Prover")
     }
 
-    fn prove(&self, pk: &SphinxProvingKey, stdin: SphinxStdin) -> Result<SphinxProof> {
-        let (public_values, _) = SphinxProver::execute(&pk.elf, &stdin)?;
-        Ok(SphinxProofWithPublicValues {
-            proof: vec![],
-            stdin,
-            public_values,
-            sphinx_version: self.version().to_string(),
-        })
-    }
-
-    fn prove_compressed(
-        &self,
-        _pk: &SphinxProvingKey,
-        _stdin: SphinxStdin,
-    ) -> Result<SphinxCompressedProof> {
-        unimplemented!()
-    }
-
-    fn prove_plonk(
-        &self,
+    fn prove<'a>(
+        &'a self,
         pk: &SphinxProvingKey,
         stdin: SphinxStdin,
-    ) -> Result<SphinxPlonkBn254Proof> {
-        let (public_values, _) = SphinxProver::execute(&pk.elf, &stdin)?;
-        Ok(SphinxPlonkBn254Proof {
-            proof: PlonkBn254Proof {
-                public_inputs: [
-                    pk.vk.hash_bn254().as_canonical_biguint().to_string(),
-                    public_values.hash().to_string(),
-                ],
-                encoded_proof: "".to_string(),
-                raw_proof: "".to_string(),
-                plonk_vkey_hash: [0; 32],
-            },
-            stdin,
-            public_values,
-            sphinx_version: self.version().to_string(),
-        })
+        opts: SphinxProverOpts,
+        context: SphinxContext<'a>,
+        kind: SphinxProofKind,
+    ) -> Result<SphinxProofWithPublicValues> {
+        match kind {
+            SphinxProofKind::Core => {
+                let (public_values, _) = SphinxProver::execute(&pk.elf, &stdin, context)?;
+                Ok(SphinxProofWithPublicValues {
+                    proof: SphinxProof::Core(vec![]),
+                    stdin,
+                    public_values,
+                    sphinx_version: self.version().to_string(),
+                })
+            }
+            SphinxProofKind::Compressed => unimplemented!(),
+            SphinxProofKind::Plonk => {
+                let (public_values, _) = SphinxProver::execute(&pk.elf, &stdin, context)?;
+                Ok(SphinxProofWithPublicValues {
+                    proof: SphinxProof::Plonk(PlonkBn254Proof {
+                        public_inputs: [
+                            pk.vk.hash_bn254().as_canonical_biguint().to_string(),
+                            public_values.hash().to_string(),
+                        ],
+                        encoded_proof: "".to_string(),
+                        raw_proof: "".to_string(),
+                        plonk_vkey_hash: [0; 32],
+                    }),
+                    stdin,
+                    public_values,
+                    sphinx_version: self.version().to_string(),
+                })
+            }
+        }
     }
 
     fn verify(
         &self,
-        _proof: &SphinxProof,
-        _vkey: &SphinxVerifyingKey,
-    ) -> Result<(), SphinxVerificationError> {
-        Ok(())
-    }
-
-    fn verify_compressed(
-        &self,
-        _proof: &SphinxCompressedProof,
-        _vkey: &SphinxVerifyingKey,
-    ) -> Result<(), SphinxVerificationError> {
-        Ok(())
-    }
-
-    fn verify_plonk(
-        &self,
-        proof: &SphinxPlonkBn254Proof,
+        bundle: &SphinxProofWithPublicValues,
         vkey: &SphinxVerifyingKey,
     ) -> Result<(), SphinxVerificationError> {
-        verify_plonk_bn254_public_inputs(vkey, &proof.public_values, &proof.proof.public_inputs)
-            .map_err(SphinxVerificationError::Plonk)?;
-        Ok(())
+        match &bundle.proof {
+            SphinxProof::Plonk(PlonkBn254Proof { public_inputs, .. }) => {
+                verify_plonk_bn254_public_inputs(vkey, &bundle.public_values, public_inputs)
+                    .map_err(SphinxVerificationError::Plonk)
+            }
+            _ => Ok(()),
+        }
     }
 }
 