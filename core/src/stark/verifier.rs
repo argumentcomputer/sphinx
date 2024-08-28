c436abe0c1cb9cb5c4be51c852b10176463db96a
John Guibas <jtguibas@gmail.com>
2024-08-28 13:57:23 -0400

    fix: verifier dos

--- core/src/stark/verifier.rs
+++ core/src/stark/verifier.rs
@@ -51,7 +51,9 @@ impl<SC: StarkGenericConfig, A: MachineAir<Val<SC>>> Verifier<SC, A> {
 
         let pcs = config.pcs();
 
-        assert_eq!(chips.len(), opened_values.chips.len());
+        if chips.len() != opened_values.chips.len() {
+            return Err(VerificationError::ChipOpeningLengthMismatch);
+        }
 
         let log_degrees = opened_values
             .chips
@@ -419,6 +421,7 @@ pub enum VerificationError<SC: StarkGenericConfig> {
     /// The shape of the opening arguments is invalid.
     OpeningShapeError(String, OpeningShapeError),
     MissingCpuChip,
+    ChipOpeningLengthMismatch,
 }
 
 impl Debug for OpeningShapeError {
@@ -485,6 +488,9 @@ impl<SC: StarkGenericConfig> Debug for VerificationError<SC> {
             VerificationError::MissingCpuChip => {
                 write!(f, "Missing CPU chip")
             }
+            VerificationError::ChipOpeningLengthMismatch => {
+                write!(f, "Chip opening length mismatch")
+            }
         }
     }
 }
@@ -503,6 +509,9 @@ impl<SC: StarkGenericConfig> Display for VerificationError<SC> {
             }
             VerificationError::MissingCpuChip => {
                 write!(f, "Missing CPU chip in shard")
+            }
+            VerificationError::ChipOpeningLengthMismatch => {
+                write!(f, "Chip opening length mismatch")
             }
         }
     }
