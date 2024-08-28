274660ecd93839ff99078088800bc8bc943be632
John Guibas <jtguibas@gmail.com>
2024-08-28 13:57:30 -0400

    get docker url

--- cli/src/build.rs
+++ cli/src/build.rs
@@ -41,7 +41,9 @@ pub(crate) fn build_program(args: &BuildArgs) -> Result<Utf8PathBuf> {
             .context("failed to run docker command")?;
 
         if !docker_check.success() {
-            eprintln!("docker is not installed or not running.");
+            eprintln!(
+                "docker is not installed or not running: https://docs.docker.com/get-docker/"
+            );
             exit(1);
         }
 
